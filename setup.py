a7b64564e0494df063a1411256bb1c2666f99ceb
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-08-01 11:57:05 +0200

    Switch from distutils to setuptools

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
-from distutils.core import setup
+from setuptools import setup
+import sys
 
 setup(name='Biopython',
-      version = '2.0 alpha 01',
+      version = '2.0a1',
       description = 'Python Distribution Utilities',
       author = "The Biopython contributors",
       url = "https://github.com/padix-key/biopython2",
@@ -20,5 +21,7 @@ setup(name='Biopython',
                   "biopython.database.rcsb"],
       install_requires = ["requests",
                           "numpy",
-                          "matplotlib"]
+                          "scipy",
+                          "matplotlib"],
+      test_suite="tests.test.test_suite"
      )
