0db6a19ba3ea6597e1553fc585cab6187767faad
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-08-15 02:20:21 +0200

    Small bug fix

--- src/biopython/sequence/align/align.py
+++ src/biopython/sequence/align/align.py
@@ -90,11 +90,11 @@ def simple_score(seq1, seq2, matrix):
     if len(seq1) != len(seq2):
         raise ValueError("Sequence lengths of {:d} and {:d} are not equal"
                          .format( len(seq1), len(seq2) ))
-    if (matrix.alphabet1() != seq1.get_alphabet() and
-        matrix.alphabet2() != seq2.get_alphabet()):
+    if (matrix.get_alphabet1() != seq1.get_alphabet() and
+        matrix.get_alphabet2() != seq2.get_alphabet()):
             raise ValueError("The sequences' alphabets do not fit the matrix")
     seq1_code = seq1.get_seq_code()
-    seq2_code = seq1.get_seq_code()
+    seq2_code = seq2.get_seq_code()
     score = 0
     for i in range(len(seq1)):
         score += matrix.get_score_by_code(seq1_code[i], seq2_code[i])
