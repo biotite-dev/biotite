d9fc62c31d04786900b40e83cee13b96a55d0d19
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-08-27 13:01:49 +0200

    Added support for multiple trajectory formats

--- src/biopython/structure/io/trajfile.py
+++ src/biopython/structure/io/trajfile.py
@@ -6,8 +6,9 @@
 import abc
 import numpy as np
 from ..atoms import AtomArray, AtomArrayStack, stack
+from ...file import File
 
-__all__ = ["TrajectoryFile", "MDTrajFile"]
+__all__ = ["TrajectoryFile"]
 
 
 class TrajectoryFile(File, metaclass=abc.ABCMeta):
@@ -19,27 +20,33 @@ class TrajectoryFile(File, metaclass=abc.ABCMeta):
         self._box = None
     
     def read(self, file_name, start=None, stop=None, step=None, atom_i=None):
-        super().read()
+        super().read(file_name)
         traj_type = self._traj_type()
         with traj_type(file_name, 'r') as f:
             if start is not None and start != 0:
                 # Discard atoms before start
                 f.read(n_frames=start, stride=None, atom_indices=atom_i)
             # The next interval is saved
-            result = f.read(stop-start, step, atom_i)
-            self._coord = self._output_value_index("coord")
-            self._time = self._output_value_index("time")
-            self._box = self._output_value_index("box")
+            if start is None or stop is None:
+                result = f.read(stride=step, atom_indices=atom_i)
+            else:
+                result = f.read(stop-start, step, atom_i)
+            self._coord = result[self._output_value_index("coord")]
+            self._time  = result[self._output_value_index("time")]
+            self._box   = result[self._output_value_index("box")]
     
-    def get_coordinates(self):
+    def get_coord(self):
         return self._coord
     
     def get_structure(self, template):
+        if template.array_length() != self._coord.shape[-2]:
+            raise ValueError("Template")
         if isinstance(template, AtomArray):
             array_stack = stack([template])
         else:
             array_stack = template.copy()
         array_stack.coord = np.copy(self._coord)
+        return array_stack
     
     def get_time(self):
         return self._time
@@ -48,7 +55,7 @@ class TrajectoryFile(File, metaclass=abc.ABCMeta):
         return self._box
     
     def write(self, file_name):
-        super().write()
+        super().write(file_name)
         traj_type = self._traj_type()
         with traj_type(file_name, 'w') as f:
             f.write(xyz=self._coord, time=self._time, box=self._box)
---
+++ src/biopython/structure/io/trr/file.py
@@ -0,0 +1,23 @@
+# Copyright 2017 Patrick Kunzmann.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+import mdtraj.formats as traj
+from ..trajfile import TrajectoryFile
+
+__all__ = ["TRRFile"]
+
+
+class TRRFile(TrajectoryFile):
+    
+    def _traj_type(self):
+        return traj.TRRTrajectoryFile
+    
+    def _output_value_index(self, value):
+        if value == "coord":
+            return 0
+        if value == "time":
+            return 1
+        if value == "box":
+            return 3
