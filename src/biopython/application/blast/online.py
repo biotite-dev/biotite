4e593abf61905c9d4722be4bfdaea253874f2462
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-08-31 19:00:30 +0200

    Continued work on BLAST application

--- src/biopython/application/blast/online.py
+++ src/biopython/application/blast/online.py
@@ -4,40 +4,63 @@
 # as part of this package.
 
 from ..application import Application, evaluation
-from ..webapp import WebApp
+from ..webapp import WebApp, RuleViolationError
+from ...sequence.sequence import Sequence
+from ...sequence.seqtypes import DNASequence, ProteinSequence
 from ...sequence.io.fasta import FastaFile
-import abc
 import time
 import requests
 from abc import abstractmethod
 
-__all__ = ["BlastOnline", "NucleotideBlastOnline", "ProteinBlastOnline"]
+__all__ = ["BlastOnline"]
 
 
-contact_delay = 3
-request_delay = 60
+_ncbi_url = "https://blast.ncbi.nlm.nih.gov/Blast.cgi"
 
-ncbi_url = "https://blast.ncbi.nlm.nih.gov/Blast.cgi"
-#ncbi_url = "http://www.ebi.ac.uk/Tools/services/rest/ncbiblast"
-
-class BlastOnline(WebApp, metaclass=abc.ABCMeta):
+class BlastOnline(WebApp):
+    
+    _last_contact = 0
+    _last_request = 0
+    _contact_delay = 3
+    _request_delay = 60
     
     def __init__(self, program, query, database="nr",
-                 app_url=ncbi_url, obey_rules=True, mail=None):
+                 app_url=_ncbi_url, obey_rules=True, mail=None):
         super().__init__(app_url, obey_rules)
+        
+        if program not in ["blastn", "megablast", "blastp",
+                           "blastx", "tblastn", "tblastx"]:
+            raise ValueError("'{:}' is not a valid BLAST program"
+                             .format(program))
         self._program = program
+        
+        requires_protein = (program in ["blastn", "megablast",
+                                        "blastx", "blastx"])
         if isinstance(query, str) and query.endswith((".fa",".fst",".fasta")):
                 file = FastaFile()
                 file.read(self._query)
-                param["QUERY"] = str(file.get_sequence())
+                sequence = file.get_sequence()
+                if isinstance(sequence, ProteinSequence) != requires_protein:
+                    raise ValueError("Query type is not suitable for program")
+        elif isinstance(query, Sequence):
+            self._query = query.copy()
         else:
-            param["QUERY"] = str(self._query)
-        self._query = query
+            if requires_protein:
+                self._query = ProteinSequence(str(query))
+            else:
+                self._query = ProteinSequence(str(query))
+        
         self._database = database
         self._gap_openining = None
         self._gap_extension = None
         self._word_size = None
         self._expect_value = None
+        
+        self._reward = None
+        self._penalty = None
+        
+        self._matrix = None
+        self._threshold = None
         
         self._mail=mail
         self._rid = None
@@ -52,11 +75,53 @@ class BlastOnline(WebApp, metaclass=abc.ABCMeta):
         
     def set_word_size(self, size):
         self._word_size = size
+        
+    def set_match_reward(self, reward):
+        self._reward = score
+    
+    def set_mismatch_penalty(self, penalty):
+        self._penalty = penalty
+        
+    def set_substitution_matrix(self, matrix_name):
+        self._matrix = matrix_name.upper()
+    
+    def set_threshold(self, threshold):
+        self._threshold = threshold
     
     def run(self):
         super().run()
-        param_dict = self._get_param_dict()
+        
+        param_dict = {}
+        param_dict["tool"] = "BiopythonClient"
+        if self._mail is not None:
+            param_dict["email"] = self._mail
+        param_dict["CMD"] = "Put"
+        param_dict["PROGRAM"] = self._program
+        param_dict["QUERY"] = str(self._query)
+        param_dict["DATABASE"] = self._database
+        if self._expect_value is not None:
+            param_dict["EXPECT"] = self._expect_value
+        if self._gap_openining is not None and self._gap_extension is not None:
+            param_dict["GAPCOSTS"] = "{:d} {:d}".format(self._gap_openining,
+                                                      self._gap_extension)
+        if self._word_size is not None:
+            param_dict["WORD_SIZE"] = self._word_size
+        
+        if self._program in ["blastn", "megablast"]:
+            if self._reward is not None:
+                param_dict["NUCL_REWARD"] = self._reward
+            if self._penalty is not None:
+                param_dict["NUCL_PENALTY"] = self._penalty
+        
+        if self._program in ["blastp", "blastx", "tblastn", "tblastx"]:
+            if self._matrix is not None:
+                param_dict["MATRIX"] = self._matrix
+            if self._threshold is not None:
+                param_dict["THRESHOLD"] = self._threshold
+        
         request = requests.get(self.app_url(), params=param_dict)
+        self._contact()
+        self._request()
         info_dict = BlastOnline._get_info(request.text)
         self._rid = info_dict["RID"]
     
@@ -64,9 +129,11 @@ class BlastOnline(WebApp, metaclass=abc.ABCMeta):
         if not self.is_started():
             raise ApplicationError("The application run has not been "
                                    "not started yet")
+        time.sleep(BlastOnline._contact_delay)
         while not self.is_finished():
             # NCBI requires a 3 second delay between requests
-            time.sleep(contact_delay)
+            time.sleep(BlastOnline._contact_delay)
+        time.sleep(BlastOnline._contact_delay)
         super().join()
     
     def is_finished(self):
@@ -83,48 +150,25 @@ class BlastOnline(WebApp, metaclass=abc.ABCMeta):
                          "RID"           : self._rid,
                          "CMD"           : "Get"}
             request = requests.get(self.app_url(), params=data_dict)
+            self._contact()
             info_dict = BlastOnline._get_info(request.text)
-            print(info_dict)
             if info_dict["Status"] == "READY":
                 self._is_finished = True
             return self._is_finished
     
     @evaluation
-    def get_result(self, format):
+    def get_alignments(self):
         param_dict = {}
         param_dict["tool"] = "BiopythonClient"
         if self._mail is not None:
             param_dict["email"] = self._mail
         param_dict["CMD"] = "Get"
         param_dict["RID"] = self._rid
-        param_dict["FORMAT_TYPE"] = format
+        param_dict["FORMAT_TYPE"] = "XML"
+        param_dict["NCBI_GI"] = "T"
         request = requests.get(self.app_url(), params=param_dict)
+        self._contact()
         print(request.text)
-    
-    @abc.abstractmethod
-    def _get_param_dict(self):
-        param = {}
-        param["tool"] = "BiopythonClient"
-        if self._mail is not None:
-            param["email"] = self._mail
-        param["CMD"] = "Put"
-        param["PROGRAM"] = self._program
-        if ( isinstance(self._query, str)
-             and self._query.endswith((".fa",".fst",".fasta")) ):
-                file = FastaFile()
-                file.read(self._query)
-                param["QUERY"] = str(file.get_sequence())
-        else:
-            param["QUERY"] = str(self._query)
-        param["DATABASE"] = self._database
-        if self._expect_value is not None:
-            param["EXPECT"] = self._expect_value
-        if self._gap_openining is not None and self._gap_extension is not None:
-            param["GAPCOSTS"] = "{:d} {:d}".format(self._gap_openining,
-                                                      self._gap_extension)
-        if self._word_size is not None:
-            param["WORD_SIZE"] = self._word_size
-        return param
     
     @staticmethod
     def _get_info(text):
@@ -143,55 +187,14 @@ class BlastOnline(WebApp, metaclass=abc.ABCMeta):
                 info_dict[pair[0].strip()] = pair[1].strip()
         return info_dict
     
-
-class NucleotideBlastOnline(BlastOnline):
+    def _contact(self):
+        contact = time.time()
+        if (contact - BlastOnline._last_contact) < BlastOnline._contact_delay:
+            self.violate_rule("The server was contacted too often")
+        BlastOnline._last_contact = contact
     
-    def __init__(self, program, query, database="nr",
-                 app_url=ncbi_url, obey_rules=True, mail=None):
-        if program not in ["blastn", "megablast"]:
-            raise ValueError("'{:}' is not a valid nucleotide BLAST program"
-                             .format(program))
-        super().__init__(program, query, database, app_url, obey_rules, mail)
-        self._reward = None
-        self._penalty = None
-    
-    def set_match_reward(self, reward):
-        self._reward = score
-    
-    def set_mismatch_penalty(self, penalty):
-        self._penalty = penalty
-    
-    def _get_param_dict(self):
-        put_dict = super()._get_param_dict()
-        if self._reward is not None:
-            put_dict["NUCL_REWARD"] = self._reward
-        if self._penalty is not None:
-            put_dict["NUCL_PENALTY"] = self._penalty
-        return put_dict
-
-
-class ProteinBlastOnline(BlastOnline):
-    
-    def __init__(self, program, query, database="nr",
-                 app_url=ncbi_url, obey_rules=True, mail=None):
-        if program not in ["blastp", "blastx", "tblastn", "tblastx"]:
-            raise ValueError("'{:}' is not a valid protein BLAST program"
-                             .format(program))
-        super().__init__(program, query, database, app_url, obey_rules, mail)
-        self._matrix = None
-        self._threshold = None
-    
-    def set_substitution_matrix(self, matrix_name):
-        self._matrix = matrix_name.upper()
-    
-    def set_threshold(self, threshold):
-        self._threshold = threshold
-    
-    def _get_param_dict(self):
-        put_dict = super()._get_param_dict()
-        if self._matrix is not None:
-            put_dict["MATRIX"] = self._matrix
-        if self._threshold is not None:
-            put_dict["THRESHOLD"] = self._threshold
-        return put_dict
-    
+    def _request(self):
+        request = time.time()
+        if (request - BlastOnline._last_request) < BlastOnline._request_delay:
+            self.violate_rule("Too frequent BLAST requests")
+        BlastOnline._last_request = request
