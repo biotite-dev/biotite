c733505534b6a0b78635bdb4fcb2e2608e1ae74a
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-09-15 15:02:40 +0200

    Source distributions do not include extension modules

--- setup.py
+++ setup.py
@@ -10,38 +10,26 @@ import sys
 release = "2.0a2"
 
 
-if "--no-extensions" in sys.argv:
+if "sdist" in sys.argv:
+    # Source distributions do not have extension modules
+    # and therefore are using not C-accelerated functions
     ext_modules = None
-    sys.argv.remove("--no-extensions")
 else:
-    try:
-        from Cython.Build import cythonize
-        ext_modules = cythonize(
-            [Extension("biopython.sequence.align.calign",
-                       ["src/biopython/sequence/align/calign.pyx"]
-                      ),
-             Extension("biopython.structure.io.pdbx.cprocessloop",
-                       ["src/biopython/structure/io/pdbx/cprocessloop.pyx"]
-                      ),
-             Extension("biopython.ccheckext",
-                       ["src/biopython/ccheckext.pyx"]
-                      )]
-        )
-    except:
-        ext_modules = \
-            [Extension("biopython.sequence.align.calign",
-                       ["src/biopython/sequence/align/calign.c"]
-                      ),
-             Extension("biopython.structure.io.pdbx.cprocessloop",
-                       ["src/biopython/structure/io/pdbx/cprocessloop.c"]
-                      ),
-             Extension("biopython.ccheckext",
-                       ["src/biopython/ccheckext.c"]
-                      )
-            ]
+    from Cython.Build import cythonize
+    ext_modules = cythonize(
+        [Extension("biopython.sequence.align.calign",
+                   ["src/biopython/sequence/align/calign.pyx"]
+                  ),
+         Extension("biopython.structure.io.pdbx.cprocessloop",
+                   ["src/biopython/structure/io/pdbx/cprocessloop.pyx"]
+                  ),
+         Extension("biopython.ccheckext",
+                   ["src/biopython/ccheckext.pyx"]
+                  )]
+    )
 
 
-setup(name="Biopython",
+setup(name="biopython",
     version = release,
     description = "A set of general tools for computational biology",
     author = "The Biopython contributors",
