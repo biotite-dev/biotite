0214f54ff89cec71f103ee30d36a9c943afed563
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-11-14 11:37:03 +0100

    Replaced license notice in file headers

--- tests/structure/test_filter.py
+++ tests/structure/test_filter.py
@@ -1,7 +1,6 @@
 # Copyright 2017 Patrick Kunzmann.
-# This code is part of the Biopython distribution and governed by its
-# license.  Please see the LICENSE file that should have been included
-# as part of this package.
+# This source code is part of the Biotite package and is distributed under the
+# 3-Clause BSD License.  Please see 'LICENSE.rst' for further information.
 
 import biopython.structure as struc
 import biopython.structure.io.npz as npz
