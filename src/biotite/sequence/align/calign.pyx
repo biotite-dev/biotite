edce6b4b2bb9db8f9fddf375f922bba5d9ce0bb9
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-11-30 10:44:06 +0100

    Removed unintentional line

--- src/biotite/sequence/align/calign.pyx
+++ src/biotite/sequence/align/calign.pyx
@@ -1,7 +1,6 @@
 # Copyright 2017 Patrick Kunzmann.
 # This source code is part of the Biotite package and is distributed under the
 # 3-Clause BSD License.  Please see 'LICENSE.rst' for further information.
-from nltk.metrics.aline import similarity_matrix
 
 cimport cython
 import numpy as np
