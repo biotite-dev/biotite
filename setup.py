8bd7cd8dfe465f801d3911ccbee30a47d145758e
Patrick Kunzmann <patrick.kunzm@gmail.com>
2017-12-02 23:17:17 +0100

    Changed classifier

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: MacOS",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
