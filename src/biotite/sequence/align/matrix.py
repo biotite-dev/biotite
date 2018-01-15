312fa78555c77f28412c6042c5b78b5c4084bc9c
Patrick Kunzmann <patrick.kunzm@gmail.com>
2018-01-15 16:55:46 +0100

    Obtaining the score matrix does not involve copying

--- src/biotite/sequence/align/matrix.py
+++ src/biotite/sequence/align/matrix.py
@@ -167,14 +167,18 @@ class SubstitutionMatrix(object):
     
     def score_matrix(self):
         """
-        Get a copy of the 2-D `ndarray` containing the score values. 
+        Get the 2-D `ndarray` containing the score values.
+        
+        In the strict sense, this breaks the immutability of the
+        `SubstitutionMatrix` object. Therefore, using the returned
+        `ndarray` read-only is strongly recommended.
         
         Returns
         -------
         matrix : ndarray
             The symbol code indexed score matrix.
         """
-        return np.copy(self._matrix)
+        return self._matrix
     
     def transpose(self):
         """
