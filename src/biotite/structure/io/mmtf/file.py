74fb86fda0a555f3c6b981d1576ae7319508bc22
Patrick Kunzmann <patrick.kunzm@gmail.com>
2018-01-23 17:46:20 +0100

    Work on new MMTF parser

--- src/biotite/structure/io/mmtf/file.py
+++ src/biotite/structure/io/mmtf/file.py
@@ -3,11 +3,12 @@
 # 3-Clause BSD License. Please see 'LICENSE.rst' for further information.
 
 import numpy as np
+import msgpack
+import struct
 from ...atoms import Atom, AtomArray, AtomArrayStack
 from ....file import File
 from ...error import BadStructureError
 from ...filter import filter_inscode_and_altloc
-import msgpack
 
 __all__ = ["MMTFFile"]
 
@@ -34,10 +35,26 @@ class MMTFFile(File):
         """
         with open(file_name, "rb") as f:
             self._content = msgpack.unpackb(f.read())
-        a = [print(e) for e in self._content]
+        for key in list(self._content.keys()):
+            self._content[key.decode("UTF-8")] = self._content.pop(key)
+        for key in list(self._content.keys()):
+            print(key)
+            self[key]
     
     def write(self, file_name):
         """
         Not implemented yet.        
         """
         raise NotImplementedError()
+    
+    def __getitem__(self, key):
+        data = self._content[key]
+        if isinstance(data, bytes) and data[0] == 0:
+            # MMTF specific format -> requires decoding
+            codec     = struct.unpack(">i", data[0:4 ])[0]
+            length    = struct.unpack(">i", data[4:8 ])[0]
+            param     = struct.unpack(">i", data[8:12])[0]
+            raw_data  = data[12:]
+            print(codec)
+        else:
+            return data
