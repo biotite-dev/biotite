80880b27c0ddd2b90dad1f017f1ba748b3018aa9
Patrick Kunzmann <patrick.kunzm@gmail.com>
2018-02-08 17:16:38 +0100

    Added Windows to operating systems

--- setup.py
+++ setup.py
@@ -88,6 +88,7 @@ setup(
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
