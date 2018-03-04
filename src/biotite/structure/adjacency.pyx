5424cf21f3964ef1368c1631d587e98115c00667
Patrick Kunzmann <patrick.kunzm@gmail.com>
2018-03-04 15:40:08 +0100

    Finished adjacency map optimization

--- src/biotite/structure/adjacency.pyx
+++ src/biotite/structure/adjacency.pyx
@@ -75,6 +75,10 @@ cdef class AdjacencyMap:
         self._box_count = ((max_coord - min_coord) // box_size) +1
         # ndarray of pointers to C-arrays
         # containing indices to atom array
+        # First deallocate, just to make sure
+        # (Constructor might be called multiple times)
+        if self._boxes is None:
+            deallocate_ptrs(self._boxes)
         self._boxes = np.zeros(self.box_count, dtype=np.uint64)
         # Stores the length of the C-arrays
         self._box_length = np.zeros(self.box_count, dtype=np.int32)
@@ -101,14 +105,7 @@ cdef class AdjacencyMap:
             self._boxes[i,j,k] = <ptr> box_ptr
             
     def __dealloc__():
-        cdef int i, j, k
-        cdef int* box_ptr
-        # Free box pointers
-        for i in self._boxes.shape[0]:
-            for j in self._boxes.shape[1]:
-                for k in self._boxes.shape[2]:
-                    box_ptr = <int*>self._boxes[i,j,k]
-                    free(box_ptr)
+        deallocate_ptrs(self._boxes)
     
     def get_atoms(self, np.ndarray coord, float32 radius):
         """
@@ -133,19 +130,20 @@ cdef class AdjacencyMap:
         --------
         get_atoms_in_box
         """
-        
-        
-        indices = self.get_atoms_in_box(coord, int(radius/self.boxsize)+1)
-        sel_coord = self.array.coord[indices]
+        cdef np.ndarray indices = \
+            self.get_atoms_in_box(coord, int(radius/self.boxsize)+1)
+        cdef np.ndarray sel_coord = np.asarray(self._coord[indices])
         dist = distance(sel_coord, coord)
         return indices[dist <= radius]
     
-    def _get_atoms(self, float32[:] coord, float32 radius):
-        pass
-    
-    def get_atoms_in_box(self, np.ndarray coord, int box_r=1):
+    def get_atoms_in_box(self, np.ndarray coord,
+                         int box_r=1,
+                         bint efficient_mode=False,
+                         np.ndarray array_indices=None):
         """
-        Search for atoms in vicinity of the given position.
+        Search for atoms in vicinity of the given box.
+        
+        This is more efficient than `get_atoms()`.
         
         Parameters
         ----------
@@ -153,60 +151,107 @@ cdef class AdjacencyMap:
             The central coordinates, around which the atoms are
             searched.
         box_r: float, optional
-            The radius around `coord` (in amount of boxes), in which the
-            atoms are searched. This does not correspond to the
+            The radius around `coord` (in amount of boxes), in which
+            the atoms are searched. This does not correspond to the
             Euclidian distance used in `get_atoms()`. In this case, all
             atoms in the box corresponding to `coord` and in adjacent
             boxes are returned.
-            By default atoms are searched in the box of `coord` and adjacent
-            boxes.
+            By default atoms are searched in the box of `coord`
+            and adjacent boxes.
+        efficient_mode : bool, optional
+            If enabled, the method will be much more efficient for
+            multiple calls of this method with the same `box_r`.
+            Rather than creating a new `ndarray` buffer for the
+            indices, the indices will be put into the provided
+            `array_indices` buffer.
+            (Default: false)
+        array_indices : ndarray, dtype=int, optional
+            If provided, the method will put the indices into the
+            buffer of this `ndarray`.
+            This increases the performance for multiple
+            calls, since no new `ndarray` needs to be created.
+            Note, that the array needs sufficient size (larger than
+            the actual amount of resulting indices), that is dependent
+            on `box_r`.
+            When you call the method the first time in
+            `efficient_mode`, leave this parameter out, a reusable
+            array of sufficient size will be created for you.
+            Has no effect if `efficient_mode` is false.
+            The array will only be partly updated with meaningful
+            values. The range of the array, that is filled with
+            meaningful values is returned in `length`.
         
         Returns
         -------
         indices : ndarray, dtype=int
             The indices of the atom array, where the atoms are in the
-            defined vicinity around `coord`.
+            defined vicinity around `coord`. If `efficient_mode`
+            is enabled. the `length` return value gives the range of
+            `indices`, that is filled with meaningful values.
+        length : int, optional
+            Exclusive range of meaningful values in `efficient_mode`.
             
         See Also
         --------
         get_atoms
         """
-        box_i =  self._get_box_index(coord)
-        atom_indices = []
-        shape = self.boxes.shape
-        for x in range(box_i[0]-box_r, box_i[0]+box_r+1):
-            if (x >= 0 and x < shape[0]):
-                for y in range(box_i[1]-box_r, box_i[1]+box_r+1):
-                    if (y >= 0 and y < shape[1]):
-                        for z in range(box_i[2]-box_r, box_i[2]+box_r+1):
-                            if (z >= 0 and z < shape[2]):
-                                atom_indices.extend(self.boxes[x,y,z])
-        return np.array(atom_indices)
+        # Pessimistic assumption on index array length requirement
+        cdef int length
+        cdef int index_array_length = (2*box_r + 1)**3 * self._max_box_length
+        cdef int[:] indices
+        if not efficient_mode or array_indices is None:
+            indices = np.zeros(index_array_length,
+                                                 dtype=np.int32):
+        else:
+            # Check if index array has sufficient size and dtype
+            if array_indices.dtype != np.int32 \
+                or len(array_indices) < index_array_length:
+                    raise ValueError("Optionally provided index array"
+                                     "is insufficient")
+            indices = array_indices
+        # Fill index array
+        length = self._get_atoms_in_box(coord.astype(float32, copy=False),
+                                        indices, box_r)
+        if not efficient_mode:
+            return np.asarray(indices[:length])
+        else:
+            return np.asarray(indices), length
     
     
-    def _get_atoms_in_box(self, float32[:] coord, int box_r=1):
+    def _get_atoms_in_box(self,
+                          float32[:] coord not None,
+                          int[:] indices not None
+                          int box_r=1):
+        cdef int length
+        cdef int* ptr
         cdef float32 x, y,z
         cdef int i, j, k
         cdef int adj_i, adj_j, adj_k
-        cdef int array_i
-        # Pessimistic assumption on index array length requirement
-        cdef int index_array_length = (2*box_r + 1)**3 * self._max_box_length
-        cdef int[:] array_indices = np.zeros(index_array_length,
-                                             dtype=np.int32):
+        cdef int array_i, box_i
         x = coord[0]
         y = coord[1]
         z = coord[2]
         if not self._check_coord(x, y, z):
             raise ValueError("Coordinates are not in range of box")
         self._get_box_index(x, y, z, &i, &j, &k)
+        array_i = 0
+        # Look into boxes of the indices and adjacent boxes
+        # in all 3 dimensions
         for adj_i in range(i-box_r, i+box_r+1):
             if (adj_i >= 0 and adj_i < shape[0]):
                 for adj_j in range(j-box_r, j+box_r+1):
                     if (adj_j >= 0 and adj_j < shape[1]):
                         for adj_k in range(k-box_r, k+box_r+1):
                             if (adj_k >= 0 and adj_k < shape[2]):
-                                
-        
+                                # Fill with index array
+                                # with indices in box
+                                ptr = <int*>self._boxes[i,j,k]
+                                length = self._box_length[i,j,k]
+                                for box_i in range(length):
+                                    indices[array_i] = ptr[box_i]
+                                    array_i += 1
+        # return the actual length of the index array
+        return array_i
     
     
     cdef inline void _get_box_index(self, float32 x, float32 y, float32 z,
@@ -224,3 +269,14 @@ cdef class AdjacencyMap:
         if z < self._min_coord[2] or z > self._max_coord[2]:
             return False
         return True
+
+
+cdef inline deallocate_ptrs(ptr[:,:,:] ptrs):
+    cdef int i, j, k
+    cdef int* box_ptr
+    # Free box pointers
+    for i in ptrs.shape[0]:
+        for j in ptrs.shape[1]:
+            for k in ptrs.shape[2]:
+                box_ptr = <int*>ptrs[i,j,k]
+                free(box_ptr)
