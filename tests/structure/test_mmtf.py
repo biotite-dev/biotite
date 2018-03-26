52b3a85158f5b5ce3af3e3b0c2487f857110516e
Patrick Kunzmann <patrick.kunzm@gmail.com>
2018-03-26 15:09:37 +0200

    Added unit tests for MMTF

--- tests/structure/test_mmtf.py
+++ tests/structure/test_mmtf.py
@@ -37,15 +37,26 @@ def test_codecs(path):
                 assert (array1 == array2).all()
 
 
-@pytest.mark.xfail(raises=NotImplementedError)
-@pytest.mark.parametrize("path", glob.glob(join(data_dir, "*.mmtf")))
-def test_array_conversion(path):
+@pytest.mark.parametrize("path, single_model",
+                         itertools.product(glob.glob(join(data_dir, "*.mmtf")),
+                                           ["False, True"])
+                        )
+def test_array_conversion(path, single_model):
+    if single_model:
+        model = 1
+    else:
+        model = None
     mmtf_file = mmtf.MMTFFile()
     mmtf_file.read(path)
-    array1 = mmtf.get_structure(mmtf_file)
-    mmtf.set_structure(mmtf_file, array1)
-    array2 = mmtf.get_structure(mmtf_file)
-    assert array1 == array2
+    a1 = mmtf.get_structure(mmtf_file, model=model)
+    mmtf_file = mmtf.MMTFFile()
+    mmtf.set_structure(mmtf_file, a1)
+    a2 = mmtf.get_structure(mmtf_file, model=model)
+    for category in a1.get_annotation_categories():
+        assert a1.get_annotation(category).tolist() == \
+               a2.get_annotation(category).tolist()
+    assert a1.coord.flatten().tolist() == \
+           approx(a2.coord.flatten().tolist(), abs=1e-3)
 
 
 mmtf_paths = sorted(glob.glob(join(data_dir, "*.mmtf")))
@@ -68,7 +79,8 @@ def test_pdbx_consistency(file_index, is_stack):
                      "atom_name", "element"]:
         assert a1.get_annotation(category).tolist() == \
                a2.get_annotation(category).tolist()
-    assert a1.coord.flatten().tolist() == approx(a2.coord.flatten().tolist())
+    assert a1.coord.flatten().tolist() == \
+           approx(a2.coord.flatten().tolist(), abs=1e-3)
 
 
 @pytest.mark.xfail(raises=NotImplementedError)
