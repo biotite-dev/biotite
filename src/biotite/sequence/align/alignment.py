1a85359267bf98ba5b12d72d94f118126093d470
Patrick Kunzmann <patrick.kunzm@gmail.com>
2018-05-02 14:45:44 +0200

    Clarified docstring

--- src/biotite/sequence/align/alignment.py
+++ src/biotite/sequence/align/alignment.py
@@ -22,9 +22,8 @@ class Alignment(object):
     Rather than saving a list of aligned symbols, this class saves the
     original *n* sequences, that were aligned, and a so called *trace*,
     which indicate the aligned symbols of these sequences.
-    The trace is a *(m x n)* `ndarray` with alignment length *m*, where
-    the first dimension is the position in the alignment and the second
-    dimension represents the sequence.
+    The trace is a *(m x n)* `ndarray` with alignment length *m* and
+    sequence count *n*.
     Each element of the trace is the index in the corresponding
     sequence. A gap is represented by the value -1.
     
