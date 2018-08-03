4a894382e9f65f151588a7bc3dd4c3c3876e4952
padix-key <patrick.kunzm@gmail.com>
2018-08-03 11:30:01 +0200

    New unit test for adjacency matrix creation

--- tests/structure/test_celllist.py
+++ tests/structure/test_celllist.py
@@ -2,12 +2,15 @@
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
+from .util import data_dir
 import biotite.structure as struc
+import biotite.structure.io as strucio
+from os.path import join
 import numpy as np
 import pytest
 
 
-def test_cell_list():
+def test_get_atoms():
     array = struc.AtomArray(length=5)
     array.coord = np.array([[0,0,i] for i in range(5)])
     cell_list = struc.CellList(array, cell_size=1)
@@ -23,3 +26,23 @@ def test_cell_list():
                         3, 4]
     indices = cell_list.get_atoms(pos, 2)
     assert indices[indices != -1].tolist() == expected_indices
+
+def test_adjacency_matrix():
+    array = strucio.load_structure(join(data_dir, "3o5r.mmtf"))
+    array = array[struc.filter_amino_acids(array)]
+    ca = array[array.atom_name == "CA"]
+    cell_list = struc.CellList(ca, cell_size=5)
+    threshold = 7
+    matrix = cell_list.create_adjacency_matrix(threshold)
+    coord = ca.coord
+    # Create distance matrix
+    diff = coord[:, np.newaxis, :] - coord[np.newaxis, :, :]
+    # Convert to float64 to avoid errorenous warning
+    # https://github.com/ContinuumIO/anaconda-issues/issues/9129
+    diff = diff.astype(np.float64)
+    distance = np.sqrt(np.sum(diff**2, axis=-1))
+    # Create adjacency matrix from distance matrix
+    expected_matrix = (distance <= threshold)
+    # Both ways to create an adjacency matrix
+    # should give the same result
+    assert matrix.tolist() == expected_matrix.tolist()
