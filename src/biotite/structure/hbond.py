e9f7f3971a2cea3a4d5df3770e9b8fb7a4b2b0a4
Daniel Bauer <daniel.bauer@headlezz.net>
2018-08-06 21:14:53 +0200

    implemented Baker&Hubbard hydrogen bond detection

--- src/biotite/structure/hbond.py
+++ src/biotite/structure/hbond.py
@@ -3,63 +3,136 @@
 # information.
 
 """
-This module provides functions for geometric measurements between atoms
-in a structure, mainly lenghts and angles.
+This module provides functions for hydrogen bonding calculation.
 """
 
 __author__ = "Daniel Bauer"
-# __all__ = ["distance", "centroid", "angle", "dihedral", "dihedral_backbone"]
+__all__ = ["is_hbond", "hbond"]
 
 from .geometry import distance, angle
 import numpy as np
-
-#### Steps to implement hbonds
-# for each frame:
-# find possible hbonds based on some criteria
-# possible implementations:
-    # most naive: go over all possible donor/acceptor pairs and calculate
-    # better: save some computation time: find donor/acceptor pairs in correct distances and only calculate angles them
+from .atoms import AtomArrayStack
 
 
-# TODO inline function into hbond method?
-def _get_bonded_hydrogen(atoms1, atoms2, cutoff=1.5):
-    hydrogens = atoms2[atoms2.element == 'H']
-
-    # TODO vectorize
-    donor_hs = []
-    for i in range(atoms1.array_length()):
-        donor = atoms1[i]
-        candidates = hydrogens[hydrogens.res_id == donor.res_id]
-        distances = distance(donor, candidates)
-        donor_hs.append(candidates[distances < cutoff])
-
-    return donor_hs
-
-
-def hbonds(donor_struct, acceptor_struct=None, cutoff_dist=2.5, cutoff_angle=120, dist_from_donor=False, donor_elements=['O'], acceptor_elements=['O']):
+def hbond(atoms1, atoms2=None, cutoff_dist=2.5, cutoff_angle=120,
+           donor_elements=['O', 'N', 'S'], acceptor_elements=['O', 'N', 'S']):
     """
-    TODO donor/acceptor atoms
+    Finds hydrogen bonds between atoms1 and atoms2.
     
+    The default criteria is: :math:`\\theta > 120deg` and :math
+    :math:`\\text(H..Acceptor) < 2.5 A` (Baker and Hubbard, 1984)
     
     Parameters
     ----------
-    structure: AtomArray or AtomArrayStack
-
+    
+    atoms1 : AtomArray or AtomArrayStack
+        TODO
+    atoms2 : AtomArray or AtomArrayStack or None
+        TODO
+    cutoff_dist: float or int
+        The maximal distance between Donor-H..Acceptor to be considered a hydrogen bond
+    cutoff_angle: float or int
+        The minimal angle between Donor-H..Acceptor to be considered a hydrogen bond  
+    donor_elements: array of strings
+        Elements to be considered as possible donors
+    acceptor_elements: array of strings
+        Elements to be considered as possible acceptors
+        
     Returns
     -------
-    np or pandas?
+    array : int
+        Nx3 matrix containing the indeces of every Donor-H..Acceptor interaction that was counted at least once. N is the number
+        of found interactions. The format is [[D_index, H_intex, A_index]
+    array : bool
+        MxN matrix that shows if an interaction N (see above) is present in the model M.
+        
+    Examples
+    --------
+    
+    Input
+    
+    >>> struct = load_structure("tests/structure/data/1l2y.pdb")
+
+    >>> triplets, mask = hbond.hbond(struct)
+    >>> hbonds_per_model = mask.sum(axis=1)
+
+    >>> plt.plot(range(len(struct)), hbonds_per_model )
+    >>> plt.xlabel("Model")
+    >>> plt.ylabel("# H-Bonds")
+    >>> plt.show()
+
+    
+    Output
+    
+    TODO
+        
+    
 
     """
 
-    donor = donor_struct[np.isin(donor_struct.element,  donor_elements)]
-    donor_h = _get_bonded_hydrogen(donor, donor_struct)
+    # Create AtomArrayStacks from AtomArrays
+    if not isinstance(atoms1, AtomArrayStack):
+        atoms1 = AtomArrayStack([atoms1])
+    if atoms2 and not isinstance(atoms2, AtomArrayStack):
+        atoms2 = AtomArrayStack([atoms2])
 
-    acceptors = acceptor_struct[np.isin(acceptor_struct.element, acceptor_elements)]
+    # If no second stack is given, the first one is used
+    if not atoms2:
+        atoms2 = atoms1
 
+    # atoms1 and atoms 2 need to be of same length
+    if atoms1.array_length() != atoms2.array_length():
+        raise ValueError("atoms1 and atoms2 must be of same length")
 
-    # TODO find acceptor_donor pairs with distance < cutoff
-    # TODO calculate Theta for matched distances
-    # TODO return format? IDs for each frame?
+    # Find donors, acceptors and donor hydrogens
+    donor = atoms1[:, np.isin(atoms1.element, donor_elements)]
+    acceptors = atoms2[:, np.isin(atoms2.element, acceptor_elements)]
+
+    def _get_bonded_hydrogen(atoms1, atoms2, cutoff=1.5):
+        """
+        Helper function to find associated hydrogens for all donors.
+        The criterium is that the hydrogen must be in the same residue
+        and the distance must be smaller then 1.5 Angstroem
+
+        """
+        hydrogens = atoms2[:, atoms2.element == 'H']
+
+        donor_hs = []
+        for i in range(atoms1.array_length()):
+            donor = atoms1[0][i]
+            candidates = hydrogens[:, hydrogens.res_id == donor.res_id]
+            distances = distance(donor, candidates[0])
+            donor_h = candidates[:, distances < cutoff]
+            donor_hs.append(donor_h)
+
+        return donor_hs
+
+    # TODO use BondList if available
+    donor_h = _get_bonded_hydrogen(donor, atoms1)
+
+    # Build a stack containing the D-H..A triplets in correct order for every possible possible hbond
+    triplets = AtomArrayStack(depth=len(donor), length=0)
+    for d_i in range(donor.array_length()):
+        for a_i in range(acceptors.array_length()):
+            for dh_i in range(donor_h[d_i].array_length()):
+                if donor[:, d_i] != acceptors[:, a_i] and donor[:, d_i].res_id != acceptors[:, a_i].res_id:
+                    triplets += donor[:, d_i] + donor_h[d_i][:, dh_i] + acceptors[:, a_i]
+
+    # Calculate angle and distance on all triplets
+    coords = triplets.coord
+    distances = distance(coords[:, 1::3], coords[:, 2::3])
+    angles = angle(coords[:, 0::3], coords[:, 1::3], coords[:, 2::3])
+
+    # Apply hbond criterion
+    cutoff_angle_radian = np.deg2rad(cutoff_angle)
+    hbond_mask = (distances < cutoff_dist) & (angles > cutoff_angle_radian)
+
+    # Reduce output to contain only triplets counted at least once
+    is_counted = hbond_mask.sum(axis=0) >= 1
+    triplets = triplets[:, np.repeat(is_counted, 3)]
+    hbond_mask = hbond_mask[:, is_counted]
+
+    return triplets, hbond_mask
 
 
 
@@ -75,4 +148,4 @@ def is_hbond(donor, donor_h, acceptor, cutoff_dist=2.5, cutoff_angle=120):
     theta = angle(donor, donor_h, acceptor)
     dist = distance(donor_h, acceptor)
 
-    return theta < cutoff_angle_rad and dist < cutoff_dist
+    return theta > cutoff_angle_rad and dist < cutoff_dist
--- tests/structure/test_hbond.py
+++ tests/structure/test_hbond.py
@@ -5,23 +5,27 @@
 import biotite.structure.hbond as hbond
 import numpy as np
 
+def test_hbond():
+    struct = "tests/structure/data/"
+
 def test_is_hbond():
     hbond_coords_valid = np.array([
-        [1.0, 1.0, 0.0],  # donor
-        [1.0, 2.0, 0.0],  # donor_h
-        [2.0, 2.0, 0.0]   # acceptor
-    ])
-
-    hbond_coords_wrong_angle = np.array([
         [1.0, 1.0, 0.0],  # donor
         [1.0, 2.0, 0.0],  # donor_h
         [1.0, 3.0, 0.0]  # acceptor
     ])
 
+    hbond_coords_wrong_angle = np.array([
+        [1.0, 1.0, 0.0],  # donor
+        [1.0, 2.0, 0.0],  # donor_h
+        [2.0, 2.0, 0.0]   # acceptor
+    ])
+
+
     hbond_coords_wrong_distance = np.array([
         [1.0, 1.0, 0.0],  # donor
         [1.0, 2.0, 0.0],  # donor_h
-        [4.0, 2.0, 0.0]  # acceptor
+        [4.0, 3.0, 0.0]  # acceptor
     ])
 
     assert hbond.is_hbond(*hbond_coords_valid)
