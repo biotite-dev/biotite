fcc2828529ef8cf91c7115233b66e35469f975aa
Daniel Bauer <daniel.bauer@headlezz.net>
2018-08-07 11:59:48 +0200

    hbond unit tests

--- tests/structure/test_hbond.py
+++ tests/structure/test_hbond.py
@@ -4,9 +4,39 @@
 
 import biotite.structure.hbond as hbond
 import numpy as np
+from biotite.structure.io import load_structure
 
-def test_hbond():
-    struct = "tests/structure/data/"
+def test_hbond_same_resid():
+    """ 1ASN in 1l2y should form a hydrogen bond between its sidechain and the N-term """
+    path = "tests/structure/data/1l2y.pdb"
+    s = load_structure(path)
+
+    triplets, mask = hbond.hbond(s[:, s.res_id == 1], s[:, s.res_id == 1])
+
+    assert triplets[:, (triplets.res_id == 1) & (triplets.atom_name == 'N')].array_length() > 1
+
+def test_hbond_total_count():
+    """
+    With the standart Baker & Hubbard criterion, 1l2y should have 28 hydrogen bonds with a frequency > 0.1
+    (comparision with external calculation)
+    """
+    path = "tests/structure/data/1l2y.pdb"
+    s = load_structure(path)
+
+    triplets, mask = hbond.hbond(s)
+    freq = hbond.get_hbond_frequency(mask)
+
+    assert len(freq[freq >= 0.1]) == 28
+
+def test_get_hbond_frequency():
+    mask = np.array([
+        [True, True, True, True, True], # 1.0
+        [False, False, False, False, False], # 0.0
+        [False, False, False, True, True] # 0.4
+    ]).T
+    print(hbond.get_hbond_frequency(mask))
+    assert not np.isin(False, np.isclose(hbond.get_hbond_frequency(mask), np.array([1.0, 0.0, 0.4])))
+
 
 def test_is_hbond():
     hbond_coords_valid = np.array([
