f79f01bbe9f9430a1c99e99c76306fe0f764133a
padix-key <patrick.kunzm@gmail.com>
2018-08-09 18:14:59 +0200

    Added stub package for Biotite

---
+++ src/biotite-stubs/array.pyi
@@ -0,0 +1,13 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+
+from typing import Iterbale, TypeVar
+import numpy as np
+
+_T = TypeVar("_T")
+
+# Generic placeholder for numpy arrays
+class Array(np.ndarray, Iterbale[_T]):
+    ...
