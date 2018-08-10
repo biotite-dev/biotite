7354205bf601031910701e68f4428f22c800c150
padix-key <patrick.kunzm@gmail.com>
2018-08-10 15:19:31 +0200

    Fixed typo

--- src/biotite/array.pyi
+++ src/biotite/array.pyi
@@ -2,12 +2,12 @@
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
-from typing import Iterbale, TypeVar
+from typing import Iterable, TypeVar
 import numpy as np
 
 
 _T = TypeVar("_T")
 
 # Generic placeholder for numpy arrays
-class Array(np.ndarray, Iterbale[_T]):
+class Array(np.ndarray, Iterable[_T]):
     ...
