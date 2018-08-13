1f61278083106720a9271034201fc55832316aed
padix-key <patrick.kunzm@gmail.com>
2018-08-13 14:47:11 +0200

    Type hints for BondList

--- src/biotite/structure/atoms.pyi
+++ src/biotite/structure/atoms.pyi
@@ -1,3 +1,7 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
 import numpy as np
 from .bonds import BondList
 from typing import (
