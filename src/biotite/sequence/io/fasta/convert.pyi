2e921dc13f3c39486978c01a092991617bf199ec
padix-key <patrick.kunzm@gmail.com>
2018-08-16 17:19:29 +0200

    Work on annotations

--- src/biotite/sequence/io/fasta/convert.pyi
+++ src/biotite/sequence/io/fasta/convert.pyi
@@ -1,57 +1,40 @@
-from biotite.sequence.align.alignment import Alignment
-from biotite.sequence.io.fasta.file import FastaFile
-from biotite.sequence.seqtypes import (
-    NucleotideSequence,
-    ProteinSequence,
-)
-from typing import (
-    Dict,
-    List,
-    Tuple,
-    Union,
-)
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+from typing import Tuple, Dict, Union, Optional, Iterable
+from .file import FastaFile
+from ...seqtypes import ProteinSequence, NucleotideSequence
+from ...align.alignment import Alignment
 
 
-def _convert_to_sequence(
-    seq_str: str
+def get_sequence(
+    fasta_file: FastaFile,
+    header: Optional[str] = None
 ) -> Union[ProteinSequence, NucleotideSequence]: ...
 
+def set_sequence(
+    fasta_file: FastaFile,
+    sequence: Union[ProteinSequence, NucleotideSequence],
+    header: Optional[str] = None
+) -> None: ...
 
-def _convert_to_string(sequence: NucleotideSequence) -> str: ...
+def get_sequences(
+    fasta_file: FastaFile
+) -> Dict[str, Union[ProteinSequence, NucleotideSequence]]: ...
 
+def set_sequences(
+    fasta_file: FastaFile,
+    sequence_dict: Dict[str, Union[ProteinSequence, NucleotideSequence]]
+) -> None: ...
 
 def get_alignment(
     fasta_file: FastaFile,
     additional_gap_chars: Tuple[str] = ('_',)
 ) -> Alignment: ...
 
-
-def get_sequence(
-    fasta_file: FastaFile,
-    header: None = None
-) -> Union[ProteinSequence, NucleotideSequence]: ...
-
-
-def get_sequences(
-    fasta_file: FastaFile
-) -> Dict[str, Union[ProteinSequence, NucleotideSequence]]: ...
-
-
 def set_alignment(
     fasta_file: FastaFile,
     alignment: Alignment,
-    seq_names: List[str]
-) -> None: ...
-
-
-def set_sequence(
-    fasta_file: FastaFile,
-    sequence: NucleotideSequence,
-    header: None = None
-) -> None: ...
-
-
-def set_sequences(
-    fasta_file: FastaFile,
-    sequence_dict: Dict[str, NucleotideSequence]
+    seq_names: Iterable[str]
 ) -> None: ...
