b88dcb7b726ab05d116225ad090f65cb32166024
padix-key <patrick.kunzm@gmail.com>
2018-08-17 14:25:41 +0200

    Ignored messages in typecheck script

--- typecheck.py
+++ typecheck.py
@@ -3,6 +3,7 @@
 # information.
 
 import glob
+import re
 import mypy.api as mypy
 
 def module_from_file(file_name):
@@ -23,6 +24,10 @@ def print_errors(err_dict):
         print()
 
 
+ignore = [
+    "Name '.*' already defined \(by an import\)"
+]
+ignore_patterns = [re.compile(e) for e in ignore]
 py_files = glob.glob("src/biotite/**/*.py", recursive=True)
 pyi_files = glob.glob("src/biotite/**/*.pyi", recursive=True)
 py_dict = {}
@@ -40,14 +45,20 @@ for file in py_files:
             is_stub = (".pyi" in file_name)
             module = module_from_file(file_name)
             if err_type == "error":
-                if is_stub:
-                    err_dict = pyi_dict
-                else:
-                    err_dict = py_dict
-                if module not in err_dict:
-                    err_dict[module] = []
-                if (line, msg) not in err_dict[module]:
-                    err_dict[module].append((line, msg))
+                is_ignored = False
+                for pattern in ignore_patterns:
+                    if pattern.match(msg) is not None:
+                        is_ignored = True
+                        break
+                if not is_ignored:
+                    if is_stub:
+                        err_dict = pyi_dict
+                    else:
+                        err_dict = py_dict
+                    if module not in err_dict:
+                        err_dict[module] = []
+                    if (line, msg) not in err_dict[module]:
+                        err_dict[module].append((line, msg))
 
 
 print("Code:")
