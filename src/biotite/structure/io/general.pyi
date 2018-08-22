b6b2ce83d14bab97becfb6e5e80d953d4b72c60a
padix-key <patrick.kunzm@gmail.com>
2018-08-22 13:53:23 +0200

    Added type hint

--- src/biotite/structure/io/general.pyi
+++ src/biotite/structure/io/general.pyi
@@ -2,13 +2,13 @@
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
-from typing import Optional, Union
+from typing import Optional, Union, TextIO, BinaryIO
 from .atoms import AtomArrayStack, AtomArray
 
 
 def load_structure(
     file_path: str,
     template: Optional[Union[AtomArrayStack, AtomArray]] = None
-) -> Union[AtomArray, AtomArrayStack]: ...
+) -> Union[AtomArray, AtomArrayStack, TextIO, BinaryIO, str]: ...
 
 def save_structure(file_path: str, array: AtomArrayStack) -> None: ...
