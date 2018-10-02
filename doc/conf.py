f04140adf173d623341175f20ed6c1877df4a3ea
padix-key <patrick.kunzm@gmail.com>
2018-10-02 13:57:30 +0200

    Removed GitHub 'Watch' button

--- doc/conf.py
+++ doc/conf.py
@@ -91,6 +91,7 @@ html_theme_options = {
     "github_user"   : "biotite-dev",
     "github_repo"   : "biotite",
     "github_banner" : "true",
+    "github_button" : "false",
     "page_width"    : "85%",
     "fixed_sidebar" : "true",
     
@@ -100,6 +101,7 @@ html_theme_options = {
 sphinx_gallery_conf = {
     "examples_dirs"             : "examples/scripts",
     "gallery_dirs"              : "examples/gallery",
+    # Do not run example scripts with a trailing '_noexec'
     "filename_pattern"          : "^((?!_noexec).)*$",
     "backreferences_dir"        : False,
     "download_section_examples" : False,
