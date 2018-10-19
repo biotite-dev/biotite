a6a5dfff77f35e44605c6f1e69b562187af160f0
padix-key <patrick.kunzm@gmail.com>
2018-10-19 17:15:02 +0200

    Updated example

--- doc/examples/scripts/sequence/blosum_dendrogram.py
+++ doc/examples/scripts/sequence/blosum_dendrogram.py
@@ -2,6 +2,9 @@
 Dendrogram of the BLOSUM62 matrix
 =================================
 
+In this example a dendrogram is created, that displays the similarity
+of amino acids in the *BLOSUM62* substitution matrix.
+The amino acids are clustered with the UPGMA method.
 """
 
 # Code source: Patrick Kunzmann
@@ -14,13 +17,15 @@ import biotite.sequence.align as align
 import biotite.sequence.phylo as phylo
 import biotite.sequence.graphics as graphics
 
+# Obtain BLOSUM62
 matrix = align.SubstitutionMatrix.std_protein_matrix()
 print(matrix)
 
 ########################################################################
-#
-#
-#
+# The original *BLOSUM62* contains symbols for ambiguous amino acids and
+# the stop signal.
+# As these are not actual amino acids, a new substitution matrix is
+# created, where these symbols are are removed.
 
 # Matrix should not contain ambiguous symbols or stop signal
 matrix = align.SubstitutionMatrix(
@@ -32,17 +37,13 @@ similarities = matrix.score_matrix()
 print(matrix)
 
 ########################################################################
+# Now a function must be defined, that converts the similarity depicted
+# by a substitution matrix into a distance required by the UPGMA method.
+# In this case, the distance is defined as the difference between the
+# similarity of the two symbols and their average maximum similarity.
 #
-#
-#
-
+# Finally the obtained (phylogenetic) tree is plotted as dendrogram.
 def get_distance(similarities, i, j):
-    """
-    s_max = (similarities[i,i] + similarities[j,j]) / 2
-    s_rand = (np.mean(similarities[i,:]) + np.mean(similarities[j,:])) / 2
-    print(similarities[i,j] - s_rand)
-    return -np.log((similarities[i,j] - s_rand) / (s_max - s_rand))
-    """
     s_max = (similarities[i,i] + similarities[j,j]) / 2
     return s_max - similarities[i,j]
 
@@ -55,8 +56,11 @@ tree = phylo.upgma(distances)
 
 fig = plt.figure(figsize=(8.0, 5.0))
 ax = fig.add_subplot(111)
+# Use the 3-letter amino acid code aa label
+labels = [seq.ProteinSequence.convert_letter_1to3(letter).capitalize()
+          for letter in matrix.get_alphabet1()]
 graphics.plot_dendrogram(
-    ax, tree, orientation="top", labels=matrix.get_alphabet1().get_symbols()
+    ax, tree, orientation="top", labels=labels
 )
 ax.set_ylabel("Distance")
 # Add grid for clearer distance perception
