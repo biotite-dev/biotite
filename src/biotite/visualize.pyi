685ada66331a231a49955ca1138ce358a38539ca
padix-key <patrick.kunzm@gmail.com>
2018-10-22 13:02:59 +0200

    Added type annotations

--- src/biotite/visualize.pyi
+++ src/biotite/visualize.pyi
@@ -2,16 +2,36 @@
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
-from typing import Dict, Tuple
-from abc import abstractmethod
-from matplotlib.figure import Figure
+from typing import NewType, Optional
+try:
+    from matplotlib.patches import FancyArrow
+    from matplotlib.text import Text
+except ImportError:
+    FancyArrow = NewType("FancyArrow", object)
+    Text = NewType("Text", object)
 
-class Visualizer():
-    def __init__(self) -> None: ...
-    def create_figure(
-        self, size: Tuple[int, int], dpi: int = 100
-    ) -> Figure: ...
-    @abstractmethod
-    def generate(self) -> Figure: ...
+
+def set_font_size_in_coord(
+    text: Text,
+    width: Optional[float] = None,
+    height: Optional[float] = None,
+    mode: str = "unlocked"
+) -> None:
+
+
+class AdaptiveFancyArrow(FancyArrow):
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        dx: float,
+        dy: float,
+        tail_width: float,
+        head_width: float,
+        head_ratio: float,
+        draw_head: bool = True,
+        shape: str = "full",
+        **kwargs
+    ) -> None: ...
 
 colors: Dict[str, str]
