fe0a85d041da2583e3be49f6a00d0628dc19a59e
padix-key <patrick.kunzm@gmail.com>
2018-10-29 13:09:39 +0100

    Added parameters for MUSCLE alignments

--- src/biotite/application/muscle/app.py
+++ src/biotite/application/muscle/app.py
@@ -5,11 +5,12 @@
 __author__ = "Patrick Kunzmann"
 __all__ = ["MuscleApp"]
 
+from ...temp import temp_file
 from ..msaapp import MSAApp
 from ..application import AppState, requires_state
 from ...sequence.sequence import Sequence
 from ...sequence.seqtypes import NucleotideSequence, ProteinSequence
-from ...sequence.io.fasta.file import FastaFile
+from ...sequence.align.matrix import SubstitutionMatrix
 from ...sequence.align.alignment import Alignment
 
 
@@ -27,11 +28,87 @@ class MuscleApp(MSAApp):
     
     def __init__(self, sequences, bin_path=None):
         super().__init__(sequences, bin_path)
+        self._matrix_file_name = None
+        self._matrix = None
+        self._gap_open = None
+        self._gap_ext = None
+        self._terminal_penalty = None
+    
+    @requires_state(AppState.CREATED)
+    def set_matrix(self, matrix):
+        self._matrix = matrix
+        self._matrix_file_name = temp_file("mat")
+    
+    @requires_state(AppState.CREATED)
+    def set_gap_penalty(self, gap_penalty):
+         # Check if gap penalty is general or affine
+        if type(gap_penalty) == int:
+            if gap_penalty > 0:
+                raise ValueError("Gap penalty must be negative")
+            self._gap_open = gap_penalty
+            self._gap_ext= gap_penalty
+        elif type(gap_penalty) == tuple:
+            if gap_penalty[0] > 0 or gap_penalty[1] > 0:
+                    raise ValueError("Gap penalty must be negative")
+            self._gap_open = gap_penalty[0]
+            self._gap_ext = gap_penalty[1]
+        else:
+            raise TypeError("Gap penalty must be either float or tuple")
     
     @staticmethod
     def get_default_bin_path():
         return "muscle"
     
     def get_cli_arguments(self):
-        return ["-in", self.get_input_file_path(),
-                "-out", self.get_output_file_path()]
+        args = [
+            "-in",  self.get_input_file_path(),
+            "-out", self.get_output_file_path()
+        ]
+        if self._matrix is not None:
+            args += ["-matrix", self._matrix_file_name]
+        if self._gap_open is not None and self._gap_ext is not None:
+            args += ["-gapopen",   f"{self._gap_open:.1f}"]
+            args += ["-gapextend", f"{self._gap_ext:.1f}"]
+            # When the gap penalty is set,
+            # use the penalty also for hydrophobic regions
+            args += ["-hydrofactor", "1.0"]
+            # Use the recommendation of the documentation
+            args += ["-center", "0.0"]
+        return args
+    
+    def run(self):
+        if self._matrix is not None:
+            with open(self._matrix_file_name, "w") as file:
+                file.write(str(self._matrix))
+        super().run()
+    
+    @classmethod
+    def align(cls, sequences, bin_path=None, matrix=None,
+              gap_penalty=None):
+        """
+        Perform a multiple sequence alignment.
+        
+        This is a convenience function, that wraps the `MSAApp`
+        execution.
+        
+        Parameters
+        ----------
+        sequences : iterable object of Sequence
+            The sequences to be aligned
+        bin_path : str, optional
+            Path of the MSA software binary. By default, the default path
+            will be used.
+        
+        Returns
+        -------
+        alignment : Alignment
+            The global multiple sequence alignment.
+        """
+        app = cls(sequences, bin_path)
+        if matrix is not None:
+            app.set_matrix(matrix)
+        if gap_penalty is not None:
+            app.set_gap_penalty(gap_penalty)
+        app.start()
+        app.join()
+        return app.get_alignment()
