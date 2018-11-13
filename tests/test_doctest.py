6db356404f918983bd7ee4d87f35fbae8d316c5d
padix-key <patrick.kunzm@gmail.com>
2018-11-13 13:54:14 +0100

    Added globals, added option flags for doctests

--- tests/test_doctest.py
+++ tests/test_doctest.py
@@ -8,6 +8,7 @@ import pkgutil
 import doctest
 import os.path
 import numpy as np
+import matplotlib.pyplot as plt
 from importlib import import_module
 import pytest
 import biotite
@@ -15,28 +16,29 @@ import biotite.structure.io as strucio
 
 
 @pytest.mark.parametrize("package_name, context_package_names", [
-    ("biotite",                      []                   ),
-    ("biotite.sequence",             []                   ),
-    ("biotite.sequence.align",       ["biotite.sequence"] ),
-    ("biotite.sequence.phylo",       ["biotite.sequence"] ),
-    ("biotite.sequence.graphics",    ["biotite.sequence"] ),
-    ("biotite.structure.io",         ["biotite.sequence"] ),
-    ("biotite.structure.io.fasta",   ["biotite.sequence"] ),
-    ("biotite.structure.io.genbank", ["biotite.sequence"] ),
-    ("biotite.structure",            []                   ),
-    ("biotite.structure.io",         ["biotite.structure"]),
-    ("biotite.structure.io.pdb",     ["biotite.structure"]),
-    ("biotite.structure.io.pdbx",    ["biotite.structure"]),
-    ("biotite.structure.io.npz",     ["biotite.structure"]),
-    ("biotite.structure.io.mmtf",    ["biotite.structure"]),
-    ("biotite.database.entrez",      []                   ),
-    ("biotite.database.rcsb",        []                   ),
-    ("biotite.application",          []                   ),
-    ("biotite.application.blast",    []                   ),
-    ("biotite.application.muscle",   []                   ),
-    ("biotite.application.clustalo", []                   ),
-    ("biotite.application.mafft",    []                   ),
-    ("biotite.application.dssp",     []                   ),
+    ("biotite",                      []                          ),
+    ("biotite.sequence",             []                          ),
+    ("biotite.sequence.align",       ["biotite.sequence"]        ),
+    ("biotite.sequence.phylo",       ["biotite.sequence"]        ),
+    ("biotite.sequence.graphics",    ["biotite.sequence"]        ),
+    ("biotite.sequence.io",          ["biotite.sequence"]        ),
+    ("biotite.sequence.io.fasta",    ["biotite.sequence"]        ),
+    ("biotite.sequence.io.genbank",  ["biotite.sequence",
+                                      "biotite.database.entrez"] ),
+    ("biotite.structure",            []                          ),
+    ("biotite.structure.io",         ["biotite.structure"]       ),
+    ("biotite.structure.io.pdb",     ["biotite.structure"]       ),
+    ("biotite.structure.io.pdbx",    ["biotite.structure"]       ),
+    ("biotite.structure.io.npz",     ["biotite.structure"]       ),
+    ("biotite.structure.io.mmtf",    ["biotite.structure"]       ),
+    ("biotite.database.entrez",      []                          ),
+    ("biotite.database.rcsb",        []                          ),
+    ("biotite.application",          []                          ),
+    ("biotite.application.blast",    []                          ),
+    ("biotite.application.muscle",   []                          ),
+    ("biotite.application.clustalo", []                          ),
+    ("biotite.application.mafft",    []                          ),
+    ("biotite.application.dssp",     []                          ),
 ])
 def test_doctest(package_name, context_package_names):
     """
@@ -55,10 +57,12 @@ def test_doctest(package_name, context_package_names):
         attrs = mod.__all__
         globs.update({attr : getattr(mod, attr) for attr in attrs})
     # Add fixed names for certain paths
-    globs["path_to_directory"] = biotite.temp_dir()
+    globs["path_to_directory"]  = biotite.temp_dir()
     globs["path_to_structures"] = "./tests/structure/data/"
+    globs["path_to_sequences"]  = "./tests/sequence/data/"
     # Add frequently used modules
-    globs["np"] = np
+    globs["np"]  = np
+    globs["plt"] = plt
     # Add frequently used objects
     globs["atom_array_stack"] = strucio.load_structure(
         "./tests/structure/data/1l2y.mmtf"
@@ -72,6 +76,7 @@ def test_doctest(package_name, context_package_names):
         mod = import_module(modname)
         results = doctest.testmod(
             mod, extraglobs=globs,
+            optionflags=doctest.ELLIPSIS | doctest.REPORT_ONLY_FIRST_FAILURE,
             verbose=False, report=False
         )
         try:
