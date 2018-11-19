249b8de369e598e90ffbb719daf2159fb3a6c89b
padix-key <patrick.kunzm@gmail.com>
2018-11-19 13:51:07 +0100

    Added 'MutableMapping' as base class

--- src/biotite/sequence/io/fastq/file.py
+++ src/biotite/sequence/io/fastq/file.py
@@ -4,15 +4,17 @@
 
 __author__ = "Patrick Kunzmann"
 
-from ...seqtypes import NucleotideSequence
-from ....file import TextFile, InvalidFileError
 import textwrap
 from collections import OrderedDict
+from collections.abc import MutableMapping
+import numpy as np
+from ...seqtypes import NucleotideSequence
+from ....file import TextFile, InvalidFileError
 
-__all__ = ["FastaFile"]
+__all__ = ["FastqFile"]
 
 
-class FastaFile(TextFile):
+class FastqFile(TextFile, MutableMapping):
     
     def __init__(self, offset, chars_per_line=None):
         super().__init__()
@@ -23,7 +25,7 @@ class FastaFile(TextFile):
     def read(self, file):
         super().read(file)
         # Remove leading and trailing whitespace in all lines
-        self.lines = [line for line in self.lines]
+        self.lines = [line.strip() for line in self.lines]
         # Filter out empty lines
         self.lines = [line for line in self.lines if len(line) != 0]
         self._find_entries()
@@ -36,18 +38,16 @@ class FastaFile(TextFile):
             )
         if not isinstance(sequence, NucleotideSequence):
             raise IndexError("Can only set 'NucleotideSequence' objects")
-        # Delete lines of entry corresponding to the header,
-        # if existing
-        if header in self._entries:
-            start, stop = self._entries[header]
-            del self.lines[start:stop]
-            del self._entries[header]
+        # Delete lines of entry corresponding to the identifier,
+        # if already existing
+        if identifier in self:
+            del self[identifier]
         # Append identifier line
         self.lines += ["@" + identifier.replace("\n","").strip()]
         # Append new lines with sequence string (with line breaks)
         if self._chars_per_line is None:
             self.lines += str(sequence)
-        else
+        else:
             self.lines += textwrap.wrap(
                 str(sequence), width=self._chars_per_line
             )
@@ -57,7 +57,7 @@ class FastaFile(TextFile):
         score_chars = scores.tobytes().decode("ascii")
         if self._chars_per_line is None:
             self.lines += score_chars
-        else
+        else:
             self.lines += textwrap.wrap(
                 score_chars, width=self._chars_per_line
             )
@@ -73,11 +73,11 @@ class FastaFile(TextFile):
         # Concatenate sequence string from the sequence and score lines,
         # respectively
         sequence = NucleotideSequence("".join(
-            self.lines[seq_start+1 : seq_stop]
+            self.lines[seq_start : seq_stop]
         ))
         scores = np.fromstring("".join(
-            self.lines[score_start+1 : score_stop]
-        ), dtype=np.uint8)
+            self.lines[score_start : score_stop]
+        ), dtype=np.int8)
         scores -= self._offset
         return sequence, scores
     
@@ -93,6 +93,9 @@ class FastaFile(TextFile):
     
     def __iter__(self):
         return self._entries.__iter__()
+    
+    def __contains__(self, identifer):
+        return identifer in self._entries
     
     def _find_entries(self):
         self._entries = OrderedDict()
@@ -132,13 +135,14 @@ class FastaFile(TextFile):
                 score_len += len(line)
                 if score_len < seq_len:
                     # Scores have not ended yet
-                elif score_len == seq_len
+                    pass
+                elif score_len == seq_len:
                     # End of scores
                     # -> End of entry
-                    score_stop_i = i
+                    score_stop_i = i + 1
                     in_scores = False
                     # Record this entry
-                    self._entries[identifer] = (
+                    self._entries[identifier] = (
                         seq_start_i, seq_stop_i, score_start_i, score_stop_i
                     )
                 else: # score_len > seq_len
@@ -148,4 +152,8 @@ class FastaFile(TextFile):
                     )
             else:
                 raise InvalidFileError(f"Line {i+1} in FASTQ file is invalid")
+        # At the end of the file, the last sequence or score block
+        # must have properly ended
+        if in_sequence or in_scores:
+            raise InvalidFileError("The last entry in the file is incomplete")
     
