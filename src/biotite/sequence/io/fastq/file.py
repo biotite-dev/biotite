41f6ac9f0f11950dd37cf62c33f0b1d57664d0d2
padix-key <patrick.kunzm@gmail.com>
2018-11-20 12:54:10 +0100

    Added docstrings

--- src/biotite/sequence/io/fastq/file.py
+++ src/biotite/sequence/io/fastq/file.py
@@ -4,6 +4,7 @@
 
 __author__ = "Patrick Kunzmann"
 
+from numbers import Integral
 import textwrap
 from collections import OrderedDict
 from collections.abc import MutableMapping
@@ -14,13 +15,94 @@ from ....file import TextFile, InvalidFileError
 __all__ = ["FastqFile"]
 
 
+_OFFSETS = {
+    "Sanger"       : 33,
+    "Solexa"       : 64,
+    "Illumina-1.3" : 64,
+    "Illumina-1.5" : 64,
+    "Illumina-1.8" : 33,
+}
+
+
 class FastqFile(TextFile, MutableMapping):
+    """
+    This class represents a file in FASTQ format.
+
+    A FASTQ file stores one or multiple sequences along with
+    sequencing quality scores.
+    Each sequence is associated with an identifer string,
+    beginning with an ``@``.
+
+    The quality scores are encoded as ASCII characters,
+    with each actual score being the ASCII code subtracted by an
+    `offset` value.
+    The offset is format dependent.
+    As the offset is not reliably deducible from the file contets, it
+    must be provided explicitly, either as number or format
+    (e.g. ``'Illumina-1.8'``).
+
+    Similar to the `FastaFile` class, this class implements the
+    `MutableMapping` interface:
+    An identifier string (without the leading ``@``) is used as index
+    to get and set the corresponding sequence and quality.
+    `del` removes an entry in the file.
+    
+    Parameters
+    ----------
+    offset : int or {'Sanger', 'Solexa', 'Illumina-1.3', 'Illumina-1.5', 'Illumina-1.8'}
+        This value that is added to the score to obtain the ASCII code.
+        Can either directly the value, or a string that indicates
+        the score format.
+    chars_per_line : int, optional
+        The number characters in a line containing sequence data
+        after which a line break is inserted.
+        Only relevant, when adding sequences to a file.
+        By default each sequence (and score string)
+        is put into one line.
+    
+    Examples
+    --------
+    
+    >>> import os.path
+    >>> file = FastqFile(offset="Sanger")
+    >>> file["seq1"] = NucleotideSequence("ATACT"), [0,3,10,7,12]
+    >>> file["seq2"] = NucleotideSequence("TTGTAGG"), [15,13,24,21,28,38,35]
+    >>> print(file)
+    @seq1
+    ATACT
+    +
+    !$+(-
+    @seq2
+    TTGTAGG
+    +
+    0.96=GD
+    >>> sequence, scores = file["seq1"]
+    >>> print(sequence)
+    ATACT
+    >>> print(scores)
+    [ 0  3 10  7 12]
+    >>> del file["seq1"]
+    >>> print(file)
+    @seq2
+    TTGTAGG
+    +
+    0.96=GD
+    >>> file.write(os.path.join(path_to_directory, "test.fastq"))
+    """
     
     def __init__(self, offset, chars_per_line=None):
         super().__init__()
         self._chars_per_line = chars_per_line
         self._entries = OrderedDict()
-        self._offset = offset
+        if isinstance(offset, Integral):
+            self._offset = offset
+        elif isinstance(offset, str):
+            self._offset = _OFFSETS[offset]
+        else:
+            raise TypeError(
+                f"The offset must be either an integer or a string "
+                f"indicating the format, not {type(offset).__name__}"
+            )
     
     def read(self, file):
         super().read(file)
@@ -31,6 +113,19 @@ class FastqFile(TextFile, MutableMapping):
         self._find_entries()
     
     def get_sequence(self, identifier):
+        """
+        Get the sequence for the specified identifier.
+
+        Parameters
+        ----------
+        identifier : str
+            The identifier of the sequence.
+        
+        Returns
+        -------
+        sequence : NucleotideSequence
+            The sequence corresponding to the identifier.
+        """
         if not isinstance(identifier, str):
             raise IndexError(
                 "'FastqFile' only supports identifier strings as keys"
@@ -44,6 +139,19 @@ class FastqFile(TextFile, MutableMapping):
         return sequence
     
     def get_quality(self, identifier):
+        """
+        Get the quality scores for the specified identifier.
+
+        Parameters
+        ----------
+        identifier : str
+            The identifier of the quality scores.
+        
+        Returns
+        -------
+        sequence : NucleotideSequence
+            The quality scores corresponding to the identifier.
+        """
         if not isinstance(identifier, str):
             raise IndexError(
                 "'FastqFile' only supports identifier strings as keys"
@@ -62,6 +170,11 @@ class FastqFile(TextFile, MutableMapping):
         
     def __setitem__(self, identifier, item):
         sequence, scores = item
+        if len(sequence) != len(scores):
+            raise ValueError(
+                f"Sequence has length {len(sequence)}, "
+                f"but score length is {len(scores)}"
+            )
         if not isinstance(identifier, str):
             raise IndexError(
                 "'FastqFile' only supports header strings as keys"
@@ -84,6 +197,8 @@ class FastqFile(TextFile, MutableMapping):
         # Append sequence-score separator
         self.lines += ["+"]
         # Append scores
+        if not isinstance(scores, np.ndarray):
+            scores = np.array(scores)
         scores = scores + self._offset
         score_chars = scores.astype(np.int8, copy=False) \
                             .tobytes() \
@@ -174,4 +289,3 @@ class FastqFile(TextFile, MutableMapping):
         # must have properly ended
         if in_sequence or in_scores:
             raise InvalidFileError("The last entry in the file is incomplete")
-    
