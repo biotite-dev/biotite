f4b069c2857caa26615e2ae5b7815232fe79ea53
padix-key <patrick.kunzm@gmail.com>
2019-01-23 17:15:02 +0100

    Transferred docstring

--- src/biotite/structure/info/masses.py
+++ src/biotite/structure/info/masses.py
@@ -21,6 +21,31 @@ with open(join(_info_dir, "residue_masses.json")) as file:
     _res_masses = json.load(file)
 
 def mass(item, is_residue=None):
+    """
+    Calculate the mass for the given object. [1]_
+    
+    Parameters
+    ----------
+    item : str or Atom or AtomArray or AtomArrayStack
+        [description]
+    is_residue : bool, optional
+        [description] (the default is None, which [default_description])
+    
+    Returns
+    -------
+    mass : float
+        The mass of the given object.
+    
+    References
+    ----------
+
+    .. [1] J Meija, TB Coplen, M Berglund, WA Brand, P De Bièvre,
+        M Gröning, NE Holden, J Irrgeher, RD Loss, T Walczyk
+        and T Prohaska
+        "Atomic weights of the elements 2013 (IUPAC Technical Report)."
+        Pure Appl Chem, 88, 265-291 (2016).
+    """
+
     if isinstance(item, str):
         if is_residue is None:
             result_mass = _atom_masses.get(item.upper())
