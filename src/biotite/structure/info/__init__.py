8ba03b67947d5045bf3fe61a822039309a86163b
padix-key <patrick.kunzm@gmail.com>
2019-01-23 17:15:31 +0100

    Removed docstring

--- src/biotite/structure/info/__init__.py
+++ src/biotite/structure/info/__init__.py
@@ -5,21 +5,6 @@
 """
 A subpackage for obtaining all kinds of chemical information of atoms
 and residues.
-
-Attributes
-----------
-atom_masses : dict of str to float
-    A dictionary that maps an element name (upper case) to an atom
-    mass [1]_.
-
-References
-----------
-
-.. [1] J Meija, TB Coplen, M Berglund, WA Brand, P De Bièvre,
-    M Gröning, NE Holden, J Irrgeher, RD Loss, T Walczyk
-    and T Prohaska
-    "Atomic weights of the elements 2013 (IUPAC Technical Report)."
-    Pure Appl Chem, 88, 265-291 (2016).
 """
 
 __author__ = "Patrick Kunzmann"
