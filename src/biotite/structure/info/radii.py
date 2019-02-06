f7958b574b4ba876a6a21f7e0d3d9f7025b51af1
padix-key <patrick.kunzm@gmail.com>
2019-02-06 14:41:07 +0100

    Added comments

--- src/biotite/structure/info/radii.py
+++ src/biotite/structure/info/radii.py
@@ -46,33 +46,47 @@ _SINGLE_RADII = {
     "I":  1.98
 }
 
-# A precalculated dictionary of radii for each residue
+# A dictionary that caches radii for each residue
 _protor_radii = {}
 
 
 def vdw_radius_protor(res_name, atom_name):
     res_name = res_name.upper()
     if res_name in _protor_radii:
+        # Use cached radii for the residue, if already calculated
         return _protor_radii[res_name][atom_name]
     else:
+        # Otherwise calculate radii for the given residue
         _protor_radii[res_name] = _calculate_protor_radii(res_name)
         return _protor_radii[res_name][atom_name]
 
 def _calculate_protor_radii(res_name):
+    """
+    Calculate the ProtOr VdW radii for all atoms (atom names) in
+    a residue.
+    """
     bonds = get_bonds_for_residue(res_name)
-    # Element, valency, H count
+    # Maps atom names to a ProtOr group
+    # -> tuple(element, valency, H count)
+    # Based on the group the radius is chosen from _PROTOR_RADII
     groups = {}
     for atom1, atom2 in bonds:
+        # Process each bond two times:
+        # One time the first atom is the one to get valency and H count
+        # for and the other time vice versa
         for main_atom, bound_atom in ((atom1, atom2), (atom2, atom1)):
+            # Only for these atoms ProtOr radii exist
             if main_atom[0] not in ["C", "N", "O", "S"]:
                 continue
-            group = groups.get(main_atom)
-            if group is None:
-                group = [main_atom[0], 0, 0]
+            # Update existing entry if already existing
+            group = groups.get(main_atom, [main_atom[0], 0, 0])
+            # Increase valency by one, since the bond entry exists
             group[1] += 1
+            # If the atom is bonded to hydrogen, increase H count
             if bound_atom[0] == "H":
                 group[2] += 1
             groups[main_atom] = group
+    # Get radii based on ProtOr groups
     radii = {atom : _PROTOR_RADII.get(tuple(group), _PROTOR_DEFAULT)
              for atom, group in groups.items()}
     return radii
