69313e9ddf1defb0189414e9921bbc1844f02d1c
Daniel Bauer <bauer@cbs.tu-darmstadt.de>
2019-02-11 13:02:18 +0100

    RDF unit tests

--- tests/structure/test_rdf.py
+++ tests/structure/test_rdf.py
@@ -7,10 +7,12 @@ import pytest
 import numpy as np
 import itertools
 
+TEST_FILE = join(data_dir, "waterbox.gro")
+
 
 def test_rdf():
     """ General test to reproduce oxygen RDF for a box of water"""
-    test_file = join(data_dir, "waterbox.pdb")
+    test_file = TEST_FILE
     stack = load_structure(test_file)
 
     # calculate oxygen RDF for water
@@ -22,7 +24,7 @@ def test_rdf():
 
     # Compare with MDTraj
     import mdtraj
-    traj = mdtraj.load(test_file)
+    traj = mdtraj.load(TEST_FILE)
     ow = [a.index for a in traj.topology.atoms if a.name == 'O']
     pairs = itertools.product([ow[0]], ow[1:])
     mdt_bins, mdt_g_r = mdtraj.compute_rdf(traj, list(pairs),
@@ -30,11 +32,12 @@ def test_rdf():
                                            periodic=False)
 
     assert np.allclose(bins, mdt_bins*10)
-    assert np.allclose(g_r, mdt_g_r, rtol=0.02)
+    assert np.allclose(g_r, mdt_g_r, rtol=0.0001)
+
 
 def test_rdf_bins():
     """ Test if RDF produce correct bin ranges """
-    stack = load_structure(join(data_dir, "waterbox.pdb"))
+    stack = load_structure(TEST_FILE)
     center = stack[:, 0]
     num_bins = 44
     bin_range = (0, 11.7)
@@ -44,11 +47,9 @@ def test_rdf_bins():
     assert(bins[1] < bin_range[1])
 
 
-# @pytest.mark.skip(reason="not implemented yet")
 def test_rdf_with_selection():
-    """ Test if the selection argument of rdf function works correctly """
-    test_file = join(data_dir, "waterbox.pdb")
-    stack = load_structure(test_file)
+    """ Test if the selection argument of rdf function works as expected """
+    stack = load_structure(TEST_FILE)
 
     # calculate oxygen RDF for water with and without a selection
     oxygen = stack[:, stack.atom_name == 'OW']
@@ -67,8 +68,7 @@ def test_rdf_with_selection():
 
 def test_rdf_atom_argument():
     """ Test if the first argument allows to use AtomArrayStack """
-    test_file = join(data_dir, "waterbox.pdb")
-    stack = load_structure(test_file)
+    stack = load_structure(TEST_FILE)
 
     # calculate oxygen RDF for water with and without a selection
     oxygen = stack[:, stack.atom_name == 'OW']
@@ -85,32 +85,57 @@ def test_rdf_atom_argument():
     assert np.allclose(g_r, nosel_g_r)
 
 
-@pytest.mark.skip(reason="not implemented yet")
 def test_rdf_multiple_center():
     """ Test if the first argument allows to use multiple centers"""
-    test_file = join(data_dir, "waterbox.pdb")
-    stack = load_structure(test_file)
+    stack = load_structure(TEST_FILE)
 
     # calculate oxygen RDF for water with and without a selection
     oxygen = stack[:, stack.atom_name == 'OW']
     interval = np.array([0, 10])
     n_bins = 100
 
-    # assert no error is raised
+    # averaging individual calculations
+    bins1, g_r1 = rdf(oxygen[:, 1].coord, oxygen, interval=interval,
+                    bins=n_bins, periodic=False)
+    bins2, g_r2 = rdf(oxygen[:, 0].coord, oxygen, interval=interval,
+                    bins=n_bins, periodic=False)
+    mean = np.mean([g_r1, g_r2], axis=0)
+
+    # this should give the same result as averaging for oxygen 0 and 1
     bins, g_r = rdf(oxygen[:, 0:2].coord, oxygen, interval=interval,
                     bins=n_bins, periodic=False)
 
+    assert np.allclose(g_r, mean, rtol=0.0001)
 
-@pytest.mark.skip(reason="not implemented yet")
+
 def test_rdf_periodic():
     """ Test if the periodic argument gives the correct results"""
-    # TODO implement
-    assert(False)
+    test_file = TEST_FILE
+    stack = load_structure(test_file)
+
+    # calculate oxygen RDF for water
+    oxygen = stack[:, stack.atom_name == 'OW']
+    interval = np.array([0, 10])
+    n_bins = 100
+    bins, g_r = rdf(oxygen[:, 0].coord, oxygen[:, 1:], interval=interval,
+                    bins=n_bins, periodic=True)
+
+    # Compare with MDTraj
+    import mdtraj
+    traj = mdtraj.load(TEST_FILE)
+    ow = [a.index for a in traj.topology.atoms if a.name == 'O']
+    pairs = itertools.product([ow[0]], ow[1:])
+    mdt_bins, mdt_g_r = mdtraj.compute_rdf(traj, list(pairs),
+                                           r_range=interval/10, n_bins=n_bins,
+                                           periodic=True)
+
+    assert np.allclose(bins, mdt_bins*10)
+    assert np.allclose(g_r, mdt_g_r, rtol=0.0001)
 
 
 def test_rdf_box():
     """ Test correct use of simulation boxes """
-    stack = load_structure(join(data_dir, "waterbox.pdb"))
+    stack = load_structure(TEST_FILE)
     box = vectors_from_unitcell(1, 1, 1, 90, 90, 90)
     box = np.repeat(box[np.newaxis, :, :], len(stack), axis=0)
 
@@ -128,4 +153,20 @@ def test_rdf_box():
     # Fail if box is of wrong size
     with pytest.raises(ValueError):
         rdf(stack[:, 0], stack, box=box[0])
+
+
+def test_rdf_normalized():
+    """ Assert that the RDF tail is normalized to 1"""
+    test_file = TEST_FILE
+    stack = load_structure(test_file)
+
+    # calculate oxygen RDF for water
+    oxygen = stack[:, stack.atom_name == 'OW']
+    interval = np.array([0, 5])
+    n_bins = 100
+
+    bins, g_r = rdf(oxygen[:, 0].coord, oxygen[:, 1:], interval=interval,
+                    bins=n_bins, periodic=True)
+
+    assert np.allclose(g_r, np.ones(n_bins), atol=0.1)
 
