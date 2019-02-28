5a72c6035721a6f14c6639f2b2d2ca6928fe5ada
padix-key <patrick.kunzm@gmail.com>
2019-02-28 14:37:55 +0100

    Removed support for type annotations

--- src/biotite/structure/io/mmtf/encode.pyi
+++
@@ -1,8 +0,0 @@
-# This source code is part of the Biotite package and is distributed
-# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
-# information.
-
-import numpy as np
-
-
-def encode_array(array: np.ndarray, codec: int, param: int) -> bytes: ...
