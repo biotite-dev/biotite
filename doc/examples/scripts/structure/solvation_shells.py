49f2bd95428cb9eb8797a19855056444a639585a
padix-key <patrick.kunzm@gmail.com>
2019-03-05 11:01:23 +0100

    Refined example

--- doc/examples/scripts/structure/solvation_shells.py
+++ doc/examples/scripts/structure/solvation_shells.py
@@ -1,6 +1,6 @@
 r"""
 Solvation shells of sodium and chloride ions
-===========================================
+============================================
 
 Based on a 10 ns Gromacs MD simulation of 0.15 mM sodium chloride,
 the distance of solvation shells to the central ion for both,
@@ -14,6 +14,12 @@ The trajectory file can be downloaded
 :download:`here </static/assets/download/waterbox_md.xtc>`
 and the template PDB can be downloaded
 :download:`here </static/assets/download/waterbox_md.pdb>`.
+
+Two things are peculiar is this plot:
+At first, the first solvation shell has a smaller distance from chloride
+ions than from sodium ions, although the radius of chlorine is higher.
+Furthermore, the second solvation shell of the chloride ions seems to be
+separated into two distinct peaks.
 """
 
 # Code source: Patrick Kunzmann
@@ -27,8 +33,8 @@ import biotite.structure as struc
 import biotite.structure.io as strucio
 
 # Put here the path of the downloaded files
-templ_file_path = "../../../static/assets/download/waterbox_gmx_md.pdb"
-traj_file_path  = "../../../static/assets/download/waterbox_gmx_md.xtc"
+templ_file_path = "../../../static/assets/download/waterbox_md.pdb"
+traj_file_path  = "../../../static/assets/download/waterbox_md.xtc"
 
 # Load the trajectory
 traj = strucio.load_structure(traj_file_path, template=templ_file_path)
@@ -55,8 +61,7 @@ bins, rdf_cl = struc.rdf(
 )
 
 # Find peaks
-# This is a bit tricky
-# and requires a bit trial and error on the parameters
+# This requires a bit trial and error on the parameters
 # The 'x' in '[x * N_BINS/10]' is the expected peak width in Å,
 # that is transformed into a peak width in amount of values
 peak_indices_na = signal.find_peaks_cwt(
@@ -67,8 +72,11 @@ peak_indices_cl = signal.find_peaks_cwt(
 )
 peak_indices_na, peak_indices_cl = peak_indices_na[:3], peak_indices_cl[:3]
 
-# Plot both RDFs
+# Create plots
 fig, ax = plt.subplots(figsize=(8.0,3.0))
+# Plot average density in box
+ax.axhline(1, color="lightgray", linestyle="--")
+# Plot both RDFs
 ax.plot(bins, rdf_na, color=biotite.colors["darkgreen"], label="Na")
 ax.plot(bins, rdf_cl, color=biotite.colors["dimorange"], label="Cl")
 # The peak positions are shown as vertical lines
@@ -80,10 +88,11 @@ ax.vlines(
     bins[peak_indices_cl], ymin=0, ymax=3,
     color=biotite.colors["dimorange"], linestyle=":"
 )
+ax.set_xticks(np.arange(0, 10.5, 0.5))
 ax.set_xlim(0,10)
 ax.set_ylim(0,2.7)
 ax.set_xlabel("Radius (Å)")
-ax.set_ylabel("Density")
+ax.set_ylabel("Relative density")
 ax.legend()
 
 fig.tight_layout()
