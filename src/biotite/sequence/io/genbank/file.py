d19f367b532bc85d88bdc82a37934c76d7d927ee
Patrick Kunzmann <patrick.kunzm@gmail.com>
2019-05-23 12:21:42 +0200

    Work on new GenBank API

--- src/biotite/sequence/io/genbank/file.py
+++ src/biotite/sequence/io/genbank/file.py
@@ -4,15 +4,198 @@
 
 __author__ = "Patrick Kunzmann"
 
-from ....file import TextFile, InvalidFileError
-from ...annotation import Location, Feature, Annotation, AnnotatedSequence
-from ...seqtypes import NucleotideSequence, ProteinSequence 
 import textwrap
 import copy
 import re
 import io
-
+from collections.abc import MutableSequence
+from ....file import TextFile, InvalidFileError
+from collections import OrderedDict
+from ...annotation import Location, Feature, Annotation, AnnotatedSequence
+from ...seqtypes import NucleotideSequence, ProteinSequence 
 __all__ = ["GenBankFile", "GenPeptFile", "MultiFile"]
+
+
+class GenBankFile(TextFile, MutableSequence):
+
+    def __init__(self):
+        super().__init__()
+        # Add '//' as general terminator of a GenBank file
+        self.lines = ["//"]
+        # Field start and stop indices in list of lines
+        # and names of categories
+        self._field_pos = []
+        self._find_field_indices()
+    
+    def read(self, file):
+        super().read(file)
+        self._find_field_indices()
+    
+    def get_fields(self, name):
+        fields = []
+        for i, (_, _, fname) in enumerate(self._field_pos):
+            if fname == name:
+                _, content, subfields = self[i]
+                fields.append(content, subfields)
+        return fields
+
+    def __getitem__(self, index):
+        start, stop, name = self._field_pos[index]
+        
+        if name in ["FEATURES", "ORIGIN"]:
+            # For those two fields return the complete lines,
+            # beginning with the line after the field name
+            content = self._get_field_content(start+1, stop, indent=0)
+            subfield_dict = OrderedDict()
+        
+        else:
+            # For all metadata fields use the
+            # standard GenBank indentation (=12)
+            # Find subfields
+            subfield_dict = OrderedDict()
+            subfield_start = None
+            first_subfield_start = None
+            for i in range(start+1, stop):
+                line = self.lines[i]
+                # Check if line contains a new subfield
+                # (Header beginning from first column)
+                if len(line) != 0 and line[:12].strip() != "":
+                    if first_subfield_start is None:
+                        first_subfield_start = i
+                    # Store previous subfield
+                    if subfield_start is not None:
+                        subfield_dict[header] = self._get_field_content(
+                            subfield_start, i, indent=12
+                        )
+                    header = line[:12].strip()
+            # Store last subfield
+            if subfield_start is not None:
+                subfield_dict[header] = self._get_field_content(
+                    subfield_start, i, indent=12
+                )
+            # Only include lines in field content,
+            # that are not part of a subfield
+            content = self._get_field_content(
+                start, first_subfield_start, indent=12
+            )
+        
+        return name, content, subfield_dict
+    
+    def __setitem__(self, index, item):
+        if index != len(self):
+            del self[index]
+        self.insert(index, item)
+    
+    def __delitem__(self, index):
+        start, stop, _ = self._field_pos[index]
+        # Shift the start/stop indices of the following fields
+        # by the amount of deleted fields
+        shift = stop - start
+        for i in range(index, len(self._field_pos)):
+            old_start, old_stop, name = self._field_pos[index]
+            self._field_pos[i] = old_start-shift, old_stop-shift, name
+        del self.lines[start : stop]
+        del self._field_pos[index]
+    
+    def __len__(self):
+        return len(self._field_pos)
+
+    def insert(self, index, item):
+        if not isinstance(item, tuple):
+            raise TypeError(
+                "Expected a tuple of name, content and optionally subfields"
+            )
+        if len(item) == 2:
+            name, content = item
+            subfields = None
+        elif len(item) == 3:
+            name, content, subfields = item
+        else:
+            raise TypeError(
+                "Expected a tuple of name, content and optionally subfields"
+            )
+        
+        name = name.strip().upper()
+        if len(name) == 0:
+            raise ValueError(f"Must give a non emtpy name")
+        subfields = OrderedDict({
+            subfield_name.upper().strip() : subfield_lines
+            for subfield_name, subfield_lines in subfields.items()
+        })
+        
+        # Create lines for new field
+        if name == "FEATURES":
+            # Header line plus all actual feature lines
+            inserted_lines = copy.copy(content).insert(
+                0, ["FEATURES" + " "*13 + "Location/Qualifiers"]
+            )
+        elif name == "ORIGIN":
+            # Header line plus all actual sequence lines
+            inserted_lines = copy.copy(content).insert(
+                0, ["ORIGIN"]
+            )
+        else:
+            name_column = []
+            content_column = []
+            # Create a line for the field name and empty lines
+            # for each additional line required by the content 
+            name_column += [name] + [""] * len(content)-1
+            content_column += content
+            for subfield_name, subfield_lines in subfields.items():
+                name_column += ["  " + subfield_name] \
+                               + [""] * len(subfield_lines)-1
+                content_column += subfield_lines
+            inserted_lines = [f"{n_col:12}{c_col}" for n_col, c_col
+                              in zip(name_column, content_column)]
+        
+        # Update file
+        # stop of previous field is start of new field
+        _, start, _ = self._field_pos[index-1]
+        # If the new lines are not inserted at the end,
+        # the following lines ned to be added, too
+        if start is not len(self.lines):
+            follow_lines = self.lines[start:]
+        else:
+            follow_lines = []
+        self.lines = self.lines[:start] + inserted_lines + follow_lines
+        # Shift the start/stop indices of the following fields
+        # by the amount of created fields
+        shift = len(inserted_lines)
+        for i in range(index, len(self._field_pos)):
+            old_start, old_stop, fname = self._field_pos[index]
+            self._field_pos[i] = old_start+shift, old_stop+shift, fname
+        # Add new entry
+        self._field_pos.insert(i, (name, start, start+len(inserted_lines)))
+
+    
+    def _find_field_indices(self):
+        """
+        Identify the start and exclusive stop indices of lines
+        corresponding to a field name for all fields in the file.
+        """
+        start = -1
+        stop = -1
+        name = ""
+        self._field_pos = []
+        for i, line in enumerate(self.lines):
+            # Check if line contains a new major field
+            # (Header beginning from first column)
+            if len(line) != 0 and line[0] != " " and line[:2] != "//":
+                stop = i
+                if start != -1:
+                    # Store previous field
+                    self._field_pos.append((start, stop, name))
+                start = i
+                name = line[0:12].strip()
+        # Store last field
+        stop = i
+        self._field_pos.append((start, stop, name))
+
+    def _get_field_content(self, start, stop, indent):
+        if indent == 0:
+            return self.lines[start : stop]
+        else:
+            return [line[12:] for line in self.lines[start : stop]]
 
 
 class GenBankFile(TextFile):
@@ -371,64 +554,6 @@ class GenBankFile(TextFile):
         regex = re.compile("[0-9]| |/")
         seq_str = regex.sub("", seq_str)
         return seq_str
-
-    def _find_field_indices(self):
-        """
-        Identify the start and exclusive stop indices of lines
-        corresponding to a field name for all fields in the file.
-        """
-        start = -1
-        stop = -1
-        name = ""
-        self._fields = []
-        for i, line in enumerate(self.lines):
-            # Check if line contains a new major field
-            # (Header beginning from first column)
-            if len(line) != 0 and line[0] != " " and line[:2] != "//":
-                stop = i
-                if start != -1:
-                    # Store previous field
-                    self._fields.append((start, stop, name))
-                start = i
-                name = line[0:12].strip()
-        # Store last field
-        stop = i
-        self._fields.append((start, stop, name))
-    
-    def _get_field_indices(self, name):
-        """
-        Get the start and exclusive stop indices of lines corresponding
-        to the given field name.
-        """
-        starts = []
-        stops = []
-        for field in self._fields:
-            if field[2] == name:
-                starts.append(field[0])
-                stops.append(field[1])
-        if len(starts) == 0:
-            raise InvalidFileError(f"File does not contain '{name}' category")
-        return starts, stops
-    
-    def _get_minor_fields(self, field_start, field_stop):
-        header = ""
-        content = ""
-        minor_field_dict = {}
-        for i in range(field_start, field_stop):
-            line = self.lines[i]
-            # Check if line contains a new minor field
-            # (Header beginning from first column)
-            if len(line) != 0 and line[:12].strip() != "":
-                # Store previous minor field
-                if content != "":
-                    minor_field_dict[header] = content
-                header = line[:12].strip()
-                content = line[12:].strip()
-            else:
-                content += " " + line[12:].strip()
-        # Store last minor field
-        minor_field_dict[header] = content
-        return minor_field_dict
 
 def _parse_locs(loc_str):
     locs = []
