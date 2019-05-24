ab37b163029fd37f2fd622137481aadee864a1e9
Patrick Kunzmann <patrick.kunzm@gmail.com>
2019-05-24 10:32:57 +0200

    Some bug fixes

--- src/biotite/sequence/io/genbank/file.py
+++ src/biotite/sequence/io/genbank/file.py
@@ -25,7 +25,7 @@ class GenBankFile(TextFile):
     Secondly it contains sequence annotations, i.e. the positions of
     a reference sequence, that fulfill certain roles, like promoters or
     coding sequences.
-    At last the file contains optionally the reference sequence.
+    Finally, the file contains optionally the reference sequence.
     
     This class provides a low-level interface to the file content.
     """
@@ -44,11 +44,12 @@ class GenBankFile(TextFile):
         self._find_field_indices()
     
     def get_fields(self, name):
+        name = name.upper()
         fields = []
         for i, (_, _, fname) in enumerate(self._field_pos):
             if fname == name:
                 _, content, subfields = self[i]
-                fields.append(content, subfields)
+                fields.append((content, subfields))
         return fields
 
     def __getitem__(self, index):
@@ -81,10 +82,11 @@ class GenBankFile(TextFile):
                             subfield_start, i, indent=12
                         )
                     header = line[:12].strip()
+                    subfield_st0art = i
             # Store last subfield
             if subfield_start is not None:
                 subfield_dict[header] = self._get_field_content(
-                    subfield_start, i, indent=12
+                    subfield_start, stop, indent=12
                 )
             # Only include lines in field content,
             # that are not part of a subfield
@@ -109,14 +111,25 @@ class GenBankFile(TextFile):
             raise TypeError(
                 "Expected a tuple of name, content and optionally subfields"
             )
+        inserted_lines = self._to_lines(name, content, subfields)
         
-        name = name.strip().upper()
-        if len(name) == 0:
-            raise ValueError(f"Must give a non emtpy name")
-        
-        if index != len(self):
-            del self[index]
-        self.insert(index, name, content, subfields)
+        # Stop of field to be replaced is start of new field
+        start, old_stop, _ = self._field_pos[index]
+        # If not the last element is set,
+        # the following lines need to be added, too
+        if old_stop is not len(self.lines):
+            follow_lines = self.lines[old_stop:]
+        else:
+            follow_lines = []
+        self.lines = self.lines[:start] + inserted_lines + follow_lines
+        # Shift the start/stop indices of the following fields
+        # by the amount of created fields
+        shift = len(inserted_lines) - (old_stop - start)
+        for i in range(index+1, len(self._field_pos)):
+            old_start, old_stop, fname = self._field_pos[i]
+            self._field_pos[i] = old_start+shift, old_stop+shift, fname
+        # Add new entry
+        self._field_pos[index] = start, start+len(inserted_lines), name.upper()
     
     def __delitem__(self, index):
         index = self._translate_idx(index)
@@ -125,7 +138,7 @@ class GenBankFile(TextFile):
         # by the amount of deleted fields
         shift = stop - start
         for i in range(index, len(self._field_pos)):
-            old_start, old_stop, name = self._field_pos[index]
+            old_start, old_stop, name = self._field_pos[i]
             self._field_pos[i] = old_start-shift, old_stop-shift, name
         del self.lines[start : stop]
         del self._field_pos[index]
@@ -135,51 +148,15 @@ class GenBankFile(TextFile):
 
     def insert(self, index, name, content, subfields=None):
         index = self._translate_idx(index, length_exclusive=False)
+        inserted_lines = self._to_lines(name, content, subfields)
         
-        if subfields is None:
-            subfields = {}
-        
-        name = name.strip().upper()
-        if len(name) == 0:
-            raise ValueError(f"Must give a non emtpy name")
-        subfields = OrderedDict({
-            subfield_name.upper().strip() : subfield_lines
-            for subfield_name, subfield_lines in subfields.items()
-        })
-        
-        # Create lines for new field
-        if name == "FEATURES":
-            # Header line plus all actual feature lines
-            inserted_lines = copy.copy(content).insert(
-                0, ["FEATURES" + " "*13 + "Location/Qualifiers"]
-            )
-        elif name == "ORIGIN":
-            # Header line plus all actual sequence lines
-            inserted_lines = copy.copy(content).insert(
-                0, ["ORIGIN"]
-            )
-        else:
-            name_column = []
-            content_column = []
-            # Create a line for the field name and empty lines
-            # for each additional line required by the content 
-            name_column += [name] + [""] * (len(content)-1)
-            content_column += content
-            for subfield_name, subfield_lines in subfields.items():
-                name_column += ["  " + subfield_name] \
-                               + [""] * (len(subfield_lines)-1)
-                content_column += subfield_lines
-            inserted_lines = [f"{n_col:12}{c_col}" for n_col, c_col
-                              in zip(name_column, content_column)]
-        
-        # Update file
-        # stop of previous field is start of new field
-        if len(self._field_pos) == 0 or index == 0:
+        # Stop of previous field is start of new field
+        if index == 0:
             start = 0
         else:
             _, start, _ = self._field_pos[index-1]
         # If the new lines are not inserted at the end,
-        # the following lines ned to be added, too
+        # the following lines need to be added, too
         if start is not len(self.lines):
             follow_lines = self.lines[start:]
         else:
@@ -189,10 +166,13 @@ class GenBankFile(TextFile):
         # by the amount of created fields
         shift = len(inserted_lines)
         for i in range(index, len(self._field_pos)):
-            old_start, old_stop, fname = self._field_pos[index]
+            old_start, old_stop, fname = self._field_pos[i]
             self._field_pos[i] = old_start+shift, old_stop+shift, fname
         # Add new entry
-        self._field_pos.insert(index, (start, start+len(inserted_lines), name))
+        self._field_pos.insert(
+            index,
+            (start, start+len(inserted_lines), name.upper())
+        )
     
     def append(self, name, content, subfields=None):
         self.insert(len(self), name, content, subfields)
@@ -226,6 +206,50 @@ class GenBankFile(TextFile):
             return self.lines[start : stop]
         else:
             return [line[12:] for line in self.lines[start : stop]]
+    
+    def _to_lines(self, name, content, subfields):
+        """
+        Convert the field name, field content und subfield dictionary
+        into text lines
+        """
+        if subfields is None:
+            subfields = {}
+        
+        name = name.strip().upper()
+        if len(name) == 0:
+            raise ValueError(f"Must give a non emtpy name")
+        subfields = OrderedDict({
+            subfield_name.upper().strip() : subfield_lines
+            for subfield_name, subfield_lines in subfields.items()
+        })
+        
+        # Create lines for new field
+        if name == "FEATURES":
+            # Header line plus all actual feature lines
+            lines = copy.copy(content)
+            lines.insert(
+                0, "FEATURES" + " "*13 + "Location/Qualifiers"
+            )
+        elif name == "ORIGIN":
+            # Header line plus all actual sequence lines
+            lines = copy.copy(content)
+            lines.insert(0, "ORIGIN")
+        else:
+            name_column = []
+            content_column = []
+            # Create a line for the field name and empty lines
+            # for each additional line required by the content 
+            name_column += [name] + [""] * (len(content)-1)
+            content_column += content
+            for subfield_name, subfield_lines in subfields.items():
+                name_column += ["  " + subfield_name] \
+                               + [""] * (len(subfield_lines)-1)
+                content_column += subfield_lines
+            lines = [f"{n_col:12}{c_col}" for n_col, c_col
+                              in zip(name_column, content_column)]
+        
+        return lines
+
     
     def _translate_idx(self, index, length_exclusive=True):
         """
