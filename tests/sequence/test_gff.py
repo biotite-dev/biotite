d1f9b5270adc3962b80e704f07e0ccc5336fe63b
padix-key <patrick.kunzm@gmail.com>
2019-07-01 15:57:46 +0200

    Added unit tests for GFF3 parser

---
+++ tests/sequence/test_gff.py
@@ -0,0 +1,178 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+from os.path import join
+import biotite
+import biotite.sequence as seq
+import biotite.sequence.io.gff as gff
+import biotite.sequence.io.gb as gb
+import numpy as np
+import pytest
+from .util import data_dir
+
+
+@pytest.mark.parametrize(
+    "path", ["bt_lysozyme.gff3", "gg_avidin.gff3", "ec_bl21.gff3"]
+)
+def test_conversion_lowlevel(path):
+    """
+    Test whether the low-level GFF3 interface can properly read
+    a GenBank file and write a file, without data changing.
+    """
+    file = gff.GFFFile()
+    file.read(join(data_dir, path))
+    ref_entries = [entry for entry in file]
+
+    file = gff.GFFFile()
+    for entry in ref_entries:
+        file.append(*entry)
+    temp_file_name = biotite.temp_file("gff3")
+    file.write(temp_file_name)
+
+    file = gff.GFFFile()
+    file.read(temp_file_name)
+    test_entries = [field for field in file]
+    assert test_entries == ref_entries
+
+
+@pytest.mark.parametrize(
+    "path", ["bt_lysozyme.gff3", "gg_avidin.gff3", "ec_bl21.gff3"]
+)
+def test_conversion_highlevel(path):
+    """
+    Test whether the high-level GFF3 interface can properly read
+    the features from a GFF3 file and write these properties to a file
+    without data changing.
+    """
+    file = gff.GFFFile()
+    file.read(join(data_dir, path))
+    ref_annot = gff.get_annotation(file)
+
+    file = gff.GFFFile()
+    file.set_annotation(ref_annot)
+    temp_file_name = biotite.temp_file("gff3")
+    file.write(temp_file_name)
+
+    file = gff.GFFFile()
+    file.read(path)
+    test_annot = gff.get_annotation(file)
+    
+    assert ref_annot == test_annot
+
+
+@pytest.mark.parametrize(
+    "path", ["bt_lysozyme", "gg_avidin"]
+)
+def test_genbank_consistency(path):
+    """
+    Test whether the same annotation (if reasonable) can be read from a
+    GFF3 file and a GenBank file.
+    """
+    file = gb.GenBankFile()
+    file.read(join(data_dir, path + ".gb"))
+    ref_annot = gb.get_annotation(file)
+
+    file = gff.GFFFile()
+    file.read(join(data_dir, path + ".gff3"))
+    test_annot = gff.get_annotation(file)
+    
+    # Remove qualifiers, since they will be different
+    # in GFF3 and GenBank
+    ref_annot = seq.Annotation(
+        [seq.Feature(feature.key, feature.locs) for feature in ref_annot]
+    )
+    test_annot = seq.Annotation(
+        [seq.Feature(feature.key, feature.locs) for feature in test_annot]
+    )
+    assert ref_annot == test_annot
+
+
+def test_file_access():
+    """
+    Test getting, setting, deleting and inserting entries in a GFF3
+    file.
+    """
+    file = gff.GFFFile()
+    entry_scaffold = ("ab", "cd", 1, 2, None, None, None, {"Id":"foo"})
+    entry = ("a",) + entry_scaffold
+    file.append(*entry)
+    assert file[0] == entry
+    file.append(*(("b",) + entry_scaffold))
+    file.insert(1, *(("c",) + entry_scaffold))
+    file[1] = ("d",) + entry_scaffold)
+    file.insert(3, *(("e",) + entry_scaffold))
+    def file[2]
+    assert [seqid for seqid, _, _, _, _, _, _, _, _ in file] \
+        == ["a", "d", "e", ]
+
+
+def test_entry_indexing():
+    """
+    Test whether a GFF3 file is indexed correctly based on an artificial
+    test file with multiple directives, including '##FASTA'.
+    """
+    file = gff.GFFFile()
+    with pytest.warns(UserWarning):
+        file.read(join(data_dir, "indexing_test.gff3"))
+    assert file._directives == [
+        ("directive 1", 1),
+        ("directive 2", 2),
+        ("directive 3", 8),
+    ]
+    assert file._entries.tolist() == [3,4,6]
+
+
+
+def test_percent_encoding():
+    """
+    Test whether percent encoding is working correctly based on an
+    artificial test file.
+    """
+    file = gff.GFFFile()
+    file.read(join(data_dir, "percent_test.gff3"))
+    seqid, source, type, start, end, score, strand, phase, attrib \
+        = file[0]
+    assert seqid == "123,456"
+    assert source == "äääh"
+    assert type == "regi&n"
+    assert attrib == {
+        "ID"   : "AnID;AnotherID",
+        "Name" : "Ångström",
+        "c$l$r": "read\tgreen\tblue"
+    }
+
+    file2 = gff.GFFFile()
+    file.append(seqid, source, type, start, end, score, strand, phase, attrib)
+    assert (seqid, source, type, start, end, score, strand, phase, attrib) \
+        == file[0]
+
+
+def test_error():
+    """
+    Assert that certain exceptions are raised
+    """
+    file = gff.GFFFile()
+    with pytest.raises("ValueError"):
+        # 'seqid' beginning with '>' is not legal
+        file.append(">xyz", "ab", "cd", 1, 2, None, None, None, {"Id":"foo"})
+    with pytest.raises("ValueError"):
+        # String fields must not be empty
+        file.append("", "ab", "cd", 1, 2, None, None, None, {"Id":"foo"})
+    with pytest.raises("ValueError"):
+        # String fields must not be empty
+        file.append("xyz", "", "cd", 1, 2, None, None, None, {"Id":"foo"})
+    with pytest.raises("ValueError"):
+        # String fields must not be empty
+        file.append("xyz", "ab", "", 1, 2, None, None, None, {"Id":"foo"})
+    with pytest.raises("ValueError"):
+        # Attributes not be empty
+        file.append("xyz", "ab", "cd", 1, 2, None, None, None, {})
+
+
+def test_feature_without_id():
+    """
+    A feature without 'ID' should raise an error if it has multiple
+    locations and consequently multiple entries in the GFF3 file.
+    """
+    pass
