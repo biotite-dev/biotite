88e50e08c647c0c88c396e9b0c9f8fe2cb025cd4
padix-key <patrick.kunzm@gmail.com>
2019-07-19 17:57:44 +0200

    Added unit tests

--- tests/sequence/test_codon.py
+++ tests/sequence/test_codon.py
@@ -5,10 +5,12 @@
 import biotite.sequence as seq
 import pytest
 
+
 @pytest.mark.parametrize("table_id",
     [1,2,3,4,5,6,9,10,11,12,13,14,16,21,22,23,24,25,26,27,28,29,30,31])
 def test_table_load(table_id):
     table = seq.CodonTable.load(table_id)
+
 
 def test_table_indexing():
     table = seq.CodonTable.load("Standard")
@@ -18,3 +20,27 @@ def test_table_indexing():
     assert table[(0, 0, 0)] == 8
     for codon in table[8]:
         assert codon in ((0, 0, 0), (0, 0, 2))
+
+
+def test_incomplete_input_dict():
+    """
+    Ensure that an exception is raised when the input codon dictionary
+    misses an entry.
+    """
+    with pytest.raises(ValueError, match=".* dictionary does not contain .*"):
+        codon_dict = seq.CodonTable.default_table().codon_dict()
+        del codon_dict["CTG"]
+        seq.CodonTable(codon_dict, starts=["ATG"])
+
+
+
+def test_codon_dict():
+    """
+    Assert that the 'codon_dict()' method returns a correct dictionary.
+    """
+    default_table = seq.CodonTable.default_table()
+    ref_codon_dict = default_table.codon_dict()
+    test_codon_dict = seq.CodonTable(
+        ref_codon_dict, default_table.start_codons()
+    ).codon_dict()
+    assert ref_codon_dict == test_codon_dict
