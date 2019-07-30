3b59f0d06a43de62d1a2efa4b6ec30d81406f667
padix-key <patrick.kunzm@gmail.com>
2019-07-30 19:03:37 +0200

    Work on additional arguments for ClustalOmega

--- src/biotite/application/clustalo/app.py
+++ src/biotite/application/clustalo/app.py
@@ -5,12 +5,14 @@
 __author__ = "Patrick Kunzmann"
 __all__ = ["ClustalOmegaApp"]
 
-from ..msaapp import MSAApp
-from ..application import AppState, requires_state
+from ...temp import temp_file
 from ...sequence.sequence import Sequence
 from ...sequence.seqtypes import NucleotideSequence, ProteinSequence
 from ...sequence.io.fasta.file import FastaFile
 from ...sequence.align.alignment import Alignment
+from ...sequence.phylo import Tree
+from ..msaapp import MSAApp
+from ..application import AppState, requires_state
 
 
 class ClustalOmegaApp(MSAApp):
@@ -47,14 +49,73 @@ class ClustalOmegaApp(MSAApp):
             self._seqtype = "DNA"
         else:
             self._seqtype = "Protein"
+        self._seq_count = len(sequences)
+        self._mbed = True
+        self._dist_matrix = None
+        self._tree = None
+        self._in_dist_matrix_file_name = temp_file("mat")
+        self._out_dist_matrix_file_name = temp_file("mat")
+        self._in_tree_file_name = temp_file("tree")
+        self._out_tree_file_name = temp_file("tree")
         super().__init__(sequences, bin_path)
     
     def run(self):
-        self.set_arguments(
-            ["--in", self.get_input_file_path(),
-             "--out", self.get_output_file_path(),
-             "--seqtype", self._seqtype,
-             # Tree order for get_alignment_order() to work properly 
-             "--output-order=tree-order"]
-        )
+        args = [
+            "--in", self.get_input_file_path(),
+            "--out", self.get_output_file_path(),
+            "--seqtype", self._seqtype,
+            # Tree order for get_alignment_order() to work properly 
+            "--output-order=tree-order",
+            "--guidetree-out", self._out_tree_file_name,
+        ]
+        if not self._mbed:
+            args += [
+                "--full",
+                "--distmat-out", self._out_dist_matrix_file_name
+            ]
+        if self._dist_matrix is not None:
+            pass
+        if self._tree is not None:
+            with open(self._in_tree_file_name, "w") as file:
+                file.write(str(self._tree))
+            args += ["--guidetree-in", self._in_tree_file_name]
+        self.set_arguments(args)
         super().run()
+    
+    def evaluate(self):
+        super().evaluate()
+        if not self._mbed:
+            with open(self._out_dist_matrix_file_name, "r") as file:
+                self._dist_matrix = file.read()
+        with open(self._out_tree_file_name, "r") as file:
+            self._tree = Tree.from_newick(file.read().replace("\n", ""))
+    
+    @requires_state(AppState.CREATED)
+    def full_matrix_calculation(self):
+        self._mbed = False
+    
+    @requires_state(AppState.CREATED)
+    def set_distance_matrix(self, matrix):
+        self._dist_matrix = matrix
+    
+    @requires_state(AppState.JOINED)
+    def get_distance_matrix(self):
+        if self._mbed:
+            raise ValueError(
+                "Getting the distance matrix requires "
+                "'full_matrix_calculation()'"
+            )
+        return self._dist_matrix
+    
+    @requires_state(AppState.CREATED)
+    def set_guide_tree(self, tree):
+        if self._seq_count != len(tree):
+            raise ValueError(
+                f"Tree with {len(tree)} leaves is not sufficient for "
+                "{self._seq_count} sequences, must be equal"
+            )
+        self._tree = tree
+    
+    @requires_state(AppState.JOINED)
+    def get_guide_tree(self):
+        return self._tree
