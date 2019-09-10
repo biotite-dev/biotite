e96f94edaa419d7d7aa4d28909f8bdbe7a60a165
padix-key <patrick.kunzm@gmail.com>
2019-09-10 16:42:16 +0200

    Added test fro each query type

--- tests/database/test_rcsb.py
+++ tests/database/test_rcsb.py
@@ -15,15 +15,18 @@ def test_fetch_cif():
     file = rcsb.fetch("1l2y", "cif", biotite.temp_dir(), overwrite=True)
     array = strucio.load_structure(file)
 
+
 @pytest.mark.xfail(raises=ConnectionError)
 def test_fetch_pdb():
     file = rcsb.fetch("1l2y", "cif", biotite.temp_dir(), overwrite=True)
     array = strucio.load_structure(file)
 
+
 @pytest.mark.xfail(raises=ConnectionError)
 def test_fetch_invalid():
     with pytest.raises(ValueError):
         file = rcsb.fetch("xxxx", "cif", biotite.temp_dir(), overwrite=True)
+
 
 @pytest.mark.xfail(raises=ConnectionError)
 def test_search():
@@ -38,10 +41,12 @@ def test_search():
             ids_comp2.append(id)
     assert ids_comp == sorted(ids_comp2)
 
+
 @pytest.mark.xfail(raises=ConnectionError)
 def test_search_empty():
     ids = rcsb.search(rcsb.MolecularWeightQuery(0, 1))
     assert len(ids) == 0
+
 
 @pytest.mark.xfail(raises=ConnectionError)
 def test_search_invalid():
@@ -52,3 +57,51 @@ def test_search_invalid():
     with pytest.raises(ValueError):
         ids = rcsb.search(InvalidQuery())
 
+
+@pytest.mark.xfail(raises=ConnectionError)
+@pytest.mark.parametrize(
+    # IMPORTANT NOTE: Since the PDB continuously adds new structures,
+    # the expected IDs might need to be updated,
+    # if an 'AssertionError' occurs
+    "query_type, params, exp_ids",
+    [
+        (
+            rcsb.ResolutionQuery,
+            {"max": 0.6},
+            ["1EJG", "1I0T", "3NIR", "3P4J", "5D8V", "5NW3"]
+        ),
+        (
+            rcsb.BFactorQuery,
+            {"min": 1.0, "max": 1.5},
+            ["1G2K", "2OL9", "3ECO", "3INZ", "3LN2", "4CA4", "6M9I"]
+        ),
+        (
+            rcsb.MolecularWeightQuery,
+            {"min": 50000000},
+            ["6CGV", "4F5X"]
+        ),
+        (
+            rcsb.MoleculeTypeQuery,
+            {"rna": False, "dna": False, "hybrid": True, "protein": False},
+            60
+        ),
+        (
+            rcsb.MethodQuery,
+            {"method": "fiber diffraction", "has_data": True},
+            ["1HGV", "1IFP", "1QL1", "2C0W", "2XKM",
+             "2ZWH", "3HQV", "3HR2", "3PDM", "4IFM"]
+        ),
+        (
+            rcsb.PubMedIDQuery,
+            {"ids": [6726807, 10490104]},
+            ["2HHB", "3HHB", "4HHB", "9GAA", "9GAC", "9GAF",]
+        ),
+    ]
+)
+def test_simple_query_types(query_type, params, exp_ids):
+    query = query_type(**params)
+    ids = rcsb.search(query)
+    if isinstance(exp_ids, int):
+        assert len(ids) == exp_ids
+    else:
+        assert set(ids) == set(exp_ids)
