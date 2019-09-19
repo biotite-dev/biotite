577710e07254613d2a2e2d0fb7a952b7b5fbdfe9
padix-key <patrick.kunzm@gmail.com>
2019-09-19 15:39:33 +0200

    Added comments

--- doc/scraper.py
+++ doc/scraper.py
@@ -4,10 +4,10 @@ import os
 from sphinx_gallery.scrapers import figure_rst
 
 
-
-def static_scraper(block, block_vars, gallery_conf):
+def static_image_scraper(block, block_vars, gallery_conf):
     script_path = os.path.dirname(block_vars["src_file"])
     image_sources = []
+    # Search for comment line containing 'biotite_static_image'
     _, code, _ = block
     lines = code.split("\n")
     for line in lines:
@@ -15,14 +15,18 @@ def static_scraper(block, block_vars, gallery_conf):
         if line.startswith("#"):
             line = line[1:].strip()
             if line.startswith("biotite_static_image"):
+                # Get the image name after the '=' character
                 image_name = line.split("=")[1].strip()
                 image_sources.append(os.path.join(script_path, image_name))
 
+    # Copy the images into the 'gallery' directory under a canonical
+    # sphinx-gallery name
     image_destinations = []
     image_path_iterator = block_vars['image_path_iterator']
     for image in image_sources:
         image_destination = image_path_iterator.next()
         image_destinations.append(image_destination)
         shutil.copy(image, image_destination)
-    # Use the `figure_rst` helper function to generate rST for image files
+    
+    # Generate rST for detected image files
     return figure_rst(image_destinations, gallery_conf['src_dir'])
