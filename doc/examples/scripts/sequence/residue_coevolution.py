fd57710a02a65d278168fc4519e2b5eabb8656ed
padix-key <patrick.kunzm@gmail.com>
2019-10-22 14:50:45 +0200

    Finished example

--- doc/examples/scripts/sequence/residue_coevolution.py
+++ doc/examples/scripts/sequence/residue_coevolution.py
@@ -14,7 +14,7 @@ distribution:
    P_{X,Y}(x,y) \cdot \log_2 \frac{P_{X,Y}(x,y)}{P_{X}(x) P_{Y}(y)}
 
 In the context of a protein the amino acid sequence is aligned to
-homologous sequences. The distribution is the distribution of
+multiple homologous sequences. The distribution is the distribution of
 amino acids in a alignment column.
 When mutations in one column are often associated with certain
 mutations in another alignment column, the MI between these two
@@ -22,16 +22,19 @@ positions is high.
 This indicates that these two sequence positions might have evolved
 together (coevolution).
 
-For more significant results the MI of the given alignment is compared
+In order to suppress randomly high or low MI due to general conservation
+in an alignment column, the MI of the given alignment is compared
 to variants of this alignment, where each alignment column is randomly
-shuffled. The results are aggregated into a Z score:
+shuffled. The results are aggregated into a Z-score:
 
 .. math:: Z_{MI} = \frac{ MI - \mu(MI_{\textrm{shuffle}}) }
                         {   \sigma(MI_{\textrm{shuffle}}) }
 
-This example demonstrates this method on the example of hen-egg white
-lysozyme (PDB: 1AKI).
-At first, homologous sequences
+This example demonstrates this method on the example of the C-Myb
+DNA-binding domain (C-Myb R1) (PDB: 1GUU).
+At first, sequences of homologous proteins are searched in the
+curated *SwissProt* database via *NCBI BLAST*.
+Afterwards these sequences are aligned with *Clustal Omega*.
 """
 
 # Code source: Patrick Kunzmann
@@ -63,6 +66,8 @@ sequence = pdbx.get_sequence(pdbx_file)[0]
 structure = pdbx.get_structure(pdbx_file, model=1, use_author_fields=False)
 structure = structure[struc.filter_amino_acids(structure)]
 
+# Identity threshold for a sequence to be counted as homologous sequence
+IDENTITY_THESHOLD = 0.4
 # Find homologous proteins in SwissProt via BLAST
 app = blast.BlastWebApp("blastp", sequence, database="swissprot")
 app.start()
@@ -71,7 +76,6 @@ alignments = app.get_alignments()
 hit_seqs = [sequence]
 hit_ids = ["Query"]
 hit_starts = [1]
-IDENTITY_THESHOLD = 0.4
 for ali in alignments:
     identity = align.get_sequence_identity(ali)
     # Do not include the exact same sequence -> identity < 1.0
@@ -97,12 +101,18 @@ graphics.plot_alignment_type_based(
     show_numbers=True, number_functions=number_functions,
     color_scheme="flower"
 )
+ax.set_title("C-Myb R1-like sequences")
 fig.tight_layout()
 
 ########################################################################
-# Based on the alignment the mutual information can be calculated...
+# Now the MI is calculated from the multiple sequence alignment and from
+# column-shuffled variants of it to obtain the MI-Z-score.
+# As the residue coevolution in C-Myb should be examined, only alignment
+# columns that have no gap in C-Myb are considered.
+# Finally, the MI-Z-score matrix is plotted.
+# High values indicate that the residues at the respective two
+# positions have coevolved.
 
-# Calculate MI Z score
 def mutual_information_zscore(alignment, n_shuffle=100):
     codes = align.get_codes(alignment).T
     alph = alignment.sequences[0].alphabet
@@ -161,9 +171,10 @@ def _mutual_information(codes, alph):
             mi[i,j] = np.sum(mi_before_sum[~np.isnan(mi_before_sum)])
     return mi
 
-# We are only intrested in alignment columns
-# that have no gap in the C-MYB sequence
+
+# Remove alignment columns that have a gap in the C-Myb sequence
 alignment = alignment[alignment.trace[:,0] != -1]
+
 mi = mutual_information_zscore(alignment)
 
 # Create the color map for the plot
@@ -174,14 +185,16 @@ cmap_val = np.stack(
 ).transpose()
 cmap = colors.ListedColormap(cmap_val)
 
+# Plot the MI-Z-Score matrix
 fig = plt.figure(figsize=(8.0, 7.0))
 ax = fig.gca()
 im = ax.pcolormesh(mi, cmap=cmap)
 cbar = fig.colorbar(im)
-cbar.set_label("Z-score of mutual information")
+cbar.set_label("MI-Z-score")
 ax.set_aspect("equal")
 ax.set_xlabel("Residue position")
 ax.set_ylabel("Residue position")
+ax.set_title("Residue coevolution in C-Myb R1")
 fig.tight_layout()
 # sphinx_gallery_thumbnail_number = 2
 
