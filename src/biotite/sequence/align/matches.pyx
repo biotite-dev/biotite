eaa1029a73debf78c0a698eef0edf81d6716551f
padix-key <patrick.kunzm@gmail.com>
2019-10-29 09:30:38 +0100

    Working 'find_matches()' function

--- src/biotite/sequence/align/matches.pyx
+++ src/biotite/sequence/align/matches.pyx
@@ -28,7 +28,7 @@ ctypedef fused CodeType:
 
 
 def find_matches(references, query):
-    alphabet = query.alphabet
+    alphabet = query.get_alphabet()
     cdef uint64 alph_length = len(alphabet)
     for i, reference in enumerate(references):
         if reference.get_alphabet() != alphabet:
@@ -45,15 +45,18 @@ def find_matches(references, query):
     cdef uint64[:] mapping_lengths
     return_val = _create_word_to_pos_mapping(query.code, alph_length)
     # Double casting is necessary to trick compiler
+    # to convert integer back into pointer
     mapping = <uint64**> <uint64> (return_val[0])
     mapping_lengths = return_val[1]
 
     #print()
     #for code in range(alph_length):
-    #    print(code, [mapping[code][i] for i in range(mapping_lengths[code])])
+    #    m = [mapping[code][i] for i in range(mapping_lengths[code])]
+    #    if len(m) != 0:
+    #        print(code, m)
 
     matches = [
-        _find_matches[CodeType](reference.code, mapping, mapping_lengths)
+        _find_matches(reference.code, <uint64> mapping, mapping_lengths)
         for reference in references
     ]
 
@@ -61,29 +64,37 @@ def find_matches(references, query):
     return matches
 
 
-cdef np.ndarray _find_matches(CodeType[:] reference,
-                              uint64** query_mapping,
-                              uint64[:] mapping_lengths):
+# This is a Python function (instead of cdef)
+# to be able to infer CodeType at run time
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
+def _find_matches(CodeType[:] reference,
+                  # 'query_mapping' is actually a pointer value
+                  uint64 query_mapping,
+                  uint64[:] mapping_lengths):
     cdef int64 ref_pos, map_pos
     cdef int match_i
+    # Convert the as uint64 masquerading pointer into an actual pointer
+    cdef uint64** mapping_ptr = <uint64**> query_mapping
     cdef uint64* positions_ptr
     cdef int positions_ptr_length
     cdef CodeType code
-    cdef int INIT_SIZE = 100
-    cdef uint64[:] matches = np.zeros((INIT_SIZE, 2), dtype=np.uint64)
+    cdef int INIT_SIZE = 1
+    cdef uint64[:,:] matches = np.zeros((INIT_SIZE, 2), dtype=np.uint64)
 
+    match_i = 0
     for ref_pos in range(reference.shape[0]):
         code = reference[ref_pos]
-        positions_ptr = query_mapping[code]
+        positions_ptr = mapping_ptr[code]
         positions_ptr_length = mapping_lengths[code]
         for map_pos in range(positions_ptr_length):
             if match_i >= matches.shape[0]:
-                matches = increase_array_size(matches)
+                matches = increase_array_size(np.asarray(matches))
             matches[match_i, 0] = ref_pos
-            matches[match_i, 1] = map_pos
+            matches[match_i, 1] = positions_ptr[map_pos]
             match_i += 1
 
-    return np.asarray(matches)
+    return np.asarray(matches[:match_i])
 
 
 cdef np.ndarray increase_array_size(np.ndarray array):
@@ -95,6 +106,10 @@ cdef np.ndarray increase_array_size(np.ndarray array):
     return new_array
 
 
+# This is a Python function (instead of cdef)
+# to be able to infer CodeType at run time
+#@cython.boundscheck(False)
+#@cython.wraparound(False)
 def _create_word_to_pos_mapping(CodeType[:] sequence not None,
                                 uint64 alph_length):
     cdef int64 pos
@@ -115,6 +130,7 @@ def _create_word_to_pos_mapping(CodeType[:] sequence not None,
         positions_ptr[length] = pos
         mapping_lengths[code] = length + 1
     
+    # Convert pointer to integer to be returnable from a Python function
     return <uint64>mapping_ptr, np.asarray(mapping_lengths)
 
 
