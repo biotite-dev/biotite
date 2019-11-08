b5186809de83e0719054576ca22b529d49a1a60b
padix-key <patrick.kunzm@gmail.com>
2019-11-08 17:13:04 +0100

    'density()' accepts directly coordinates

--- src/biotite/structure/density.py
+++ src/biotite/structure/density.py
@@ -12,28 +12,23 @@ __all__ = ["density"]
 
 from numbers import Integral
 import numpy as np
-from .atoms import Atom, AtomArray, stack, array, coord, AtomArrayStack
-from .box import box_volume
-from .geometry import displacement
-from .util import vector_dot
-from .celllist import CellList
+from .atoms import coord
 
 
 def density(atoms, selection=None, delta=1.0, bins=None,
             density=False, weights=None):
     r"""
-    Compute the density of an atoms selection.
+    Compute the density of the selected atoms.
 
     This creates a 3d histogram over the coordinates of selected atoms.
     By default, the grid for the histogram is built based on the
     coordinates of the given `atoms` with an even gridspacing of
-    `delta` in all three
-    dimensions.
+    `delta` in all three dimensions.
     Alternatively, a custom grid can be used.
 
     Parameters
     ----------
-    atoms : AtomArray or AtomArrayStack
+    atoms : AtomArray or AtomArrayStack or ndarray, shape=(n,3) or shape=(m,n,3)
         The density is calculated based on these atoms.
     selection : ndarray, dtype=bool, shape=(n,), optional
         Boolean mask for `atoms` to calculate the density only on a set
@@ -49,12 +44,11 @@ def density(atoms, selection=None, delta=1.0, bins=None,
         - If `bins` is a string, it defines the function used to
           calculate the bins.
 
-        See `numpy.histogramdd()` for further details.
+        See :func:`numpy.histogramdd()` for further details.
     density : boolean, optional
         If False, the number of samples in each bin is returned.
         Otherwise, returns the probability density function of each bin.
-
-        See `numpy.histogramdd()` for further details.
+        See :func:`numpy.histogramdd()` for further details.
     weights: ndarray, shape=(N) or shape=(N,M), optional
         An array of values to weight the contribution of N atoms in 
         models.
@@ -71,14 +65,16 @@ def density(atoms, selection=None, delta=1.0, bins=None,
     edges : list of ndarray, dtype=float
         A list containing the 3 arrays describing the bin edges.
     """
-    is_stack = isinstance(atoms, AtomArrayStack)
+    coords = coord(atoms)
+    
+    is_stack = coords.ndim == 3
 
     # Define the grid for coordinate binning based on coordinates of
     # supplied atoms
     # This makes the binning independent of a supplied box vector and 
     # fluctuating box dimensions are not a problem
     # However, this means that the user has to make sure the region of
-    # interest is in the center of the box, i.e. by centering the to
+    # interest is in the center of the box, i.e. by centering the
     # investigated protein in the box.
     if bins is None:
         if is_stack:
@@ -86,7 +82,7 @@ def density(atoms, selection=None, delta=1.0, bins=None,
         else:
             axis = 0
         grid_min, grid_max = np.min(
-            atoms.coord, axis=axis), np.max(atoms.coord, axis=axis
+            coords, axis=axis), np.max(coords, axis=axis
         )
         bins = np.array([
             np.arange(grid_min[0], grid_max[0]+delta, delta),
@@ -95,20 +91,20 @@ def density(atoms, selection=None, delta=1.0, bins=None,
         ])
 
     if selection is None:
-        selected = atoms
+        selected_coords = coords
     else:
-        selected = atoms[...,selection]
+        selected_coords = coords[...,selection, :]
 
-    # reshape the coords into Nx3
-    coords = selected.coord.reshape((np.prod(selected.shape), 3))
+    # Reshape the coords into Nx3
+    coords = selected_coords.reshape((np.prod(selected_coords.shape[:-1]), 3))
 
-    # We need a weight value per coordinate, but input might be per atom.
+    # We need a weight value per coordinate, but input might be per atom
     if weights is not None:
         if is_stack and len(weights.shape) < 2:
-            weights = np.repeat(weights, len(selected))
+            weights = np.repeat(weights, len(selected_coords))
         weights = weights.reshape(coords.shape[0])
     
-    # calculate the histogram
+    # Calculate the histogram
     hist = np.histogramdd(
         coords, bins=bins, density=density, weights=weights
     )
