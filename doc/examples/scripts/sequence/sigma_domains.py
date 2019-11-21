12dbcee5bc98371f9e6fabf9708a19d09d6eb340
padix-key <patrick.kunzm@gmail.com>
2019-11-21 12:02:40 +0100

    Fix for Matplotlib version 3.1.1

--- doc/examples/scripts/sequence/sigma_domains.py
+++ doc/examples/scripts/sequence/sigma_domains.py
@@ -102,9 +102,9 @@ for i, (gene_name, domain_pos_for_gene, length) \
 ax.set_xlim(0, max(seq_lengths))
 ax.set_xlabel("Sequence position")
 # Inverted y-axis
-ax.set_ylim(len(genes), -1)
 ax.set_yticks(np.arange(len(genes)))
 ax.set_yticklabels(list(genes.keys()))
+ax.set_ylim(len(genes), -1)
 ax.set_title(r"Domains of E. coli $\sigma$ factors")
 fig.tight_layout()
 
