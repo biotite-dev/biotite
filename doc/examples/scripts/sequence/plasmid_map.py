66df69c13e24b071632f876838b1d830f6b395cb
padix-key <patrick.kunzm@gmail.com>
2020-01-30 16:54:03 +0100

    Renamed script

--- doc/examples/scripts/sequence/plasmid_map.py
+++ doc/examples/scripts/sequence/plasmid_map.py
@@ -15,21 +15,20 @@ import biotite.sequence as sequence
 import biotite.sequence.io.genbank as gb
 import biotite.sequence.graphics as graphics
 import biotite.database.entrez as entrez
-"""
+
 file_name = entrez.fetch(
     "L37382", target_path=biotite.temp_dir(),
     suffix="gb", db_name="nuccore", ret_type="gb"
 )
 file = gb.GenBankFile()
 file.read(file_name)
-annot_seq = file.get_annotated_sequence(ignore=["source"])
+annot_seq = gb.get_annotated_sequence(file)
 
 fig = plt.figure(figsize=(8.0, 0.8))
-ax = fig.add_subplot(111)
-graphics.plot_feature_map(
-    ax, annot_seq.annotation,
-    multi_line=False, loc_range=(1, len(annot_seq.sequence)),
+ax = fig.add_subplot(111, projection="polar")
+graphics.plot_plasmid_map(
+    ax, annot_seq.annotation, loc_range=(1, len(annot_seq.sequence)+1),
+    label="L37382"
 )
 fig.tight_layout()
 plt.show()
-"""
