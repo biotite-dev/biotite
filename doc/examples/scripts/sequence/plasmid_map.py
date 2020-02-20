227534c96ece3839fe95075be096f07557b7d464
padix-key <patrick.kunzm@gmail.com>
2020-02-20 14:00:16 +0100

    Work on plasmid map examples

--- doc/examples/scripts/sequence/plasmid_map.py
+++ doc/examples/scripts/sequence/plasmid_map.py
@@ -1,6 +1,6 @@
 """
-Plasmid map of an modified pSB1C3 vector
-========================================
+Plasmid map of a modified pSB1C3 vector
+=======================================
 
 
 """
@@ -8,27 +8,50 @@ Plasmid map of an modified pSB1C3 vector
 # Code source: Patrick Kunzmann
 # License: BSD 3 clause
 
+import io
+import requests
 import matplotlib.pyplot as plt
 import numpy as np
-import biotite
-import biotite.sequence as sequence
 import biotite.sequence.io.genbank as gb
 import biotite.sequence.graphics as graphics
 import biotite.database.entrez as entrez
 
-file_name = entrez.fetch(
-    "J01749", target_path=biotite.temp_dir(),
-    suffix="gb", db_name="nuccore", ret_type="gb"
-)
+
+PLASMID_URL = "https://media.addgene.org/snapgene-media/" \
+              "v1.6.2-0-g4b4ed87/sequences/67/17/246717/" \
+              "addgene-plasmid-26094-sequence-246717.gbk"
+
+
+#file_name = entrez.fetch(
+#    "KX986172", target_path=biotite.temp_dir(),
+#    suffix="gb", db_name="nuccore", ret_type="gb"
+#)
+#response = requests.get(PLASMID_URL)
+#file = gb.GenBankFile()
+#file.read(io.StringIO(response.text))
+#annot_seq = gb.get_annotated_sequence(file)
+
+response = requests.get(PLASMID_URL)
+with open("test.gb", "w") as file:
+    file.write(response.text)
 file = gb.GenBankFile()
-file.read(file_name)
-annot_seq = gb.get_annotated_sequence(file)
+file.read("test.gb")
+annotation = gb.get_annotation(file, include_only=[
+    "promoter", "terminator", "protein_bind", "RBS", "CDS", "rep_origin"
+])
+primters = gb.get_annotation(file, include_only=["primer_bind"])
+_, seq_length, _, _, _, _ = gb.get_locus(file)
+plasmid_name = file.get_fields("KEYWORDS")[0][0][0]
+
+def feature_formatter(feature):
+    label = feature.qual.get("label")
+    return True, "green", "black", label
 
 fig = plt.figure(figsize=(8.0, 8.0))
 ax = fig.add_subplot(111, projection="polar")
 graphics.plot_plasmid_map(
-    ax, annot_seq.annotation, loc_range=(1, len(annot_seq.sequence)+1),
-    label="pBR322"
+    ax, annotation, loc_range=(1, seq_length+1),
+    label=plasmid_name, feature_formatter=feature_formatter
 )
 fig.tight_layout()
 plt.show()
---
+++ doc/examples/scripts/sequence/plasmid_map_custom.py
@@ -0,0 +1,53 @@
+"""
+Custom visualization of a custom plasmid
+========================================
+
+
+"""
+
+# Code source: Patrick Kunzmann
+# License: BSD 3 clause
+
+import matplotlib.pyplot as plt
+import numpy as np
+import biotite
+import biotite.sequence as seq
+import biotite.sequence.io.genbank as gb
+import biotite.sequence.graphics as graphics
+import biotite.database.entrez as entrez
+
+
+NAME = "This is   a A-Test_bla_bla______________________--------------"
+#NAME = "T_"
+#NAME = "araC promoter"
+#NAME = "Test"
+
+annotation = seq.Annotation([
+    seq.Feature(
+        "CDS", [seq.Location(190,310)]
+    ),
+    seq.Feature(
+        "gene", [seq.Location(190,310)]
+    ),
+    seq.Feature(
+        "CDS", [seq.Location(200,300)], {"product": NAME}
+    )
+])
+
+fig = plt.figure(figsize=(8.0, 8.0))
+ax = fig.add_subplot(111, projection="polar")
+graphics.plot_plasmid_map(
+    ax, annotation, loc_range=(1, 700),
+    label="My plasmid"
+)
+
+ticks = ax.get_xticks()
+labels = ax.get_xticklabels()
+print(ticks)
+print(np.pi/2)
+ax.set_xticks(list(ticks) + [np.pi/2])
+print(ax.get_xticks())
+ax.set_xticklabels(labels + ["EcoRI"])
+
+fig.tight_layout()
+plt.show()
