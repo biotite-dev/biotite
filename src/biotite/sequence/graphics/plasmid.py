27d331c946cd8dc45d63b37be2dfe0e5cf342f50
padix-key <patrick.kunzm@gmail.com>
2020-02-20 17:34:10 +0100

    Moving into separate artists

--- src/biotite/sequence/graphics/plasmid.py
+++ src/biotite/sequence/graphics/plasmid.py
@@ -6,6 +6,7 @@ __author__ = "Patrick Kunzmann"
 __all__ = ["plot_plasmid_map"]
 
 import copy
+import warnings
 import abc
 import numpy as np
 import re
@@ -13,28 +14,26 @@ from ...visualize import colors
 from ..annotation import Annotation, Feature, Location
 
 
-def plot_plasmid_map(axes, annotation, loc_range=None, radius=15,
+def plot_plasmid_map(axes, annotation, plasmid_size=None, radius=15,
                      tick_length= 0.2, tick_step=200, ring_width=0.2,
                      feature_width=1.0, spacing=0.2, arrow_head_width=0.5,
                      label=None, face_properties=None, label_properties=None,
                      feature_formatter=None):
-    from matplotlib.transforms import Bbox
-    
     ### Setup parameters ###
-    if loc_range is None:
-        loc_range = annotation.get_location_range()
-    else:
-        annotation = annotation[loc_range[0] : loc_range[1]]
+    if plasmid_size is None:
+        # 'stop' of 'get_location_range()' is exclusive -> -1
+        plasmid_size = annotation.get_location_range()[1] - 1
     if face_properties is None:
-         face_properties = {}
+        face_properties = {}
     if label_properties is None:
-         label_properties = {}
+        label_properties = {}
     if feature_formatter is None:
         feature_formatter = _default_feature_formatter
-    
-    
+
+
     ### Setup matplotlib ###
     # The x-coordinate is given as angle (rad)
+    # Full circle -> 2*pi
     axes.set_xlim(0, 2*np.pi)
     axes.set_ylim(0, radius)
     axes.yaxis.set_visible(False)
@@ -46,13 +45,14 @@ def plot_plasmid_map(axes, annotation, loc_range=None, radius=15,
     axes.spines["polar"].set_visible(False)
     axes.grid(False)
     # Setup ticks: Angle is replaced by sequence location
-    ticks = [loc_range[0]] \
-            + [(i+1) * tick_step for i in range(0, loc_range[1] // tick_step)]
-    axes.xaxis.set_ticks([_loc_to_rad(tick, loc_range) for tick in ticks])
+    ticks = [1] \
+        + [(i+1) * tick_step for i in range(0, plasmid_size // tick_step)]
+    axes.xaxis.set_ticks([_loc_to_rad(tick, plasmid_size) for tick in ticks])
     axes.xaxis.set_ticklabels(ticks)
 
-    
+
     ### Draw plasmid ring with ticks and central label ###
+    
     # Plasmid ring
     # Use 'barh()' instead of a Rectangle patch to ensure that the axes
     # is properly initialized
@@ -61,13 +61,15 @@ def plot_plasmid_map(axes, annotation, loc_range=None, radius=15,
         radius-ring_width-tick_length, 2*np.pi, ring_width,
         align="edge", color="black"
     )
+    
     # Ticks (ticks itself, not the tick labels)
     for tick in ticks:
-        angle = _loc_to_rad(tick, loc_range)
+        angle = _loc_to_rad(tick, plasmid_size)
         axes.plot(
             (angle, angle), (radius-tick_length, radius),
             color="black", linewidth=1, linestyle="-"
         )
+    
     # Central plasmid label
     if label is not None:
         axes.text(
@@ -75,183 +77,24 @@ def plot_plasmid_map(axes, annotation, loc_range=None, radius=15,
             color="black", size=32, fontweight="bold"
         )
 
-    
-    ### Draw features ###
-    # Find the maximum amount of feature rows
-    # (used for overlapping features)
+
+    ### Draw plasmid interior ###
     radius_eff = radius - ring_width - tick_length
-    row_count = int(radius_eff // (feature_width + spacing))
-    # Tracks the location ranges of feature that were added to a row
-    # in order to check if that row is occupied
-    ranges_in_row = [[] for i in range(row_count)]
-    # Stores the bottom coordinate (radius) for each row
-    row_bottoms = [radius_eff - (row+1) * (feature_width+spacing)
-                  for row in range(row_count)]
     features = sorted(
-        [_merge_over_periodic_boundary(feature, loc_range)
-         for feature in annotation],
+        [
+            _merge_over_periodic_boundary(feature, plasmid_size)
+            for feature in annotation
+        ],
         # Features are sorted by the length of their location range
         # The shortest come first
         key = lambda feature: np.diff(feature.get_location_range())[0],
         reverse = True
     )
-
-    for feature in features:
-        row_bottom = None
-        first, last = feature.get_location_range()
-        for row_i, curr_range in enumerate(ranges_in_row):
-            is_occupied = False
-            if curr_range is not None:
-                # Check if row is occupied
-                for curr_first, curr_last in curr_range:
-                    if first <= curr_last and last >= curr_first:
-                        is_occupied = True
-            if not is_occupied:
-                # Row is not occupied by another feature
-                # in the location range of the new feature
-                # -> Use this row
-                ranges_in_row[row_i].append((first, last))
-                row_bottom = row_bottoms[row_i]
-                break
-        if row_bottom is None:
-            # No free row
-            raise ValueError(
-                "Too many feature overlaps, try to increase the radius "
-                "or decrease the feature width or spacing"
-            )
-        for loc in feature.locs:
-            # Draw features as curved arrows
-            # Calculate arrow shape parameters
-            row_center = row_bottom + feature_width/2
-            row_top = row_bottom + feature_width
-            # Same apparent width for all arrows,
-            # irrespective of the radius of the polar plot
-            head_angle_width = arrow_head_width / row_center
-            start_angle = _loc_to_rad(loc.first, loc_range)
-            stop_angle  = _loc_to_rad(loc.last, loc_range)
-            bbox = Bbox.from_extents(
-                start_angle, row_bottom, stop_angle, row_top
-            )
-            axes.add_artist(Feature_Indicator(
-                axes, 0, feature, loc, bbox, head_angle_width, face_properties,
-                label_properties, feature_formatter
-            ))
-
-
-def _loc_to_rad(loc, loc_range):
-    start = loc_range[0]
-    stop = loc_range[1]
-    return (loc-start) / (stop-start) * 2*np.pi
-
-
-def _rad_to_loc(rad, loc_range):
-    start = loc_range[0]
-    stop = loc_range[1]
-    return rad / (2*np.pi) * (stop-start) + start
-
-
-def _merge_over_periodic_boundary(feature, loc_range):
-    if len(feature.locs) == 1:
-        # Only one location -> no merge possible
-        return feature
-    first_loc = None
-    last_loc  = None
-    for loc in feature.locs:
-        if first_loc is None or loc.first < first_loc.first:
-            first_loc = loc
-    for loc in feature.locs:
-        if last_loc is None or loc.last > last_loc.last:
-            last_loc = loc
-    if first_loc.first == loc_range[0] and last_loc.last == loc_range[1]-1 \
-        and first_loc.strand == last_loc.strand:
-            new_locs = set(feature.locs)
-            new_locs.remove(first_loc)
-            new_locs.remove(last_loc)
-            new_locs.add(Location(
-                first  = loc_range[0] - (loc_range[1] - last_loc.first),
-                last   = first_loc.last,
-                strand = first_loc.strand,
-                defect = first_loc.defect | last_loc.defect
-            ))
-            return Feature(feature.key, new_locs, feature.qual)
-    else:
-        return feature
-
-
-# ' ', '-' and '_' are word delimiters
-separators = re.compile("\s|_|-")
-def _split_into_words(string):
-    match_indices = sorted(
-        [match.start() for match in separators.finditer(string)]
-    )
-    current_index = 0
-    words = []
-    for i in match_indices:
-        # Add word up to delimiter
-        words.append(string[current_index : i])
-        # Add delimiter
-        words.append(string[i : i+1])
-        current_index = i+1
-    # If there is a word after the last delimiter, add it too
-    if current_index < len(string):
-        words.append(string[current_index:])
-    return words
-
-
-def _default_feature_formatter(f):
-    """
-    Returns
-    -------
-    directional : bool
-        True, if the direction of the feature should be indicated by
-        an arrow.
-    face_color: tuple or str, optional
-        A matplotlib compatible color for the feature indicator.
-    label_color: tuple or str, optional
-        A matplotlib compatible color for the feature label.
-    label: str or None
-        The label to be displayed for this feature.
-        None, if no label should be displayed.
-    """
-    # Source
-    if f.key == "source":
-        return False, colors["darkorange"], "white", f.qual.get("organism")
-    
-    # Origin of Replication
-    elif f.key == "rep_origin":
-        return False, colors["dimorange"], "black", \
-               f.qual.get("standard_name", "ori")
-    
-    # Coding sequences
-    elif f.key == "gene":
-        return True, colors["darkgreen"], "black", f.qual.get("gene")
-    elif f.key in ["CDS", "rRNA"]:
-        label = f.qual.get("product")
-        if label is None:
-            label = f.qual.get("gene")
-        return True, colors["darkgreen"], "black", label
-    
-    elif f.key == "regulatory":
-        # Promoters
-        if f.qual.get("regulatory_class") in [
-            "promoter",
-            "TATA_box",
-            "minus_35_signal",
-            "minus_10_signal"
-        ]:
-            return True, colors["green"], "black", f.qual.get("note")
-        
-        # Terminators
-        elif f.qual.get("regulatory_class") in "terminator":
-            return True, "black", "white", f.qual.get("note")
-        
-        # RBS
-        elif f.qual.get("regulatory_class") == "ribosome_binding_site":
-            return True, colors["lightorange"], "white", None
-    
-    # Misc
-    return True, "gray", "black", f.qual.get("gene")
-
+    axes.add_artist(PlasmidMap(
+        axes, 0, features, plasmid_size, radius_eff, feature_width, spacing,
+        arrow_head_width, label, face_properties, label_properties,
+        feature_formatter
+    ))
 
 
 try:
@@ -262,7 +105,89 @@ try:
 
 
     class PlasmidMap(Artist):
-        pass
+        def __init__(self, axes, zorder, features, plasmid_size, radius,
+                     feature_width, spacing, arrow_head_width, label,
+                     face_properties, label_properties, feature_formatter):
+            super().__init__()
+            self._axes = axes
+            self.zorder = zorder
+            self._features = features
+            self._plasmid_size = plasmid_size
+            self._radius = radius
+            self._feature_width = feature_width
+            self._spacing = spacing
+
+            self._all_indicators = []
+            for feature in features:
+                indicators_for_feature = []
+                for loc in feature.locs:
+                     # Set proper positions in 'draw()' method
+                    bbox = None
+                    # Draw features as curved arrows (feature indicator)
+                    indicator = axes.add_artist(Feature_Indicator(
+                        axes, self.zorder + 1, feature, loc, bbox,
+                        arrow_head_width, face_properties, label_properties,
+                        feature_formatter
+                    ))
+                    indicators_for_feature.append(indicator)
+                self._all_indicators.append(indicators_for_feature)
+
+        
+        def draw(self, renderer, *args, **kwargs):
+            # Find the maximum amount of feature rows
+            # (used for overlapping features)
+            row_count = int(
+                self._radius // (self._feature_width + self._spacing)
+            )
+            # Tracks the location ranges of feature that were added to
+            # a row in order to check if that row is occupied
+            ranges_in_row = [[] for i in range(row_count)]
+            # Stores the bottom coordinate (radius) for each row
+            row_bottoms = [
+                self._radius - (row+1) * (self._feature_width + self._spacing)
+                for row in range(row_count)
+            ]
+
+            # Arrange the feature indicators in an way,
+            # that there is no overlap between them
+            for feature, indicators_for_feature in zip(
+                self._features, self._all_indicators
+            ):
+                row_bottom = None
+                first, last = feature.get_location_range()
+                for row_i, curr_range in enumerate(ranges_in_row):
+                    is_occupied = False
+                    if curr_range is not None:
+                        # Check if row is occupied
+                        for curr_first, curr_last in curr_range:
+                            if first <= curr_last and last >= curr_first:
+                                is_occupied = True
+                    if not is_occupied:
+                        # Row is not occupied by another feature
+                        # in the location range of the new feature
+                        # -> Use this row
+                        ranges_in_row[row_i].append((first, last))
+                        row_bottom = row_bottoms[row_i]
+                        break
+                if row_bottom is None:
+                    # No free row -> ignore feature and raise warning
+                    warnings.warn(
+                        "Too many feature overlaps, try to increase the "
+                        "radius or decrease the feature width or spacing"
+                    )
+                else:
+                    for loc, indicator in zip(
+                        feature.locs, indicators_for_feature
+                    ):
+                        # Calculate arrow shape parameters
+                        row_center = row_bottom + self._feature_width/2
+                        row_top = row_bottom + self._feature_width
+                        start_ang = _loc_to_rad(loc.first, self._plasmid_size)
+                        stop_ang  = _loc_to_rad(loc.last, self._plasmid_size)
+                        bbox = Bbox.from_extents(
+                            start_ang, row_bottom, stop_ang, row_top
+                        )
+                        indicator.set_bbox(bbox)
 
 
     class Feature_Indicator(Artist):
@@ -310,22 +235,29 @@ try:
                 ))
             else:
                 self._label = None
+        
+
+        def set_bbox(self, bbox):
+            self._bbox = bbox
 
 
         def draw(self, renderer, *args, **kwargs):
             bbox = self._bbox
             center_x = (bbox.x0 + bbox.x1) / 2
             center_y = (bbox.y0 + bbox.y1) / 2
+
+            # Same width for all arrows
+            # irrespective of the radius of the polar plot
+            # Calculate actual angle width from given absolute width
+            head_width = self._head_width / center_y
             
-            # Check if the feature location is too small for
-            # arrow tail AND head to be drawn
+            # Check if the head should be drawn
             if self._arrow_head is None:
                 head_width = 0
-            elif self._head_width > bbox.width:
+            # Check if the feature location is too small for
+            elif head_width > bbox.width:
                 # Limit size of arrow head to range of location
                 head_width = bbox.width
-            else:
-                head_width = self._head_width
 
             if self._direction == Location.Strand.FORWARD:
                 rect_pos = (bbox.x0, bbox.y0)
@@ -433,3 +365,127 @@ try:
 
 except ImportError:
     pass
+
+
+
+
+def _loc_to_rad(loc, plasmid_size):
+    if loc > plasmid_size:
+        raise ValueError(
+            f"Location {loc} is larger then the plasmid size of {plasmid_size}"
+        )
+    # Location starts at 1 -> (loc-1)
+    return ((loc-1) / plasmid_size) * 2*np.pi
+
+
+def _rad_to_loc(rad, plasmid_size):
+    # Location starts at 1 -> + 1
+    return rad / (2*np.pi) * plasmid_size + 1
+
+
+def _merge_over_periodic_boundary(feature, plasmid_size):
+    if len(feature.locs) == 1:
+        # Only one location -> no merge possible
+        return feature
+    first_loc = None
+    last_loc  = None
+    # Find total first location of the feature
+    for loc in feature.locs:
+        if first_loc is None or loc.first < first_loc.first:
+            first_loc = loc
+    # Find total last location of the feature
+    for loc in feature.locs:
+        if last_loc is None or loc.last > last_loc.last:
+            last_loc = loc
+    # If the first and last location meet at the periodic boundary of
+    # the plasmid -> merge them
+    if first_loc.first == 1 and last_loc.last == plasmid_size \
+        and first_loc.strand == last_loc.strand:
+            new_locs = set(feature.locs)
+            new_locs.remove(first_loc)
+            new_locs.remove(last_loc)
+            new_locs.add(Location(
+                # the fist base is now at negative location
+                first  = 1 - (plasmid_size - last_loc.first),
+                last   = first_loc.last,
+                strand = first_loc.strand,
+                defect = first_loc.defect | last_loc.defect
+            ))
+            return Feature(feature.key, new_locs, feature.qual)
+    else:
+        return feature
+
+
+# ' ', '-' and '_' are word delimiters
+separators = re.compile("\s|_|-")
+def _split_into_words(string):
+    match_indices = sorted(
+        [match.start() for match in separators.finditer(string)]
+    )
+    current_index = 0
+    words = []
+    for i in match_indices:
+        # Add word up to delimiter
+        words.append(string[current_index : i])
+        # Add delimiter
+        words.append(string[i : i+1])
+        current_index = i+1
+    # If there is a word after the last delimiter, add it too
+    if current_index < len(string):
+        words.append(string[current_index:])
+    return words
+
+
+def _default_feature_formatter(f):
+    """
+    Returns
+    -------
+    directional : bool
+        True, if the direction of the feature should be indicated by
+        an arrow.
+    face_color: tuple or str, optional
+        A matplotlib compatible color for the feature indicator.
+    label_color: tuple or str, optional
+        A matplotlib compatible color for the feature label.
+    label: str or None
+        The label to be displayed for this feature.
+        None, if no label should be displayed.
+    """
+    # Source
+    if f.key == "source":
+        return False, colors["darkorange"], "white", f.qual.get("organism")
+    
+    # Origin of Replication
+    elif f.key == "rep_origin":
+        return False, colors["dimorange"], "black", \
+               f.qual.get("standard_name", "ori")
+    
+    # Coding sequences
+    elif f.key == "gene":
+        return True, colors["darkgreen"], "black", f.qual.get("gene")
+    elif f.key in ["CDS", "rRNA"]:
+        label = f.qual.get("product")
+        if label is None:
+            label = f.qual.get("gene")
+        return True, colors["darkgreen"], "black", label
+    
+    elif f.key == "regulatory":
+        # Promoters
+        if f.qual.get("regulatory_class") in [
+            "promoter",
+            "TATA_box",
+            "minus_35_signal",
+            "minus_10_signal"
+        ]:
+            return True, colors["green"], "black", f.qual.get("note")
+        
+        # Terminators
+        elif f.qual.get("regulatory_class") in "terminator":
+            return True, "black", "white", f.qual.get("note")
+        
+        # RBS
+        elif f.qual.get("regulatory_class") == "ribosome_binding_site":
+            return True, colors["lightorange"], "white", None
+    
+    # Misc
+    return True, "gray", "black", f.qual.get("gene")
