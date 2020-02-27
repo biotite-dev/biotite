a8f7e160c6cbf1f92cee8a1e293c41e0f7a8cbd1
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-02-27 11:52:52 +0100

    Bumped minor version

--- src/biotite/__init__.py
+++ src/biotite/__init__.py
@@ -9,7 +9,7 @@ it does provide utilities and base classes used by a lot of *Biotite*'s
 modules.
 """
 
-__version__ = "0.19.2"
+__version__ = "0.20.0"
 __name__ = "biotite"
 __author__ = "Patrick Kunzmann"
 
