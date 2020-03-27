539dc3de24171e78b778dccee6eb7c479a321d49
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-03-27 14:54:05 +0100

    Added 'expect_looped' parameter

--- src/biotite/structure/io/pdbx/file.py
+++ src/biotite/structure/io/pdbx/file.py
@@ -159,7 +159,7 @@ class PDBxFile(TextFile, MutableMapping):
         return sorted(blocks)
     
     
-    def get_category(self, category, block=None):
+    def get_category(self, category, block=None, expect_looped=False):
         """
         Get the dictionary for a given category.
         
@@ -170,14 +170,17 @@ class PDBxFile(TextFile, MutableMapping):
         block : string, optional
             The name of the data block. Default is the first
             (and most times only) data block of the file.
-        
-        This function optionally uses C-extensions.
+        expect_looped : bool, optional
+            If set to true, the returned dictionary will always contain
+            arrays (only if the category exists):
+            If the category is *non-looped*, each array will contain
+            only one element.
             
         Returns
         -------
-        category_dict : dict or None
+        category_dict : dict of (str or ndarray, dtype=str) or None
             A entry keyed dictionary. The corresponding values are
-            strings or `ndarrays` of strings for *non-looped* and
+            strings or array of strings for *non-looped* and
             *looped* categories, respectively.
             Returns None, if the data block does not contain the given
             category.
@@ -240,6 +243,11 @@ class PDBxFile(TextFile, MutableMapping):
         else:
             category_dict = _process_singlevalued(lines)
         
+        if expect_looped:
+            if not is_loop:
+                for key, val in category_dict:
+                    category_dict[key] = np.array([val], dtype=object)
+
         return category_dict
             
     
