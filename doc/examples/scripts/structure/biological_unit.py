73ba4542182f5e264559f17fe6de901b4c68d807
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-03-30 20:03:47 +0200

    Added example

--- doc/examples/scripts/structure/biological_unit.py
+++ doc/examples/scripts/structure/biological_unit.py
@@ -1,8 +1,36 @@
 """
-Biological unit of a structure
-==============================
+Biological assembly of a structure
+==================================
 
-Quaternary structure
+Often the biological assembly (or biological unit) reveals the complete
+picture of a protein function, may it be a viral capsid or a
+microfilament.
+However, the usual records in an *PDB*/*mmCIF*/*MMTF* usually describe
+only the asymmetric unit.
+For large complexes the asymmetric unit may only display one monomer or
+one small subcomplex.
+Multiple copies of the asymmetric unit must be geometrically arranged to
+build the assembly.
+
+In order to get the entire assembly, the *mmCIF* files provided by the
+*RCSB PDB* contain the following fields:
+
+    - ``pdbx_struct_assembly`` - General information about the
+      assemblies
+    - ``pdbx_struct_assembly_prop`` - More specific properties
+    - ``pdbx_struct_oper_list`` - A list of tranformation operations
+    - ``pdbx_struct_assembly_gen`` - Which tranformation operations from
+      ``pdbx_struct_oper_list`` need to be applied for the assemblies
+
+More information about biological assemblies is provided by the
+*RCSB PDB* on
+`this page <https://pdb101.rcsb.org/learn/guide-to-understanding-pdb-data/biological-assemblies>`_.
+
+In this example, we will create the complete biological assembly of the
+capsid from *Paramecium bursaria Chlorella virus type 1*
+- a homo-5040-mer!
+
+At first we well check, which assemblies are available to us.
 """
 
 # Code source: Patrick Kunzmann
@@ -15,70 +43,32 @@ import biotite.structure.io as strucio
 import biotite.database.rcsb as rcsb
 
 
-def transform(coord, struct_oper_list, index):
-    rotation_matrix = np.array([
-        [float(struct_oper_list[f"matrix[{i}][{j}]"][index]) for j in (1,2,3)]
-        for i in (1,2,3)
-    ])
-    translation_vector = np.array([
-        float(struct_oper_list[f"vector[{i}]"][index]) for i in (1,2,3)
-    ])
-    # Rotate
-    coord = np.matmul(rotation_matrix, coord.T).T
-    # Translate
-    coord += translation_vector
-    return coord
-
-
-def repeat(template, coord):
-    orig_length = template.array_length()
-    new_length = coord.shape[-2]
-    if new_length % orig_length != 0:
-        raise ValueError(
-            "The length of the new coordinates must be a multiple of the "
-            "template length"
-        )
-    repetitions = new_length // orig_length
-
-    repeated = struc.AtomArray(new_length)
-    
-    for category in template.get_annotation_categories():
-        annot = np.tile(template.get_annotation(category), repetitions)
-        repeated.set_annotation(category, annot)
-    if template.bonds is not None:
-        bonds = template.bonds
-        for _ in range(repetitions-1):
-            bonds += template.bonds
-        repeated.bonds = bonds
-    if template.box is not None:
-        repeated.box = template.box.copy()
-    repeated.coord = coord
-    
-    return repeated
-
-
 pdbx_file = pdbx.PDBxFile()
 pdbx_file.read(rcsb.fetch("1M4X", "mmcif"))
 
-structure = pdbx.get_structure(pdbx_file, model=1)
-coord = structure.coord
+assemblies = pdbx.get_assembly_list(pdbx_file)
+print("ID    name")
+print()
+for assembly_id, name in assemblies.items():
+    print(f"{assembly_id:2}    {name}")
 
+########################################################################
+# `'complete icosahedral assembly'` sounds good.
+# In fact, often the first assembly is the complete assembly.
+# Hence, the :func:`get_assembly()` function, that we will use the get
+# the chosen assembly as :class:`AtomArray`, builds the first assembly
+# by default.
+# Since we know the assembly ID we want (``'1'``), we will provide it
+# anyway.
+# Note that the assembly ID is a string, not an integer.
 
-struct_oper_list = pdbx_file["pdbx_struct_oper_list"]
+biological_unit = pdbx.get_assembly(pdbx_file, assembly_id="1", model=1)
+print("Number of protein chains:", struc.get_chain_count(biological_unit))
 
-biological_unit_coord = []
-# For each symmetric capsid component, build the asymmetric subunit
-for i, transformation_type_i in enumerate(struct_oper_list["type"]):
-    if transformation_type_i == "build point asymmetric unit":
-        coord_sym = transform(coord, struct_oper_list, i)
-        for j, transformation_type_j in enumerate(struct_oper_list["type"]):
-            if transformation_type_j == "point symmetry operation":
-                coord_asym = transform(coord_sym, struct_oper_list, j)
-                biological_unit_coord.append(coord_asym)
-
-
-biological_unit_coord = np.concatenate(biological_unit_coord, axis=0)
-biological_unit = repeat(structure, biological_unit_coord)
+########################################################################
+# Now we could do some analysis on the assembly.
+# But for this example we will simply save the entire assembly as *PDB*
+# file for later visualization.
 
 # For brevity, save only CA atoms to file for visualization with PyMOL
 #biological_unit = biological_unit[biological_unit.atom_name == "CA"]
