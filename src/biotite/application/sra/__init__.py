2dd5c17072d7891577a1c6a168cc7c8f27ecf4ad
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-08-04 15:57:55 +0200

    Add 'sra-tools' interface

---
+++ src/biotite/application/sra/__init__.py
@@ -0,0 +1,13 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+"""
+A subpackage for obtaining sequencing data from the *NCBI*
+*sequence read archive* (SRA).
+"""
+
+__name__ = "biotite.application.sra"
+__author__ = "Patrick Kunzmann"
+
+from .app import *
---
+++ src/biotite/application/sra/app.py
@@ -0,0 +1,71 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+__name__ = "biotite.application.sra"
+__author__ = "Patrick Kunzmann"
+__all__ = ["FastqDumpApp"]
+
+import glob
+from tempfile import NamedTemporaryFile, gettempdir
+from ..localapp import LocalApp
+from ..application import AppState, requires_state
+from ...sequence.io.fastq.file import FastqFile
+from ...sequence.io.fastq.convert import get_sequences
+
+
+class FastqDumpApp(LocalApp):
+    """
+    """
+    
+    def __init__(self, uid, output_path_prefix=None, bin_path="fasterq-dump"):
+        super().__init__(bin_path)
+        self._uid = uid
+        if output_path_prefix is None:
+            self._out_file = NamedTemporaryFile("r")
+            self._prefix = self._out_file.name
+        else:
+            self._prefix = output_path_prefix
+
+    def run(self):
+        self.set_arguments([
+            "-o", self._prefix,
+            "-t", gettempdir(),
+            "-f",
+            self._uid
+        ])
+        super().run()
+    
+    def evaluate(self):
+        super().evaluate()
+        file_names = (
+            # For entries with one read per spot
+            glob.glob(self._prefix +   ".fastq") + 
+            # For entries with multiple reads per spot
+            glob.glob(self._prefix + "_*.fastq")
+        )
+        self._fastq_files = [
+            FastqFile.read(file_name, offset="Sanger")
+            for file_name in file_names
+        ]
+    
+    def clean_up(self):
+        super().clean_up()
+        self._out_file.close()
+    
+    @requires_state(AppState.JOINED)
+    def get_sequences(self):
+        return [get_sequences(fastq_file) for fastq_file in self._fastq_files]
+
+    @requires_state(AppState.JOINED)
+    def get_fastq(self):
+        return self._fastq_files
+    
+    @staticmethod
+    def fetch(uid, output_path_prefix=None, bin_path="fasterq-dump"):
+        app = FastqDumpApp(
+            uid, output_path_prefix=None, bin_path="fasterq-dump"
+        )
+        app.start()
+        app.join()
+        return app.get_sequences()
