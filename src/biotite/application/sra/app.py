eac97544b7b3245c3b9488b7c84b9d99b265dea5
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-08-18 17:12:39 +0200

    Add docstrings

--- src/biotite/application/sra/app.py
+++ src/biotite/application/sra/app.py
@@ -16,6 +16,28 @@ from ...sequence.io.fastq.convert import get_sequences
 
 class FastqDumpApp(LocalApp):
     """
+    Fetch sequencing data as FASTQ from the *NCBI sequence read archive*
+    (SRA) using *sra-tools*.
+
+    Parameters
+    ----------
+    uid : str
+        A *unique identifier* (UID) of the file to be downloaded.
+    output_path_prefix : str, optional
+        The prefix of the path to store the downloaded FASTQ file.
+        ``.fastq`` is appended to this prefix if the run contains
+        a single read per spot.
+        ``_1.fastq``, `_2.fastq``, etc. is appended if it contains
+        multiple reads per spot.
+        By default, the files are created in a temporary directory and
+        deleted after the files have been read.
+    bin_path : str, optional
+        Path to the ``fasterq-dump`` binary.
+    offset : int or {'Sanger', 'Solexa', 'Illumina-1.3', 'Illumina-1.5', 'Illumina-1.8'}, optional
+        This value is subtracted from the FASTQ ASCII code to obtain the
+        quality score.
+        Can either be directly the value, or a string that indicates
+        the score format.
     """
     
     def __init__(self, uid, output_path_prefix=None, bin_path="fasterq-dump",
@@ -27,8 +49,8 @@ class FastqDumpApp(LocalApp):
             self._out_file = NamedTemporaryFile("r")
             self._prefix = self._out_file.name
         else:
-            self._prefix = output_path_prefix
             self._out_file = None
+            self._prefix = output_path_prefix
 
     def run(self):
         self.set_arguments([
@@ -59,15 +81,74 @@ class FastqDumpApp(LocalApp):
     
     @requires_state(AppState.JOINED)
     def get_sequences(self):
+        """
+        Get the sequences and score values from the downloaded file(s).
+        
+        Returns
+        -------
+        sequences_and_scores : list of dict (str -> (NucleotideSequence, ndarray))
+            This list contains the reads for each spot:
+            The first item contains the first read for each spot, the
+            second item contains the second read for each spot (if existing),
+            etc.
+            Each item in the list is a dictionary mapping identifiers to its
+            corresponding sequence and score values.
+        """
         return [get_sequences(fastq_file) for fastq_file in self._fastq_files]
 
     @requires_state(AppState.JOINED)
     def get_fastq(self):
+        """
+        Get the `FastqFile` objects from the downloaded file(s).
+
+        Returns
+        -------
+        fastq_files : list of FastqFile
+            This list contains the reads for each spot:
+            The first item contains the first read for each spot, the
+            second item contains the second read for each spot (if existing),
+            etc.
+        """
         return self._fastq_files
     
     @staticmethod
-    def fetch(uid, output_path_prefix=None, bin_path="fasterq-dump"):
-        app = FastqDumpApp(uid, output_path_prefix, bin_path)
+    def fetch(uid, output_path_prefix=None, bin_path="fasterq-dump",
+              offset="Sanger"):
+        """
+        Get the sequences and score values belonging to the UID from the
+        *NCBI sequence read archive* (SRA).
+
+        Parameters
+        ----------
+        uid : str
+            A *unique identifier* (UID) of the file to be downloaded.
+        output_path_prefix : str, optional
+            The prefix of the path to store the downloaded FASTQ file.
+            ``.fastq`` is appended to this prefix if the run contains
+            a single read per spot.
+            ``_1.fastq``, `_2.fastq``, etc. is appended if it contains
+            multiple reads per spot.
+            By default, the files are created in a temporary directory and
+            deleted after the files have been read.
+        bin_path : str, optional
+            Path to the ``fasterq-dump`` binary.
+        offset : int or {'Sanger', 'Solexa', 'Illumina-1.3', 'Illumina-1.5', 'Illumina-1.8'}, optional
+            This value is subtracted from the FASTQ ASCII code to obtain the
+            quality score.
+            Can either be directly the value, or a string that indicates
+            the score format.
+        
+        Returns
+        -------
+        sequences_and_scores : list of dict (str -> (NucleotideSequence, ndarray))
+            This list contains the reads for each spot:
+            The first item contains the first read for each spot, the
+            second item contains the second read for each spot (if existing),
+            etc.
+            Each item in the list is a dictionary mapping identifiers to its
+            corresponding sequence and score values.
+        """
+        app = FastqDumpApp(uid, output_path_prefix, bin_path, offset)
         app.start()
         app.join()
         return app.get_sequences()
