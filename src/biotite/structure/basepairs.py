b255da2601a19474458148fd3700bf179496df67
tomtomhdx <tom.mueller@beachouse.de>
2020-11-05 13:09:44 +0100

    bugfix - also remap residue starts to original array if unique=False

--- src/biotite/structure/basepairs.py
+++ src/biotite/structure/basepairs.py
@@ -440,10 +440,10 @@ def base_pairs(atom_array, min_atoms_per_base = 3, unique = True):
         # Remove all flagged basepairs from the output `ndarray`
         basepair_array = np.delete(basepair_array, to_remove, axis=0)
 
-        # Remap values to original atom array
-        basepair_array = np.where(boolean_mask)[0][basepair_array]
-        for i, row in enumerate(basepair_array):
-            basepair_array[i] = get_residue_starts_for(atom_array, row)
+    # Remap values to original atom array
+    basepair_array = np.where(boolean_mask)[0][basepair_array]
+    for i, row in enumerate(basepair_array):
+        basepair_array[i] = get_residue_starts_for(atom_array, row)
 
     return basepair_array
 
