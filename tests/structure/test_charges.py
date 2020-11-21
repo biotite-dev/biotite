423f53ce00ee684a659d588c90a048734174846f
Jacob Anter <anter.jacob@gmail.com>
2020-11-21 18:41:39 +0100

    Adding license

---
+++ tests/structure/test_charges.py
@@ -0,0 +1,7 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+import pytest
+import numpy as np
+from biotite.structure.info import residue
