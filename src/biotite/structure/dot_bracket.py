e127f6c3c603d9d4f16fe3542be9089f833e2e79
tomtomhdx <tom.mueller@beachouse.de>
2020-11-23 17:19:02 +0100

    increassed effiency

--- src/biotite/structure/dot_bracket.py
+++ src/biotite/structure/dot_bracket.py
@@ -11,32 +11,43 @@ __name__ = "biotite.structure"
 __author__ = "Tom David Müller"
 __all__ = ["dot_bracket"]
 
+import numpy as np
 from .basepairs import base_pairs
 from .pseudoknots import pseudoknots
 from .residues import get_residue_starts
 
-OPENING_BRACKETS = "([<ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-CLOSING_BRACKETS = ")]>abcdefghijklmnopqrstuvwxyz"
+_OPENING_BRACKETS = "([<ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+_CLOSING_BRACKETS = ")]>abcdefghijklmnopqrstuvwxyz"
 
 
 def dot_bracket(nucleic_acid_strand):
+    """
+    Convert structural information
+
+    Args:
+        nucleic_acid_strand ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
     basepairs = base_pairs(nucleic_acid_strand)
     pseudoknot_order = pseudoknots(basepairs)
+    residue_starts = get_residue_starts(nucleic_acid_strand)
 
     notation = [""]*len(pseudoknot_order)
     for s, solution in enumerate(pseudoknot_order):
         opened_brackets = set()
-        for residue_start in get_residue_starts(nucleic_acid_strand):
+        for residue_start in residue_starts:
             if residue_start not in basepairs:
                 notation[s] += "."
             else:
                 # Get position in ``basepairs`` and ``pseudoknot_order``
                 pos = np.where(basepairs == residue_start)[0][0]
                 if residue_start in opened_brackets:
-                    notation[s] += CLOSING_BRACKETS[solution[pos]]
+                    notation[s] += _CLOSING_BRACKETS[solution[pos]]
                 else:
                     for base in basepairs[pos]:
                         opened_brackets.add(base)
-                    notation[s] += OPENING_BRACKETS[solution[pos]]
+                    notation[s] += _OPENING_BRACKETS[solution[pos]]
 
     return notation
