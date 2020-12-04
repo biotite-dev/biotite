a96f92873b7da2fafd5927d46d33a3623ebd653d
tomtomhdx <tom.mueller@beachouse.de>
2020-12-04 15:37:44 +0100

    add docstrings

--- src/biotite/application/viennarna/rnaplot/app.py
+++ src/biotite/application/viennarna/rnaplot/app.py
@@ -16,6 +16,42 @@ from ....sequence import NucleotideSequence
 from ....structure.dotbracket import dot_bracket
 
 class RNAplotApp(LocalApp):
+    """
+    Get coordinates for a 2d representation of any unknotted RNA
+    structure using ViennaRNA's RNAplot.
+
+    The structure has to be provided either in dot bracket notation or
+    as a ``ndarray`` of base pairs and the total sequence length.
+
+    Internally this creates a :class:`Popen` instance, which handles
+    the execution.
+
+    Parameters
+    ----------
+    dot_bracket : str, optional (default: None)
+        The structure in dot bracket notation.
+    basepairs : ndarray, shape=(n,2), optional (default: None)
+        Each row corresponds to the positions of the bases in the
+        strand.
+    length : int, optional (default: None)
+        The number of bases in the strand.
+    bin_path : str, optional
+        Path of the RNAplot binary.
+
+    Examples
+    --------
+
+    >>> app = RNAplotApp('((..))')
+    >>> app.start()
+    >>> app.join()
+    >>> print(app.get_coordinates())
+    [[ -92.5 ,   92.5 ],
+     [ -92.5 ,   77.5 ],
+     [ -90.31,   58.24],
+     [-109.69,   58.24],
+     [-107.5 ,   77.5 ],
+     [-107.5 ,   92.5 ]]
+    """
 
     def __init__(self, dot_bracket = None, base_pairs=None, length = None,
                  bin_path="RNAplot"):
@@ -23,7 +59,6 @@ class RNAplotApp(LocalApp):
 
         if dot_bracket is not None:
             self._dot_bracket = dot_bracket
-
         elif (base_pairs is not None) and (length is not None):
             self._dot_bracket = dot_bracket(
                 base_pairs, length, max_pseudoknot_depth = 0
@@ -54,11 +89,62 @@ class RNAplotApp(LocalApp):
 
     @requires_state(AppState.JOINED)
     def get_coordinates(self):
+        """
+        Get coordinates for a 2d representation of the input structure.
+
+        Returns
+        -------
+        coordinates : ndarray, shape=(n,2)
+            The 2d coordinates. Each row represents the *x* and *y*
+            coordinates for a total sequence length of *n*.
+
+        Examples
+        --------
+
+        >>> app = RNAplotApp('((..))')
+        >>> app.start()
+        >>> app.join()
+        >>> print(app.get_coordinates())
+        [[ -92.5 ,   92.5 ],
+        [ -92.5 ,   77.5 ],
+        [ -90.31,   58.24],
+        [-109.69,   58.24],
+        [-107.5 ,   77.5 ],
+        [-107.5 ,   92.5 ]]
+        """
         return self._coordinates
 
     @staticmethod
     def compute_coordinates(dot_bracket = None, base_pairs=None, length = None,
                             bin_path="RNAplot"):
+        """
+        Get coordinates for a 2d representation of any unknotted RNA
+        structure using ViennaRNA's RNAplot.
+
+        The structure has to be provided either in dot bracket notation
+        or as a ``ndarray`` of base pairs and the total sequence length.
+
+        This is a convenience function, that wraps the
+        :class:`RNAplotApp` execution.
+
+        Parameters
+        ----------
+        dot_bracket : str, optional (default: None)
+            The structure in dot bracket notation.
+        basepairs : ndarray, shape=(n,2), optional (default: None)
+            Each row corresponds to the positions of the bases in the
+            strand.
+        length : int, optional (default: None)
+            The number of bases in the strand.
+        bin_path : str, optional
+            Path of the RNAplot binary.
+
+        Returns
+        -------
+        coordinates : ndarray, shape=(n,2)
+            The 2d coordinates. Each row represents the *x* and *y*
+            coordinates for a total sequence length of *n*.
+        """
         app = RNAplotApp(dot_bracket=dot_bracket, base_pairs=base_pairs,
                          length=length, bin_path=bin_path)
         app.start()
