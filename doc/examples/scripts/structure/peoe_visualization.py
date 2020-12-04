dc0f01addc3c31a16ac98688b65a5403730fa33b
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-12-04 14:38:24 +0100

    Finished example script

--- doc/examples/scripts/structure/peoe_visualization.py
+++ doc/examples/scripts/structure/peoe_visualization.py
@@ -25,17 +25,27 @@ import biotite.structure.info as info
 import biotite.structure.graphics as graphics
 
 
+#Acetylsalicylic acid
 MOLECULE_NAME = "AIN"
 
+# The number of iterations for the PEOE algorithm
 ITERATION_NUMBER = 6
+# The size of the element lables
 ELEMENT_FONT_SIZE = 10
-BALL_SCALE = 500
-RAY_SCALE = 8000
+# The scaling factor of the atom 'balls'
+BALL_SCALE = 20
+# The higher this number, the more detailed are the rays
+N_RAY_STEPS = 20
+# The scaling factor of the 'ray' of charged molecules
+RAY_SCALE = 100
+# The transparency value for each 'ray ring'
+RAY_ALPHA = 0.03
+# The color map to use to depict the charge
 CMAP_NAME = "bwr_r"
 
 
+
 # Get an atom array for the selected molecule
-# Caffeine has the PDB reside name 'CFF'
 molecule = info.residue(MOLECULE_NAME)
 
 # Align molecule with principal component analysis:
@@ -50,6 +60,8 @@ molecule = struc.align_vectors(molecule, pca.components_[-1], [0, 0, 1])
 charges = struc.partial_charges(molecule, ITERATION_NUMBER)
 # Later this variable stores values between 0 and 1 for use in color map
 normalized_charges = charges.copy()
+# Show no partial charged for atoms
+# that are not parametrized for the PEOE algorithm
 normalized_charges[np.isnan(normalized_charges)] = 0
 # Norm charge values to highest absolute value
 max_charge = np.max(np.abs(normalized_charges))
@@ -65,15 +77,27 @@ ball_sizes = np.array(
     [info.vdw_radius_single(e) for e in molecule.element]
 ) * BALL_SCALE
 
+# Gradient of ray strength
+ray_full_sizes = ball_sizes + np.abs(charges) * RAY_SCALE   
+ray_sizes = np.array([
+    np.linspace(ray_full_sizes[i], ball_sizes[i], N_RAY_STEPS, endpoint=False)
+    for i in range(molecule.array_length())
+]).T
 
 
+# The plotting begins here
 fig = plt.figure(figsize=(8.0, 6.0))
 ax = fig.add_subplot(111, projection="3d")
+
+# Plot the atoms
+# As `axes.scatter` uses sizes in points**2,
+# the VdW-radii as also squared
 graphics.plot_ball_and_stick_model(
-    ax, molecule, colors, ball_size=ball_sizes, line_width=3,
+    ax, molecule, colors, ball_size=ball_sizes**2, line_width=3,
     line_color=color_map(0.5), background_color=(.05, .05, .05), zoom=1.5
 )
 
+# Plot the element labels
 for atom in molecule:
     ax.text(
         *atom.coord, atom.element,
@@ -81,16 +105,14 @@ for atom in molecule:
         ha="center", va="center", zorder=100
     )
 
-# Gradient of ray strength
-ray_full_size = ball_sizes + np.abs(charges) * RAY_SCALE
-ray_half_size = ball_sizes + np.abs(charges) * RAY_SCALE/2
-ax.scatter(
-    *molecule.coord.T, s=ray_full_size, c=colors, linewidth=0, alpha=0.2
-)
-ax.scatter(
-    *molecule.coord.T, s=ray_half_size, c=colors, linewidth=0, alpha=0.2
-)
+# Plots the rays
+for i in range(N_RAY_STEPS):
+    ax.scatter(
+        *molecule.coord.T, s=ray_sizes[i]**2, c=colors,
+        linewidth=0, alpha=RAY_ALPHA
+    )
 
+# Plot the colorbar
 color_bar = fig.colorbar(ScalarMappable(
     norm=Normalize(vmin=-max_charge, vmax=max_charge),
     cmap=color_map
