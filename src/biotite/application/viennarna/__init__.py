81e8f6661b679ba647a14ed06f05eeb87f8b3227
tomtomhdx <tom.mueller@beachouse.de>
2021-01-06 08:17:59 +0100

    add init.py

---
+++ src/biotite/application/viennarna/__init__.py
@@ -0,0 +1,10 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+"""
+A subpackage that provides interfaces to the ViennaRNA software package.
+"""
+
+__name__ = "biotite.application.viennarna"
+__author__ = "Tom David Müller"
