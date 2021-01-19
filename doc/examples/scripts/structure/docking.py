0b63d2c3546f4de96d7a6e03ea462ec0fb21a1ea
Patrick Kunzmann <patrick.kunzm@gmail.com>
2021-01-19 14:54:14 +0100

    Finish docking example

--- doc/examples/scripts/structure/docking.py
+++ doc/examples/scripts/structure/docking.py
@@ -2,7 +2,29 @@
 Docking biotin to streptavidin
 ==============================
 
-Lore ipsum...
+This example shows how to use *AutoDock Vina* [1]_ from within *Biotite*
+for docking a ligand molecule to a known receptor structure.
+As example, we examine the famous streptavidin-biotin interaction.
+
+At first we download a high resolution structure of the complex.
+The resolution is important here:
+For one thing, the docking procedure requires annotated hydrogen atoms
+for the receptor molecule, which seldom is the case for low resolution
+X-ray structures.
+On the other hand, we would like to have a reliable reference binding
+mode of the ligand, to evaluate how well out docking procedure went
+
+After separation of the receptor and the reference ligand,
+a biotin model is loaded from the chemical components dictionary and
+docked into the binding cavity of streptavidin.
+
+Finally, the docked model is compared to the reference model, with
+respect to their RMSD.
+
+.. [1] O Trott and AJ Olson,
+   "AutoDock Vina: improving the speed and accuracy of docking with a
+   new scoring function, efficient optimization and multithreading."
+   J Comput Chem, 31, 455-461 (2010).
 """
 
 # Code source: Patrick Kunzmann
@@ -19,18 +41,29 @@ import biotite.database.rcsb as rcsb
 import biotite.application.autodock as autodock
 
 
+# Get the receptor structure
+# and the original 'correct' confromation of the ligand
 mmtf_file = mmtf.MMTFFile.read(rcsb.fetch("2RTG", "mmtf"))
 structure = mmtf.get_structure(
-    # Include formal charge for accurate partial charge calc
-    mmtf_file, model=1, include_bonds=True, extra_fields=["charge"])
+    # Include formal charge for accurate partial charge calculation
+    mmtf_file, model=1, include_bonds=True, extra_fields=["charge"]
+)
+# The asymmetric unit describes a streptavidin homodimer
+# However, we are only interested in a single monomer
 structure = structure[structure.chain_id == "B"]
 receptor = structure[struc.filter_amino_acids(structure)]
+
 ref_ligand = structure[structure.res_name == "BTN"]
 ref_ligand_center = struc.centroid(ref_ligand)
 
+
+# Independently, get the ligand without optimized conformation
+# from the chemical components dictionary
 ligand = info.residue("BTN")
 
-app = autodock.VinaApp(ligand, receptor, ref_ligand_center, [10, 10, 10])
+# Search for a binding mode in a 20 Å radius
+# of the original ligand position
+app = autodock.VinaApp(ligand, receptor, ref_ligand_center, [20, 20, 20])
 # For reproducibility
 app.set_seed(0)
 # This is the maximum number:
@@ -39,19 +72,31 @@ app.set_seed(0)
 app.set_number_of_models(100)
 # Effectively no limit
 app.set_energy_range(100.0)
+# Start docking run
 app.start()
 app.join()
 docked_coord = app.get_coord()
 energies = app.get_energies()
 
+# Create an AtomArrayStack for alle docked binding modes
 docked_ligand = struc.from_template(ligand, docked_coord)
+# As Vina discards all non-polar hydrogen atoms, their respective
+# coordinates are NaN -> remove these atoms
 docked_ligand = docked_ligand[
     ..., ~np.isnan(docked_ligand.coord[0]).any(axis=-1)
 ]
+
+
+# For comparison of the docked pose with the experimentally determined
+# reference conformation, the atom order of both must be exactly the
+# same
+# Therefore, all atoms, that are additional in one of both models,
+# e.g. carboxy or non-polar hydrogen atoms, are removed...
 docked_ligand = docked_ligand[
     ..., np.isin(docked_ligand.atom_name, ref_ligand.atom_name)
 ]
 docked_ligand = docked_ligand[..., info.standardize_order(docked_ligand)]
+# ...and the atom order is standardized
 ref_ligand = ref_ligand[np.isin(ref_ligand.atom_name, docked_ligand.atom_name)]
 ref_ligand = ref_ligand[info.standardize_order(ref_ligand)]
 
@@ -62,8 +107,6 @@ rmsd = struc.rmsd(ref_ligand, docked_ligand)
 
 # Evaluate correlation between RMSD and binding energies 
 correlation, p_value = spearmanr(energies, rmsd)
-print(correlation)
-print(p_value)
 
 figure, ax = plt.subplots(figsize=(8.0, 6.0))
 ax.set_title(f"$r_s$ = {correlation:.2f} ($p$ = {p_value*100:.1f}%)")
@@ -71,47 +114,39 @@ ax.scatter(energies, rmsd, marker="+", color="black")
 ax.set_xlabel("Energy (kcal/mol)")
 ax.set_ylabel("RMSD (Å)")
 figure.tight_layout()
+plt.show()
 
 ########################################################################
-# For this specific case *AutoDock Vina* shows a low but significant
-# correlation between the RMSD of the calculated models to the correct
-# binding mode and the associated calculated binding energy.
+# For this specific case *AutoDock Vina* shows only a low Spearman
+# correlation between the RMSD of the calculated models to the
+# correct binding mode and the associated calculated binding energy.
 # A high correlation is desireable to ensure that docking results with
 # good binding energies correspond to the correct binding mode for cases
 # in which the correct binding conformation is unknown.
-# Most importantly, the calculated model with the lowest energy is also
+# However, the calculated model with the lowest energy is also
 # the conformation with the lowest deviation from the experimental
 # result in this instance.
+# Hence, *AutoDock Vina* was able to predict an almost correct
+# binding mode as its best *guess*.
 #
 # In a final step, we want to visually compare the experimentally
 # determined conformation of biotin in the binding pocket with the
 # minimum-energy docked conformation, which is also the conformation
-# with the lowest RMSD in this case. 
+# with the lowest RMSD in this case.
+# The docked conformation is shown as ball-and-stick model, the original
+# experimentally determined biotin conformation is shown in transparent
+# blue. 
 
 
-best_model = docked_ligand[np.argmin(rmsd)]
+# Get the best fitting model,
+# i.e the model with the lowest RMSD to the reference conformation
+docked_ligand = docked_ligand[np.argmin(rmsd)]
 
-ref_ligand.bonds = struc.connect_via_residue_names(ref_ligand)
-ref_ligand.res_id[:] = 1
-best_model.res_id[:] = 2
-merged = ref_ligand + best_model
+# Vina only keeps polar hydrogens in the modeled structure
+# For consistency, remove all hydrogen atoms in the reference and
+# modelled structure
+ref_ligand = ref_ligand[ref_ligand.element!= "H"]
+docked_ligand = docked_ligand[docked_ligand.element!= "H"]
 
-# Color by element, except the C-atoms, which are colored by molecule
-colors = np.zeros((merged.array_length(), 3))
-colors[merged.element == "H"] = (0.8, 0.8, 0.8) # Gray
-colors[merged.element == "N"] = (0.0, 0.0, 0.8) # Blue
-colors[merged.element == "O"] = (0.8, 0.0, 0.0) # Red
-colors[merged.element == "S"] = (0.8, 0.8, 0.0) # Yellow
-# Green for reference ligand carbon atoms
-colors[(merged.element == "C") & (merged.res_id == 1)] = (0.0, 0.8, 0.0)
-# Cyan for docked ligand carbon atoms
-colors[(merged.element == "C") & (merged.res_id == 2)] = (0.0, 0.8, 0.8)
-
-fig = plt.figure(figsize=(8.0, 8.0))
-ax = fig.add_subplot(111, projection="3d")
-graphics.plot_atoms(
-    ax, merged, colors, line_width=3, background_color="white",
-    zoom=1.5
-)
-fig.tight_layout()
-plt.show()
+# sphinx_gallery_thumbnail_number = 2
+# Visualization with PyMOL...
