590f788ac67c6531063caf8a2f3f2f9974c6181c
Patrick Kunzmann <patrick.kunzm@gmail.com>
2021-05-17 12:22:38 +0200

    Add example

--- doc/examples/scripts/structure/xkcd_formula.py
+++ doc/examples/scripts/structure/xkcd_formula.py
@@ -1,8 +1,25 @@
+"""
+Structural formula of a small molecule in xkcd style
+====================================================
+
+This script draws a structural formula of a molecule from an
+:class:`AtomArray`, by using the bond, element and charge information.
+The layout of the diagram is computed via *NetworkX*' *Kamada–Kawai*
+implementation.
+The *Matplotlib* *xkcd* style distracts from the fact that used graph
+layout algorithm is poorly suitable for structural formulas, so the
+application of this example is probaby in rather non-serious contexts.
+"""
+
+
+# Code source: Patrick Kunzmann
+# License: BSD 3 clause
+
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import biotite.structure as struc
-import biotite.structure.io.pdbx as pdbx
+import biotite.structure.info as info
 
 
 SEPARATION = 0.01
@@ -14,7 +31,20 @@ COLORS = {
 }
 
 
+BOND_ORDER = {
+    struc.BondType.SINGLE: 1,
+    struc.BondType.DOUBLE: 2,
+    struc.BondType.TRIPLE: 3,
+    struc.BondType.QUADRUPLE: 4,
+    struc.BondType.AROMATIC_SINGLE: 1,
+    struc.BondType.AROMATIC_DOUBLE: 2,
+}
+
+
 def split_bond(i_pos, j_pos, separation, number):
+    """
+    Get line start and end positions for double, triple, etc. bonds.
+    """
     if number == 1:
         return [(i_pos, j_pos)]
     
@@ -26,86 +56,74 @@ def split_bond(i_pos, j_pos, separation, number):
         for p in np.linspace(-number/2, number/2, number)
     ]
 
+# Cyanidin, partly responsible for the red color in roses
+molecule = info.residue("HWB")
 
-def get_residue(components_file, res_name):
-    BOND_ORDERS = {
-        "SING" : struc.BondType.SINGLE,
-        "DOUB" : struc.BondType.DOUBLE,
-        "TRIP" : struc.BondType.TRIPLE,
-        "QUAD" : struc.BondType.QUADRUPLE
-    }
+# Combine an heteroatom + hydrogen in the label
+labels = molecule.element.copy()
+for i, j, _ in molecule.bonds.as_array():
+    if molecule.element[i] == "H":
+        labels[j] += "H"
+    if molecule.element[j] == "H":
+        labels[i] += "H"
+# Add charge to heteroatom labels
+for i in range(molecule.array_length()):
+    if molecule.charge[i] > 0:
+        labels[i] += "+"
+    if molecule.charge[i] < 0:
+        labels[i] += "-"
 
-    cif_atoms = components_file.get_category("chem_comp_atom", block=res_name)
-    cif_bonds = components_file.get_category("chem_comp_bond", block=res_name)
+# Do not show hydrogen atoms
+hydrogen_mask = (molecule.element == "H")
+molecule = molecule[~hydrogen_mask]
+labels = labels[~hydrogen_mask]
 
-    array = struc.AtomArray(len(list(cif_atoms.values())[0]))
-
-    array.res_name = cif_atoms["comp_id"]
-    array.atom_name = cif_atoms["atom_id"]
-    array.element = cif_atoms["type_symbol"]
-    array.charge = cif_atoms["charge"]
-    array.hetero[:] = True
-    
-    array.coord[:,0] = cif_atoms["pdbx_model_Cartn_x_ideal"]
-    array.coord[:,1] = cif_atoms["pdbx_model_Cartn_y_ideal"]
-    array.coord[:,2] = cif_atoms["pdbx_model_Cartn_z_ideal"]
-        
-    bonds = struc.BondList(array.array_length())
-    if cif_bonds is not None:
-        for atom1, atom2, order, aromatic_flag in zip(
-            cif_bonds["atom_id_1"], cif_bonds["atom_id_2"],
-            cif_bonds["value_order"], cif_bonds["pdbx_aromatic_flag"]
-        ):
-            atom_i = np.where(array.atom_name == atom1)[0][0]
-            atom_j = np.where(array.atom_name == atom2)[0][0]
-            bond_type = BOND_ORDERS[order]
-            bonds.add_bond(atom_i, atom_j, bond_type)
-    array.bonds = bonds
-
-    return array
-
-
-components_file = pdbx.PDBxFile.read("/home/kunzmann/downloads/components.cif")
-molecule = get_residue(components_file, "CFF")
-#molecule = molecule[molecule.element != "H"]
-
+# Arrange atoms in 2D space
 graph = nx.Graph()
 graph.add_edges_from(
-    [(i, j, {"order": int(order)})
-     for i, j, order in molecule.bonds.as_array()]
+    [(i, j, {"bond_type": int(bond_type)})
+     for i, j, bond_type in molecule.bonds.as_array()]
 )
 pos = nx.kamada_kawai_layout(graph)
+pos = {i: np.flip(p) for i, p in pos.items()}
 
 
+# Create the diagram
 with plt.xkcd(scale=1, length=100, randomness=2):
-    fig, ax = plt.subplots(figsize=(8.0, 8.0))
+    fig, ax = plt.subplots(figsize=(6.0, 8.0))
     ax.set_aspect("equal")
     ax.axis("off")
-    ax.set_title("KEEPS THE WORLD GOING...", fontsize=30)
+    ax.set_title(
+        "ROSES ARE RED,\n"
+        "VIOLETS ARE BLUE,\n"
+        "AND ANTHOCYANINS\n"
+        "GIVE THEM THEIR HUE",
+        fontsize=30
+    )
     
+    # Plot the bonds
     for i, j in graph.edges():
         i_pos = pos[i]
         j_pos = pos[j]
-        order = graph.edges[i,j]["order"]
+        order = BOND_ORDER[graph.edges[i,j]["bond_type"]]
         for i_pos, j_pos in split_bond(i_pos, j_pos, SEPARATION, order):
             x_data = (i_pos[0], j_pos[0])
             y_data = (i_pos[1], j_pos[1])
             ax.plot(x_data, y_data, color="black")
-    
+
+    # Plot the heteroatom labels
     for i in graph.nodes():
-        symbol = molecule.element[i]
-        if symbol != "C":
+        if molecule.element[i] != "C":
             x = pos[i][0]
             y = pos[i][1]
-            ax.plot(
-                [x], [y],
-                marker="o", markersize=NODE_SIZE, color="white"
-            )
-            ax.text(
-                x, y, symbol,
+            t = ax.text(
+                x, y, labels[i],
                 fontsize=NODE_SIZE, ha="center", va="center",
-                color=COLORS.get(symbol, "black")
+                color=COLORS.get(molecule.element[i], "black")
             )
+            # Remove clashes of the label with bond lines
+            # by setting a white background behind the label
+            t.set_bbox({'pad': 0, 'color': 'white'})
     
     fig.tight_layout()
 
