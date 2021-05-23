874bd00c59352a0f875a3aaadf7e1ebfedd04d53
MaxGreil <maximilian_greil@web.de>
2021-05-23 11:05:25 +0200

    Typo

--- src/biotite/database/uniprot/check.py
+++ src/biotite/database/uniprot/check.py
@@ -2,7 +2,7 @@
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
-__name__ = "biotite.database.entrez"
+__name__ = "biotite.database.uniprot"
 __author__ = "Maximilian Greil"
 __all__ = ["assert_valid_response"]
 
