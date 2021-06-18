e66f90ade7fe0a2747a6b5435a991d4828cbd121
Patrick Kunzmann <patrick.kunzm@gmail.com>
2021-06-18 12:36:31 +0200

    Test affine gap penalties

--- src/biotite/sequence/align/localgapped.pyx
+++ src/biotite/sequence/align/localgapped.pyx
@@ -34,23 +34,6 @@ ctypedef fused CodeType2:
     #uint64
 
 
-# See tracetable.pyx for more information
-DEF MATCH    = 1
-DEF GAP_LEFT = 2
-DEF GAP_TOP  = 4
-DEF MATCH_TO_MATCH       = 1
-DEF GAP_LEFT_TO_MATCH    = 2
-DEF GAP_TOP_TO_MATCH     = 4
-DEF MATCH_TO_GAP_LEFT    = 8
-DEF GAP_LEFT_TO_GAP_LEFT = 16
-DEF MATCH_TO_GAP_TOP     = 32
-DEF GAP_TOP_TO_GAP_TOP   = 64
-DEF NO_STATE       = 0
-DEF MATCH_STATE    = 1
-DEF GAP_LEFT_STATE = 2
-DEF GAP_TOP_STATE  = 3
-
-
 DEF INIT_SIZE = 100
 
 
@@ -212,18 +195,26 @@ def _align_region(code1, code2, matrix, threshold, gap_penalty,
 
     # Table filling
     ###############
-    score_table = np.zeros(init_size, dtype=np.int32)
     # Set the initial (upper left) score value to 'threshold + 1',
     # to be able to use '0' as minus infinity value
     init_score = threshold + 1
-    # This implementation does not initialize the entire first
-    # row/column to avoid issues with premature pruning in the table
-    # filling process
-    score_table[0,0] = init_score
-    
     if affine_penalty:
-        raise NotImplementedError()
+        m_table = np.zeros(init_size, dtype=np.int32)
+        g1_table = np.zeros(init_size, dtype=np.int32)
+        g2_table = np.zeros(init_size, dtype=np.int32)
+        # This implementation does not initialize the entire first
+        # row/column to avoid issues with premature pruning in the table
+        # filling process
+        m_table[0,0]  = init_score
+        g1_table[0,0] = init_score
+        g2_table[0,0] = init_score
+        trace_table, m_table, g1_table, g2_table = _fill_align_table_affine(
+            code1, code2, matrix, trace_table, m_table, g1_table, g2_table,
+            threshold, gap_penalty[0], gap_penalty[1], score_only
+        )
     else:
+        score_table = np.zeros(init_size, dtype=np.int32)
+        score_table[0,0] = init_score
         trace_table, score_table = _fill_align_table(
             code1, code2, matrix, trace_table, score_table, threshold,
             gap_penalty, score_only
@@ -231,7 +222,15 @@ def _align_region(code1, code2, matrix, threshold, gap_penalty,
     
     # If only the score is desired, the traceback is not necessary
     if score_only:
-        max_score = np.max(score_table)
+        if affine_penalty:
+            # The maximum score in the gap score tables do not need to
+            # be considered, as these starting positions would indicate
+            # that the alignment starts with a gap
+            # Hence the maximum score value in these tables is always
+            # less than in the match table
+            max_score = np.max(m_table)
+        else:
+            max_score = np.max(score_table)
         # The initial score needs to be subtracted again,
         # since it was artificially added for convenience resaons
         return max_score - init_score, None
@@ -253,7 +252,10 @@ def _align_region(code1, code2, matrix, threshold, gap_penalty,
     # Multiple starting points possible,
     # when duplicates of maximal score exist 
     if affine_penalty:
-        raise NotImplementedError()
+        # Only consicder match table (see reason above)
+        max_score = np.max(m_table)
+        i_list, j_list = np.where((m_table == max_score))
+        state_list = np.append(state_list, np.full(len(i_list), 1))
     else:
         max_score = np.max(score_table)
         i_list, j_list = np.where((score_table == max_score))
@@ -311,7 +313,6 @@ def _fill_align_table(CodeType1[:] code1 not None,
                       bint score_only):
     """
     """
-    
     cdef int i, j, k=0
     # The ranges for i in the current (k=0) 
     # and previous (k=1, k=2) antidiagonals, that point to valid cells
@@ -375,9 +376,11 @@ def _fill_align_table(CodeType1[:] code1 not None,
 
         for i in range(i_min, i_max+1):
             j = k - i
+            
             # Evaluate score from diagonal direction
             if i != 0 and j != 0:
                 from_diag = score_table[i-1, j-1]
+                # Check if score stems from cells that are valid
                 if from_diag != 0:
                     # -1 in sequence index is necessary
                     # due to the shift of the sequences
@@ -404,9 +407,9 @@ def _fill_align_table(CodeType1[:] code1 not None,
                     from_diag, from_left, from_top, &score
                 )
             
-            if score > max_score:
-                max_score = score
-                req_score = max_score - threshold
+            # Check if the obtained score reaches the required threshold
+            # and if they even exceed the maximum score
+            if score >= req_score:
                 if i_min_k_0 == k:
                     # 'i_min_k_0 == k'
                     # -> i_min_k_0 has not been set in this iteration, yet
@@ -415,16 +418,198 @@ def _fill_align_table(CodeType1[:] code1 not None,
                 score_table[i,j] = score
                 if not score_only:
                     trace_table[i,j] = trace
-            elif score >= req_score:
-                if i_min_k_0 == k:
-                    i_min_k_0 = i
-                i_max_k_0 = i
-                score_table[i,j] = score
-                if not score_only:
-                    trace_table[i,j] = trace
+                if score > max_score:
+                    max_score = score
+                    req_score = max_score - threshold
+                
     
     return np.asarray(trace_table)[:i_max_total+1, :j_max_total+1], \
            np.asarray(score_table)[:i_max_total+1, :j_max_total+1]
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def _fill_align_table_affine(CodeType1[:] code1 not None,
+                             CodeType2[:] code2 not None,
+                             const int32[:,:] matrix not None,
+                             uint8[:,:] trace_table not None,
+                             int32[:,:] m_table not None,
+                             int32[:,:] g1_table not None,
+                             int32[:,:] g2_table not None,
+                             int32 threshold,
+                             int32 gap_open,
+                             int32 gap_ext,
+                             bint score_only):
+    """
+    """
+    cdef int i, j, k=0
+    # The ranges for i in the current (k=0) 
+    # and previous (k=1, k=2) antidiagonals, that point to valid cells
+    cdef int i_min_k_0=0, i_max_k_0=0
+    cdef int i_min_k_1=0, i_max_k_1=0
+    cdef int i_min_k_2=0, i_max_k_2=0
+    # The pruned range for i and j in the current antidiagonal,
+    # calculated from the previous antidiagonals
+    cdef int i_min, i_max
+    cdef int j_max
+    # The maximum values for i and j ever encountered while iterating
+    # over the antidiagonals -> used for final trimming of tables
+    cdef int i_max_total=0, j_max_total=0
+
+    cdef uint8 trace = 0
+    cdef bint is_valid_cell
+
+    cdef int32 mm_score, g1m_score, g2m_score
+    cdef int32 mg1_score, g1g1_score
+    cdef int32 mg2_score, g2g2_score
+    cdef int32 m_score, g1_score, g2_score
+    cdef int32 similarity_score
+    cdef int32 max_score = m_table[0, 0]
+    cdef int32 req_score = max_score - threshold
+
+    # Instead of iteration over row and column,
+    # iterate over antidiagonals and diagonals to achieve symmetric
+    # treatment of both sequences
+    for k in range(1, code1.shape[0] + code2.shape[0] + 1):
+        # Prepare values for iteration
+        i_min_k_2 = i_min_k_1
+        i_max_k_2 = i_max_k_1
+        i_min_k_1 = i_min_k_0
+        i_max_k_1 = i_max_k_0
+        # Reset values for iteration to most 'restrictive' values
+        # These restrictive values are overwritten in the next iteration
+        # if valid cells are present
+        i_min_k_0 = k
+        i_max_k_0 = 0
+
+        # Prune index range for antidiagonal
+        # to range where valid cells exist
+        i_min = _min(i_min_k_1,     i_min_k_2 + 1)
+        i_max = _max(i_max_k_1 + 1, i_max_k_2 + 1)
+        # The index must also not be out of sequence range
+        i_min = _max(i_min, k - code2.shape[0])
+        i_max = _min(i_max, code1.shape[0])
+        # The algorithm has finished,
+        # if the calculated antidiagonal has no range of valid cells
+        if i_min > i_max:
+            break
+        
+        j_max = k - i_min
+        # Expand ndarrays
+        # if their size would be exceeded in the following iteration
+        if i_max >= m_table.shape[0]:
+            m_table  = _extend_table(np.asarray(m_table),  0)
+            g1_table = _extend_table(np.asarray(g1_table), 0)
+            g2_table = _extend_table(np.asarray(g2_table), 0)
+            if not score_only:
+                trace_table = _extend_table(np.asarray(trace_table), 0)
+        if j_max >= m_table.shape[1]:
+            m_table  = _extend_table(np.asarray(m_table),  1)
+            g1_table = _extend_table(np.asarray(g1_table), 1)
+            g2_table = _extend_table(np.asarray(g2_table), 1)
+            if not score_only:
+                trace_table = _extend_table(np.asarray(trace_table), 1)
+        i_max_total = _max(i_max_total, i_max)
+        j_max_total = _max(j_max_total, j_max)
+
+        for i in range(i_min, i_max+1):
+            j = k - i
+
+            # Evaluate score from diagonal direction
+            if i != 0 and j != 0:
+                # -1 in sequence index is necessary
+                # due to the shift of the sequences
+                # to the bottom/right in the table
+                similarity_score = matrix[code1[i-1], code2[j-1]]
+                mm_score  = m_table[i-1,j-1]
+                g1m_score = g1_table[i-1,j-1]
+                g2m_score = g2_table[i-1,j-1]
+                # Check if scores stem from cells that are valid
+                if mm_score != 0:
+                    mm_score += similarity_score
+                if g1m_score != 0:
+                    g1m_score += similarity_score
+                if g2m_score != 0:
+                    g2m_score += similarity_score
+            else:
+                mm_score = 0
+                g1m_score = 0
+                g2m_score = 0
+            # Evaluate score through gap insertion
+            # No transition from g1_table to g2_table and vice versa,
+            # since this would mean adjacent gaps in both sequences;
+            # a substitution makes more sense in this case
+            if j != 0:
+                mg1_score  =  m_table[i,j-1] + gap_open
+                g1g1_score = g1_table[i,j-1] + gap_ext
+            else:
+                mg1_score  = 0
+                g1g1_score = 0
+            if i != 0:
+                mg2_score  = m_table[i-1,j]  + gap_open
+                g2g2_score = g2_table[i-1,j] + gap_ext
+            else:
+                mg2_score  = 0
+                g2g2_score = 0
+            
+            
+            
+            if score_only:
+                m_score = _max(mm_score, _max(g1m_score, g2m_score))
+                g1_score = _max(mg1_score, mg1_score)
+                g2_score = _max(mg2_score, g2g2_score)
+            else:
+                trace = get_trace_affine(
+                    mm_score, g1m_score, g2m_score,
+                    mg1_score, g1g1_score,
+                    mg2_score, g2g2_score,
+                    # The max score values to be written
+                    &m_score, &g1_score, &g2_score
+                )
+            
+
+            # Check if the obtained scores reach the required threshold
+            # and if they even exceed the maximum score
+            is_valid_cell = False
+
+            if m_score >= req_score:
+                if i_min_k_0 == k:
+                    i_min_k_0 = i
+                i_max_k_0 = i
+                m_table[i,j] = m_score
+                is_valid_cell = True
+                if m_score > max_score:
+                    max_score = m_score
+                    req_score = max_score - threshold
+            
+            if g1_score >= req_score:
+                if i_min_k_0 == k:
+                    i_min_k_0 = i
+                i_max_k_0 = i
+                g1_table[i,j] = g1_score
+                is_valid_cell = True
+                if g1_score > max_score:
+                    max_score = g1_score
+                    req_score = max_score - threshold
+            
+            if g2_score >= req_score:
+                if i_min_k_0 == k:
+                    i_min_k_0 = i
+                i_max_k_0 = i
+                g2_table[i,j] = g2_score
+                is_valid_cell = True
+                if g2_score > max_score:
+                    max_score = g2_score
+                    req_score = max_score - threshold
+            
+            if is_valid_cell and not score_only:
+                trace_table[i,j] = trace
+
+    
+    return np.asarray(trace_table)[:i_max_total+1, :j_max_total+1], \
+           np.asarray(m_table    )[:i_max_total+1, :j_max_total+1], \
+           np.asarray(g1_table   )[:i_max_total+1, :j_max_total+1], \
+           np.asarray(g2_table   )[:i_max_total+1, :j_max_total+1]
 
 
 def _extend_table(table, int dimension):
@@ -438,8 +623,8 @@ def _extend_table(table, int dimension):
     return new_table
 
 
-cdef inline int _min(int a, int b):
+cdef inline int _min(int32 a, int32 b):
     return a if a < b else b
 
-cdef inline int _max(int a, int b):
+cdef inline int _max(int32 a, int32 b):
     return a if a > b else b
