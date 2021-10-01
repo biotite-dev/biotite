8f69af1c97a75753cd48fdb9addd33372ac947b1
Patrick Kunzmann <patrick.kunzm@gmail.com>
2021-10-01 16:33:16 +0200

    Add proper error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use ndarray::{Array, Ix1, Ix2, Ix3};
 use pyo3::prelude::*;
 use pyo3::exceptions;
+use pyo3::create_exception;
 
 use numpy::PyArray;
+
+
+create_exception!(fastpdb, InvalidFileError, exceptions::PyException);
+create_exception!(fastpdb, BadStructureError, exceptions::PyException);
 
 
 #[derive(Debug)]
@@ -28,21 +33,21 @@ impl PDBFile {
 
 
     fn parse_coord_single_model(&self, model: isize) -> PyResult<Py<PyArray<f32, Ix2>>> {
-        let array = self.parse_coord(Some(model));
+        let array = self.parse_coord(Some(model))?;
         Python::with_gil(|py| {
             match array{
                 CoordArray::Single(array) => Ok(PyArray::from_array(py, &array).to_owned()),
-                CoordArray::Multi(_) => Err(exceptions::PyTypeError::new_err(""))
+                CoordArray::Multi(_) => panic!("No multi-model coordinates should be returned"),
             }
         })
     }
 
 
     fn parse_coord_multi_model(&self) -> PyResult<Py<PyArray<f32, Ix3>>> {
-        let array = self.parse_coord(None);
+        let array = self.parse_coord(None)?;
         Python::with_gil(|py| {
             match array{
-                CoordArray::Single(_) => Err(exceptions::PyTypeError::new_err("")),
+                CoordArray::Single(_) => panic!("No single-model coordinates should be returned"),
                 CoordArray::Multi(array) => Ok(PyArray::from_array(py, &array).to_owned()),
             }
         })
@@ -67,7 +72,7 @@ impl PDBFile {
                                                             Option<Py<PyArray<f32,  Ix1>>>,
                                                             Option<Py<PyArray<i32,  Ix1>>>)> {
         let model_start_i = self.get_model_start_indices();
-        let (model_start, model_stop) = self.get_model_boundaries(model, &model_start_i);
+        let (model_start, model_stop) = self.get_model_boundaries(model, &model_start_i)?;
 
         let atom_line_i: Vec<usize> = self.get_atom_indices(model_start, model_stop);
         
@@ -114,7 +119,11 @@ impl PDBFile {
         for (atom_i, line_i) in atom_line_i.iter().enumerate() {
             let line = &self.lines[*line_i];
             write_string_to_array(&mut chain_id,  atom_i, line[21..22].trim());
-            res_id[atom_i] = line[22..26].trim().parse().unwrap();
+            res_id[atom_i] = line[22..26].trim().parse().or_else(|_|
+                return Err(BadStructureError::new_err(format!(
+                    "'{}' cannot be parsed into an integer", line[22..26].trim()
+                )))
+            )?;
             write_string_to_array(&mut ins_code,  atom_i, line[26..27].trim());
             write_string_to_array(&mut res_name,  atom_i, line[17..20].trim());
             hetero[atom_i] = if &line[0..4] == "ATOM" { false } else { true };
@@ -124,19 +133,41 @@ impl PDBFile {
 
             // Set optional annotation arrays
             if include_atom_id {
-                atom_id[atom_i] = line[6..11].trim().parse().unwrap();
+                atom_id[atom_i] = line[6..11].trim().parse().or_else(|_|
+                    Err(BadStructureError::new_err(format!(
+                        "'{}' cannot be parsed into an integer", line[6..11].trim()
+                    )))
+                )?;
             }
             if include_b_factor {
-                b_factor[atom_i] = line[60..66].trim().parse().unwrap();
+                b_factor[atom_i] = line[60..66].trim().parse().or_else(|_|
+                    Err(BadStructureError::new_err(format!(
+                        "'{}' cannot be parsed into a float", line[60..66].trim()
+                    )))
+                )?;
             }
             if include_occupancy {
-                occupancy[atom_i] = line[54..60].trim().parse().unwrap();
+                occupancy[atom_i] = line[54..60].trim().parse().or_else(|_|
+                    Err(BadStructureError::new_err(format!(
+                        "'{}' cannot be parsed into a float", line[54..60].trim()
+                    )))
+                )?;
             }
             if include_charge {
                 let mut charge_raw = line[78..80].chars();
                 let raw_number = charge_raw.next().unwrap();
                 let raw_sign = charge_raw.next().unwrap();
-                let number = if raw_number == ' ' { 0 } else { raw_number.to_digit(10).unwrap() };
+                let number;
+                if raw_number == ' ' {
+                    number = 0;
+                }
+                else {
+                    number = raw_number.to_digit(10).ok_or(
+                        BadStructureError::new_err(format!(
+                            "'{}' cannot be parsed into an integer", raw_number
+                        ))
+                    )?;
+                }
                 let sign = if raw_sign == '-' { -1 } else { 1 };
                 charge[atom_i] = number as i32 * sign;
             }
@@ -164,11 +195,11 @@ impl PDBFile {
 
 impl PDBFile {
 
-    fn parse_coord(&self, model: Option<isize>) -> CoordArray {
+    fn parse_coord(&self, model: Option<isize>) -> PyResult<CoordArray> {
         let model_start_i = self.get_model_start_indices();
         
         let (model_start, model_stop) = match model {
-            Some(model_number) => self.get_model_boundaries(model_number, &model_start_i),
+            Some(model_number) => self.get_model_boundaries(model_number, &model_start_i)?,
             None => (0, self.lines.len())
         };
     
@@ -177,17 +208,32 @@ impl PDBFile {
         let mut coord = Array::zeros((atom_line_i.len(), 3));
         for (atom_i, line_i) in atom_line_i.iter().enumerate() {
             let line = &self.lines[*line_i];
-            coord[[atom_i, 0]] = line[30..38].trim().parse().unwrap();
-            coord[[atom_i, 1]] = line[38..46].trim().parse().unwrap();
-            coord[[atom_i, 2]] = line[46..54].trim().parse().unwrap();
+            coord[[atom_i, 0]] = line[30..38].trim().parse().or_else(|_|
+                Err(BadStructureError::new_err(format!(
+                    "'{}' cannot be parsed into a float", line[30..38].trim()
+                )))
+            )?;
+            coord[[atom_i, 1]] = line[38..46].trim().parse().or_else(|_|
+                Err(BadStructureError::new_err(format!(
+                    "'{}' cannot be parsed into a float", line[38..46].trim()
+                )))
+            )?;
+            coord[[atom_i, 2]] = line[46..54].trim().parse().or_else(|_|
+                Err(BadStructureError::new_err(format!(
+                    "'{}' cannot be parsed into a float", line[46..54].trim()
+                )))
+            )?;
         }
         
         match model {
-            Some(_) => CoordArray::Single(coord),
+            Some(_) => Ok(CoordArray::Single(coord)),
             None => {
                 // Check whether all models have the same length
-                let length = self.get_model_length(&model_start_i, &atom_line_i);
-                CoordArray::Multi(coord.into_shape((model_start_i.len(), length, 3)).unwrap())
+                let length = self.get_model_length(&model_start_i, &atom_line_i)?;
+                Ok(
+                    CoordArray::Multi(coord.into_shape((model_start_i.len(), length, 3))
+                    .expect("Model length is invalid"))
+                )
             }
         }
     }
@@ -215,7 +261,7 @@ impl PDBFile {
     
     fn get_model_boundaries(&self,
                             model: isize,
-                            model_start_i: &Vec<usize>) -> (usize, usize) {
+                            model_start_i: &Vec<usize>) -> PyResult<(usize, usize)> {
         let model_i: isize;
         if model > 0 {
             model_i = model - 1;
@@ -224,23 +270,28 @@ impl PDBFile {
             model_i = model_start_i.len() as isize + model;
         }
         else {
-            panic!("The model index must not be 0");
+            return Err(exceptions::PyValueError::new_err("Model index must not be 0"));
         }
     
         if model_i >= model_start_i.len() as isize || model_i < 0 {
-            panic!("Out of bounds model");
+            return Err(exceptions::PyValueError::new_err(format!(
+                "The file has {} models, the given model {} does not exist",
+                model_start_i.len(), model
+            )));
         }
     
         if model_i == model_start_i.len() as isize - 1 {
             // Last model -> Model reaches to end of file
-            (model_start_i[model_i as usize], self.lines.len())
+            Ok((model_start_i[model_i as usize], self.lines.len()))
         }
         else {
-            (model_start_i[model_i as usize], model_start_i[(model_i+1) as usize])
+            Ok((model_start_i[model_i as usize], model_start_i[(model_i+1) as usize]))
         }
     }
     
-    fn get_model_length(&self, model_start_i: &Vec<usize>, atom_line_i: &Vec<usize>) -> usize {
+    fn get_model_length(&self,
+                        model_start_i: &Vec<usize>,
+                        atom_line_i: &Vec<usize>) -> PyResult<usize> {
         let n_models = model_start_i.len();
         let mut length: Option<usize> = None;
         for model_i in 0..n_models {
@@ -252,13 +303,15 @@ impl PDBFile {
                 .count();
             match length {
                 None => length = Some(model_length),
-                Some(l) => if model_length != l { panic!("Inconsistent number of models"); }
+                Some(l) => if model_length != l { return Err(BadStructureError::new_err(
+                    "Inconsistent number of models"
+                )); }
             };
         }
     
         match length {
-            None => panic!("File contains no model"),
-            Some(l) => l
+            None => panic!("Length cannot be 'None'"),
+            Some(l) => Ok(l)
         }
     }
 }
