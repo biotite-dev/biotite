a94277938d556e36781fa949d7a890c65ca62925
Patrick Kunzmann <patrick.kunzm@gmail.com>
2021-10-06 14:16:24 +0200

    Remove copyright notice

--- fastpdb/__init__.py
+++ fastpdb/__init__.py
@@ -1,10 +1,7 @@
-# This source code is part of the Biotite package and is distributed
-# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
-# information.
-
 __name__ = "fastpdb"
 __author__ = "Patrick Kunzmann"
 __all__ = ["PDBFile"]
+__version__ = "1.0.0"
 
 import numpy as np
 import biotite
