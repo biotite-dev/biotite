49f57b35eb13c2bf37372905b86099b264297613
Patrick Kunzmann <patrick.kunzm@gmail.com>
2022-05-06 14:21:21 +0200

    Fix #379

--- src/biotite/structure/io/pdb/file.py
+++ src/biotite/structure/io/pdb/file.py
@@ -685,8 +685,11 @@ class PDBFile(TextFile):
         
         # Mapping from atom ids to indices in an AtomArray
         atom_id_to_index = np.zeros(atom_ids[-1]+1, dtype=int)
-        for i, id in enumerate(atom_ids):
-           atom_id_to_index[id] = i
+        try:
+            for i, id in enumerate(atom_ids):
+                atom_id_to_index[id] = i
+        except IndexError as e:
+            raise InvalidFileError("Atom IDs are not strictly increasing")
 
         bonds = []
         for line in conect_lines:
