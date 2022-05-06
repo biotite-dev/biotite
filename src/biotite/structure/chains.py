7d0536191f3fc06089e19eb46ba881b602f33788
Patrick Kunzmann <patrick.kunzm@gmail.com>
2022-05-06 16:32:57 +0200

    Add more chain-level utitlity functions

--- src/biotite/structure/chains.py
+++ src/biotite/structure/chains.py
@@ -9,7 +9,9 @@ atom level.
 
 __name__ = "biotite.structure"
 __author__ = "Patrick Kunzmann"
-__all__ = ["get_chain_starts", "get_chains", "get_chain_count", "chain_iter"]
+__all__ = ["get_chain_starts", "apply_chain_wise", "spread_chain_wise",
+           "get_chain_masks", "get_chain_starts_for", "get_chain_positions",
+           "chain_iter", "get_chains", "get_chain_count", "chain_iter"]
 
 import numpy as np
 from .resutil import *
@@ -61,6 +63,169 @@ def get_chain_starts(array, add_exclusive_stop=False):
         return np.concatenate(([0], chain_starts, [array.array_length()]))
     else:
         return np.concatenate(([0], chain_starts))
+
+
+def apply_chain_wise(array, data, function, axis=None):
+    """
+    Apply a function to intervals of data, where each interval
+    corresponds to one chain.
+    
+    The function takes an atom array (stack) and an data array
+    (`ndarray`) of the same length. The function iterates through the
+    chain IDs of the atom array (stack) and identifies intervals of
+    the same ID. Then the data is
+    partitioned into the same intervals, and each interval (also an
+    :class:`ndarray`) is put as parameter into `function`. Each return value is
+    stored as element in the resulting :class:`ndarray`, therefore each element
+    corresponds to one chain. 
+    
+    Parameters
+    ----------
+    array : AtomArray or AtomArrayStack
+        The atom array (stack) to determine the chains from.
+    data : ndarray
+        The data, whose intervals are the parameter for `function`. Must
+        have same length as `array`.
+    function : function
+        The `function` must have either the form *f(data)* or
+        *f(data, axis)* in case `axis` is given. Every `function` call
+        must return a value with the same shape and data type.
+    axis : int, optional
+        This value is given to the `axis` parameter of `function`.
+        
+    Returns
+    -------
+    processed_data : ndarray
+        Chain-wise evaluation of `data` by `function`. The size of the
+        first dimension of this array is equal to the amount of
+        chains.
+        
+    See also
+    --------
+    apply_residue_wise
+    """
+    starts = get_chain_starts(array, add_exclusive_stop=True)
+    return apply_segment_wise(starts, data, function, axis)
+
+
+def spread_chain_wise(array, input_data):
+    """
+    Expand chain-wise data to atom-wise data.
+
+    Each value in the chain-wise input is assigned to all atoms of
+    this chain:
+    
+    ``output_data[i] = input_data[j]``,
+    *i* is incremented from atom to atom,
+    *j* is incremented every chain change.
+    
+    Parameters
+    ----------
+    array : AtomArray or AtomArrayStack
+        The atom array (stack) to determine the chains from.
+    input_data : ndarray
+        The data to be spread. The length of axis=0 must be equal to
+        the amount of different chain IDs in `array`.
+        
+    Returns
+    -------
+    output_data : ndarray
+        Chain-wise spread `input_data`. Length is the same as
+        `array_length()` of `array`.
+        
+    See also
+    --------
+    spread_residue_wise
+    """
+    starts = get_chain_starts(array, add_exclusive_stop=True)
+    return spread_segment_wise(starts, input_data)
+
+
+def get_chain_masks(array, indices):
+    """
+    Get boolean masks indicating the chains to which the given atom
+    indices belong.
+
+    Parameters
+    ----------
+    array : AtomArray, shape=(n,) or AtomArrayStack, shape=(m,n)
+        The atom array (stack) to determine the chains from.
+    indices : ndarray, dtype=int, shape=(k,)
+        These indices indicate the atoms to get the corresponding
+        chains for.
+        Negative indices are not allowed.
+    
+    Returns
+    -------
+    chains_masks : ndarray, dtype=bool, shape=(k,n)
+        Multiple boolean masks, one for each given index in `indices`.
+        Each array masks the atoms that belong to the same chain as
+        the atom at the given index.
+    
+    See also
+    --------
+    get_residue_masks
+    """
+    starts = get_chain_starts(array, add_exclusive_stop=True)
+    return get_segment_masks(starts, indices)
+
+
+def get_chain_starts_for(array, indices):
+    """
+    For each given atom index, get the index that points to the
+    start of the chain that atom belongs to.
+
+    Parameters
+    ----------
+    array : AtomArray or AtomArrayStack
+        The atom array (stack) to determine the chains from.
+    indices : ndarray, dtype=int, shape=(k,)
+        These indices point to the atoms to get the corresponding
+        chain starts for.
+        Negative indices are not allowed.
+    
+    Returns
+    -------
+    start_indices : ndarray, dtype=int, shape=(k,)
+        The indices that point to the chain starts for the input
+        `indices`.
+    
+    See also
+    --------
+    get_residue_starts_for
+    """
+    starts = get_chain_starts(array, add_exclusive_stop=True)
+    return get_segment_starts_for(starts, indices)
+
+
+def get_chain_positions(array, indices):
+    """
+    For each given atom index, obtain the position of the chain
+    corresponding to this index in the input `array`.
+
+    For example, the position of the first chain in the atom array is
+    ``0``, the the position of the second chain is ``1``, etc.
+
+    Parameters
+    ----------
+    array : AtomArray or AtomArrayStack
+        The atom array (stack) to determine the chains from.
+    indices : ndarray, dtype=int, shape=(k,)
+        These indices point to the atoms to get the corresponding
+        chain positions for.
+        Negative indices are not allowed.
+    
+    Returns
+    -------
+    start_indices : ndarray, dtype=int, shape=(k,)
+        The indices that point to the position of the chains.
+    
+    See also
+    --------
+    get_residue_positions
+    """
+    starts = get_chain_starts(array, add_exclusive_stop=True)
+    return get_segment_positions(starts, indices)
 
 
 def get_chains(array):
--- tests/structure/test_chains.py
+++ tests/structure/test_chains.py
@@ -34,6 +34,38 @@ def test_get_chain_starts_same_id(array):
     merged = array + array
     assert struc.get_chain_starts(merged).tolist() == [0, array.array_length()]
 
+def test_apply_chain_wise(array):
+    data = struc.apply_chain_wise(array, np.ones(len(array)), np.sum)
+    assert data.tolist() == [
+        len(array[array.chain_id == chain_id])
+        for chain_id in np.unique(array.chain_id)
+    ]
+
+def test_spread_chain_wise(array):
+    input_data = np.unique(array.chain_id)
+    output_data = struc.spread_chain_wise(array, input_data)
+    assert output_data.tolist() == array.chain_id.tolist()
+
+def test_get_chain_masks(array):
+    SAMPLE_SIZE = 100
+    np.random.seed(0)
+    indices = np.random.randint(0, array.array_length(), SAMPLE_SIZE)
+    test_masks = struc.get_chain_masks(array, indices)
+    for index, test_mask in zip(indices, test_masks):
+        ref_mask = array.chain_id == array.chain_id[index]
+        assert test_mask.tolist() == ref_mask.tolist()
+
+def test_get_chain_starts_for(array):
+    SAMPLE_SIZE = 100
+    np.random.seed(0)
+    indices = np.random.randint(0, array.array_length(), SAMPLE_SIZE)
+    ref_starts = np.array(
+        [np.where(mask)[0][0] for mask
+         in struc.get_chain_masks(array, indices)]
+    )
+    test_starts = struc.get_chain_starts_for(array, indices)
+    assert test_starts.tolist() == ref_starts.tolist()
+
 def test_get_chains(array):
     assert struc.get_chains(array).tolist() == ["A", "B", "C", "D", "E", "F"]
 
