ff6209728fe2d565a45619486b04dae0fbb00dac
Patrick Kunzmann <patrick.kunzm@gmail.com>
2022-12-08 14:03:08 +0100

    Add new test for P-SEA implementation

--- tests/structure/test_sse.py
+++ tests/structure/test_sse.py
@@ -2,20 +2,47 @@
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
-import biotite.structure as struc
-import biotite.structure.io as strucio
+import glob
+from os.path import join, basename, splitext
 import numpy as np
-from os.path import join
+import biotite.structure as struc
+import biotite.structure.io.mmtf as mmtf
+import biotite.sequence.io.fasta as fasta
 from ..util import data_dir
-import pytest
 
 
 def test_sse():
-    array = strucio.load_structure(join(data_dir("structure"), "3o5r.mmtf"))
-    test_sse = struc.annotate_sse(array, "A")
-    ref_sse = (
-        "caaaaaacccccccccccccbbbbbccccccbbbbccccccccccccccc"
-        "ccccccccccccbbbbbbcccccccaaaaaaaaaccccccbbbbbccccc"
-        "ccccccccccccbbbbbbbccccccccc"
-    )
-    assert "".join(test_sse.tolist()) == ref_sse
+    """
+    Compare output of :func:`annotate_sse()` to output of original
+    *P-SEA* software and ensure that almost all annotations are equal.
+    Small deviations may appear due to some ambiguous descriptions in
+    the original algorithm description.
+    """
+    THRESHOLD = 0.85
+
+    matches = 0
+    total = 0
+
+    for file_name in glob.glob(join(data_dir("structure"), "*.mmtf")):
+
+        pdb_id = splitext(basename(file_name))[0]
+
+        atoms = mmtf.get_structure(mmtf.MMTFFile.read(file_name), model=1)
+        atoms = atoms[struc.filter_canonical_amino_acids(atoms)]
+        if atoms.array_length() == 0:
+            # Structure contains no peptide to annotate SSE for
+            continue
+        atoms = atoms[atoms.chain_id == atoms.chain_id[0]]
+
+        ref_sse = fasta.FastaFile.read(
+            join(data_dir("structure"), "psea.fasta")
+        )[pdb_id]
+        ref_sse = np.array(list(ref_sse))
+        
+        test_sse = struc.annotate_sse(atoms, atoms.chain_id[0])
+        
+        assert len(test_sse) == len(ref_sse)
+        matches += np.count_nonzero(test_sse == ref_sse)
+        total += len(ref_sse)
+
+    assert matches / total >= THRESHOLD
