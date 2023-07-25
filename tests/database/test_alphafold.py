66e9d102e3a362962ae26c465483c5bf4c536689
Alex Carlin <alxcarln@gmail.com>
2023-07-24 23:58:28 -0700

    Implement fetching from AlphaFold DB
    - add module for fetching from AlphaFold DB
    - add tests for module
    - more to come

---
+++ tests/database/test_alphafold.py
@@ -0,0 +1,46 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+import itertools
+import tempfile
+import pytest
+import biotite.database.alphafold as alphafold
+import biotite.structure.io.pdb as pdb 
+from biotite.database import RequestError
+from ..util import cannot_connect_to
+
+
+ALPHAFOLD_URL = "https://alphafold.com/"
+
+
+@pytest.mark.skipif(
+    cannot_connect_to(ALPHAFOLD_URL),
+    reason="AlphaFold is not available"
+)
+@pytest.mark.parametrize(
+    "as_file_like",
+    itertools.product([False, True])
+)
+def test_fetch(as_file_like):
+    path = None if as_file_like else tempfile.gettempdir()
+    file = alphafold.fetch(
+        "P12345", path, overwrite=True
+    )
+    pdb_file = pdb.PDBFile.read(file)
+    array_stack = pdb_file.get_structure()
+    assert len(array_stack) > 0
+    
+
+@pytest.mark.skipif(
+    cannot_connect_to(ALPHAFOLD_URL),
+    reason="AlphaFold is not available"
+)
+@pytest.mark.parametrize("format", ["pdb", "cif", "bcif"])
+def test_fetch_invalid(format):
+    with pytest.raises(RequestError):
+        file = alphafold.fetch(
+            "XYZ", format, tempfile.gettempdir(), overwrite=True
+        )
+
+
