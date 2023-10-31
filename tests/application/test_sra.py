e1a96ec47de1edcfe28d485c8264adc7418da51a
Patrick Kunzmann <padix.key@gmail.com>
2023-10-31 13:48:45 +0100

    Increase performance of fetching via sra-tools

--- tests/application/test_sra.py
+++ tests/application/test_sra.py
@@ -2,22 +2,63 @@
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
+import itertools
 from os.path import join
 from tempfile import gettempdir
 import pytest
-from biotite.application.sra import FastqDumpApp
+from biotite.application.sra import FastqDumpApp, FastaDumpApp
 from biotite.sequence.io.fastq import FastqFile
+from biotite.sequence.io.fasta import FastaFile
 
 
-@pytest.mark.parametrize("custom_prefix", [False, True])
-def test_fastq_dump(custom_prefix):
+@pytest.mark.parametrize(
+    "app_class, custom_prefix", itertools.product(
+        [FastqDumpApp, FastaDumpApp],
+        [False, True]
+    )
+)
+def test_objects(app_class, custom_prefix):
+    """
+    Test return types of methods from the respective `Application` class.
+    """
+    # Small dataset for low server impact
+    UID = "ERR11344941"
+
     prefix = join(gettempdir(), "test_fastq") if custom_prefix else None
-    app = FastqDumpApp("SRR6986517", output_path_prefix=prefix)
+    app = app_class(UID, output_path_prefix=prefix)
     app.start()
     app.join()
 
-    for sequences_and_scores in app.get_sequences():
-        assert isinstance(sequences_and_scores, dict)
-    
-    for fastq_file in app.get_fastq():
-        assert isinstance(fastq_file, FastqFile)
+    for sequences in app.get_sequences():
+        assert isinstance(sequences, dict)
+
+    if app_class == FastqDumpApp:
+        for sequences_and_scores in app.get_sequences_and_scores():
+            assert isinstance(sequences_and_scores, dict)
+
+    if app_class == FastqDumpApp:
+        for fastq_file in app.get_fastq():
+            assert isinstance(fastq_file, FastqFile)
+    else:
+        for fasta_file in app.get_fasta():
+            assert isinstance(fasta_file, FastaFile)
+
+
+@pytest.mark.parametrize(
+    "app_class, custom_prefix", itertools.product(
+        [FastqDumpApp, FastaDumpApp],
+        [False, True]
+    )
+)
+def test_classmethod(app_class, custom_prefix):
+    """
+    Test return types of the `fetch()` class method.
+    """
+    UID = "ERR11344941"
+
+    prefix = join(gettempdir(), "test_fastq") if custom_prefix else None
+
+    sequences = app_class.fetch(UID, output_path_prefix=prefix)
+
+    for sequences in sequences:
+        assert isinstance(sequences, dict)
