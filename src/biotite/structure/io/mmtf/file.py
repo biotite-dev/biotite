4d75a529894b7928f16c5293ca4ecc339465f7a0
Patrick Kunzmann <padix.key@gmail.com>
2024-01-25 20:36:16 +0100

    Deprecate MMTFFile

--- src/biotite/structure/io/mmtf/file.py
+++ src/biotite/structure/io/mmtf/file.py
@@ -12,6 +12,7 @@ import struct
 import copy
 import numpy as np
 import msgpack
+import warnings
 from ....file import File, is_binary, is_open_compatible
 from ...error import BadStructureError
 from .decode import decode_array
@@ -21,7 +22,7 @@ from .encode import encode_array
 class MMTFFile(File, MutableMapping):
     """
     This class represents a MMTF file.
-    
+
     When reading a file, the *MessagePack* unpacker is used to create
     a dictionary of the file content.
     This dictionary is accessed by indexing the :class:`MMTFFile`
@@ -29,10 +30,13 @@ class MMTFFile(File, MutableMapping):
     If the dictionary value is an encoded array, the value automatically
     decoded.
     Decoded arrays are always returned as :class:`ndarray` instances.
-    
+
+    DEPRECATED: Use :class:`biotite.structure.io.pdbx.BinaryCIFFile`
+    instead.
+
     Examples
     --------
-    
+
     >>> import os.path
     >>> mmtf_file = MMTFFile.read(os.path.join(path_to_structures, "1l2y.mmtf"))
     >>> print(mmtf_file["title"])
@@ -42,24 +46,28 @@ class MMTFFile(File, MutableMapping):
      'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A' 'A'
      'A' 'A']
     """
-    
+
     def __init__(self):
+        warnings.warn(
+            "'MMTFFile' is deprecated, use 'BinaryCIFFile' instead",
+            DeprecationWarning
+        )
         super().__init__()
         self._content = {}
         self._content["mmtfVersion"] = "1.0.0"
         self._content["mmtfProducer"] = "UNKNOWN"
-    
+
     @classmethod
     def read(self, file):
         """
         Read a MMTF file.
-        
+
         Parameters
         ----------
         file : file-like object or str
             The file to be read.
             Alternatively a file path can be supplied.
-        
+
         Returns
         -------
         file_object : MMTFFile
@@ -80,11 +88,11 @@ class MMTFFile(File, MutableMapping):
                 file.read(), use_list=True, raw=False
             )
         return mmtf_file
-    
+
     def write(self, file):
         """
         Write contents into a MMTF file.
-        
+
         Parameters
         ----------
         file : file-like object or str
@@ -101,20 +109,20 @@ class MMTFFile(File, MutableMapping):
             if not is_binary(file):
                 raise TypeError("A file opened in 'binary' mode is required")
             file.write(packed_bytes)
-    
+
     def __copy_fill__(self, clone):
         super().__copy_fill__(clone)
         clone._content = copy.deepcopy(self._content)
-    
+
     def get_codec(self, key):
         """
         Obtain the codec ID of an MMTF encoded value.
-        
+
         Parameters
         ----------
         key : str
             The key for the potentially encoded value.
-        
+
         Returns
         -------
         codec : int or None
@@ -126,16 +134,16 @@ class MMTFFile(File, MutableMapping):
             return codec
         else:
             return None
-    
+
     def get_length(self, key):
         """
         Obtain the length of an MMTF encoded value.
-        
+
         Parameters
         ----------
         key : str
             The key for the potentially encoded value.
-        
+
         Returns
         -------
         codec : int or None
@@ -148,16 +156,16 @@ class MMTFFile(File, MutableMapping):
             return length
         else:
             return None
-    
+
     def get_param(self, key):
         """
         Obtain the parameter of an MMTF encoded value.
-        
+
         Parameters
         ----------
         key : str
             The key for the potentially encoded value.
-        
+
         Returns
         -------
         codec : int or None
@@ -170,7 +178,7 @@ class MMTFFile(File, MutableMapping):
             return param
         else:
             return None
-    
+
     def set_array(self, key, array, codec, param=0):
         length = len(array)
         raw_bytes = encode_array(array, codec, param)
@@ -179,7 +187,7 @@ class MMTFFile(File, MutableMapping):
              + struct.pack(">i", param) \
              + raw_bytes
         self._content[key] = data
-    
+
     def __getitem__(self, key):
         data = self._content[key]
         if isinstance(data, bytes) and data[0] == 0:
@@ -191,22 +199,22 @@ class MMTFFile(File, MutableMapping):
             return decode_array(codec, raw_bytes, param)
         else:
             return data
-    
+
     def __setitem__(self, key, item):
         if isinstance(item, np.ndarray):
             raise TypeError("Arrays that need to be encoded must be addeed "
                             "via 'set_array()'")
         self._content[key] = item
-    
+
     def __delitem__(self, key):
         del self._content[key]
-    
+
     def __iter__(self):
         return self._content.__iter__()
-    
+
     def __len__(self):
         return len(self._content)
-    
+
     def __contains__(self, item):
         return item in self._content
 
