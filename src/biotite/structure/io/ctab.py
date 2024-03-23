5db499e8dc0a41d05cb779b8ae0a02aaed30de88
Patrick Kunzmann <padix.key@gmail.com>
2024-03-23 16:51:20 +0100

    Use proper relative imports

--- src/biotite/structure/io/ctab.py
+++ src/biotite/structure/io/ctab.py
@@ -13,7 +13,7 @@ __all__ = ["read_structure_from_ctab", "write_structure_to_ctab"]
 
 import warnings
 import numpy as np
-from biotite.structure.error import BadStructureError
+from ..error import BadStructureError
 from ..atoms import AtomArray, AtomArrayStack
 from ..bonds import BondList, BondType
 
