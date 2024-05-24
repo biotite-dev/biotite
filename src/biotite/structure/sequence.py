ae0f331613dccf3781ee694ebc58c7288803fbca
Patrick Kunzmann <padix.key@gmail.com>
2024-05-24 15:27:28 +0200

    Add `to_sequence()`

---
+++ src/biotite/structure/sequence.py
@@ -0,0 +1,112 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+"""
+Function for converting a structure into a sequence.
+"""
+
+__name__ = "biotite.structure"
+__author__ = "Patrick Kunzmann"
+__all__ = ["to_sequence"]
+
+import numpy as np
+from .info.misc import one_letter_code
+from .info.groups import amino_acid_names, nucleotide_names
+from .residues import get_residues
+from .chains import get_chain_starts
+from .error import BadStructureError
+from ..sequence.seqtypes import ProteinSequence, NucleotideSequence
+
+
+HETERO_PLACEHOLDER = "."
+
+
+def to_sequence(atoms, allow_hetero=False):
+    """
+    Convert each chain in a structure into a sequence.
+
+    Parameters
+    ----------
+    atoms : AtomArray or AtomArrayStack
+        The structure.
+        May contain multiple chains.
+        Each chain must be either a peptide or a nucleic acid.
+    allow_hetero : bool, optional
+        If true, residues inside a amino acid or nucleotide chain,
+        that have no one-letter code, are replaced by the respective
+        '*any*' symbol (`"X"` or `"N"`, respectively).
+        The same is true for amino acids in nucleotide chains and vice
+        versa.
+        By default, an exception is raised.
+
+    Returns
+    -------
+    sequences : list of Sequence, length=n
+        The sequence for each chain in the structure.
+    chain_start_indices : ndarray, shape=(n,), dtype=int
+        The atom index where each chain starts.
+
+    Notes
+    -----
+    Residues are considered amino acids or nucleotides based on their
+    appearance :func:`info.amino_acid_names()` or
+    :func:`info.nucleotide_names()`, respectively.
+
+    Examples
+    --------
+
+    >>> sequences, chain_starts = to_sequence(atom_array)
+    >>> print(sequences)
+    [ProteinSequence("NLYIQWLKDGGPSSGRPPPS")]
+
+    """
+    sequences = []
+    chain_start_indices = get_chain_starts(atoms, add_exclusive_stop=True)
+    for i in range(len(chain_start_indices)-1):
+        start = chain_start_indices[i]
+        stop = chain_start_indices[i+1]
+        chain = atoms[start:stop]
+        _, residues = get_residues(chain)
+        one_letter_symbols = np.array(
+            [one_letter_code(res) or HETERO_PLACEHOLDER for res in residues]
+        )
+        hetero_mask = one_letter_symbols == HETERO_PLACEHOLDER
+
+        aa_count  = np.count_nonzero(np.isin(residues, amino_acid_names()))
+        nuc_count = np.count_nonzero(np.isin(residues, nucleotide_names()))
+        if aa_count == 0 and nuc_count == 0:
+            raise BadStructureError(
+                f"Chain {chain.chain_id[0]} contains neither amino acids "
+                "nor nucleotides"
+            )
+        elif aa_count > nuc_count:
+            # Chain is a peptide
+            hetero_mask |= ~np.isin(residues, amino_acid_names())
+            if not allow_hetero and np.any(hetero_mask):
+                hetero_indices = np.where(hetero_mask)[0]
+                raise BadStructureError(
+                    f"Hetero residue(s) "
+                    f"{', '.join(residues[hetero_indices])} in peptide"
+                )
+            one_letter_symbols[hetero_mask] = "X"
+            # Replace selenocysteine and pyrrolysine
+            one_letter_symbols[one_letter_symbols == "U"] = "C"
+            one_letter_symbols[one_letter_symbols == "O"] = "K"
+            sequences.append(ProteinSequence("".join(one_letter_symbols)))
+        else:
+            # Chain is a nucleic acid
+            hetero_mask |= ~np.isin(residues, nucleotide_names())
+            if not allow_hetero and np.any(hetero_mask):
+                hetero_indices = np.where(hetero_mask)[0]
+                raise BadStructureError(
+                    f"Hetero residue(s) "
+                    f"{', '.join(residues[hetero_indices])} in nucleic acid"
+                )
+            one_letter_symbols[hetero_mask] = "N"
+            # Replace uracil
+            one_letter_symbols[one_letter_symbols == "U"] = "T"
+            sequences.append(NucleotideSequence("".join(one_letter_symbols)))
+
+    # Remove exclusive stop
+    return sequences, chain_start_indices[:-1]
---
+++ tests/structure/test_sequence.py
@@ -0,0 +1,73 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+import glob
+from os.path import join
+import pytest
+import biotite.structure as struc
+import biotite.sequence as seq
+import biotite.sequence.align as align
+import biotite.structure.io.pdbx as pdbx
+from ..util import data_dir
+
+
+@pytest.mark.parametrize(
+    "path", glob.glob(join(data_dir("structure"), "*.bcif"))
+)
+def test_pdbx_sequence_consistency(path):
+    """
+    Check if sequences created with :func:`to_sequence()` are equal to
+    the ones already stored in the PDBx file.
+    """
+    if "4gxy" in path:
+        pytest.skip(
+            "Edge case: contains 'GTP' which has one-letter code, "
+            "but is a 'NON-POLYMER' in the CCD"
+        )
+
+    pdbx_file = pdbx.BinaryCIFFile.read(path)
+    ref_sequences = pdbx.get_sequence(pdbx_file)
+
+    atoms = pdbx.get_structure(pdbx_file, use_author_fields=False, model=1)
+    # Remove pure solvent chains
+    # In those chains the "label_seq_id" is usually "."
+    # which is translated to -1
+    atoms = atoms[atoms.res_id != -1]
+    test_sequences, _ = struc.to_sequence(atoms, allow_hetero=True)
+
+    # Matching against the PDBx file is not trivial
+    #   * The file stores duplicate sequences only once
+    #     -> Matching any sequence from file is sufficient
+    #   * The resolved part of the chain might miss residues
+    #     -> Sequence identity from alignment is expected to be 1.0
+    for sequence in test_sequences:
+        best_aln, best_identity = _find_best_match(sequence, ref_sequences)
+        try:
+            assert best_identity == 1.0
+        except AssertionError:
+            print(best_aln)
+            raise
+
+
+def _find_best_match(sequence, ref_sequences):
+    best_alignment = None
+    best_identity = 0.0
+    for ref_sequence in ref_sequences:
+        if type(sequence) != type(ref_sequence):
+            continue
+        if isinstance(sequence, seq.ProteinSequence):
+            matrix = align.SubstitutionMatrix.std_protein_matrix()
+        else:
+            matrix = align.SubstitutionMatrix.std_nucleotide_matrix()
+        alignment = align.align_optimal(
+            sequence, ref_sequence, matrix,
+            terminal_penalty=False, max_number=1
+        )[0]
+        # The 'shortest' identity is 1.0, if every residue in the
+        # test sequence is aligned to an identical residue
+        identity = align.get_sequence_identity(alignment, mode="shortest")
+        if identity > best_identity:
+            best_alignment = alignment
+            best_identity = identity
+    return best_alignment, best_identity
