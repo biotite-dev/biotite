9c25dbb32bec0ae9d7ee47e0f6160251ec172d9b
Martin Larralde <martin.larralde@embl.de>
2024-09-14 15:57:27 +0200

    Add `to_3di` helper function to obtain a `StructureSequence` from an `AtomArray`

--- tests/structure/test_3di.py
+++ tests/structure/test_3di.py
@@ -5,7 +5,9 @@ import numpy
 
 from biotite.structure.io.pdb import PDBFile
 from biotite.structure.atoms import AtomArray
+from biotite.structure.alphabet import to_3di
 from biotite.structure.alphabet.encoder import Encoder, PartnerIndexEncoder, VirtualCenterEncoder
+from biotite.structure.chains import apply_chain_wise
 from tests.util import data_dir
 
 
@@ -16,125 +18,34 @@ def _get_structure(name):
     return structure if isinstance(structure, AtomArray) else structure[0]
 
 
-class TestVirtualCenterEncoder(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.encoder = VirtualCenterEncoder()
-
-    def test_calc_virtual_center(self):
-        ca = numpy.array([[34.826, 19.254, 17.339]])
-        cb = numpy.array([[35.285, 18.694, 15.994]])
-        n_ = numpy.array([[35.805, 19.041, 18.426]])
-
-        vc = self.encoder._compute_virtual_center(ca, cb, n_)
-        self.assertAlmostEqual(vc[0, 0], 32.2276, places=4)
-        self.assertAlmostEqual(vc[0, 1], 20.2157, places=4)
-        self.assertAlmostEqual(vc[0, 2], 16.0518, places=4)
-
-        ca = numpy.array([[21.056, 18.27, 0.063]])
-        cb = numpy.array([[21.428, 19.604, 0.838]])
-        n_ = numpy.array([[21.789, 17.734, -1.084]])
-
-        vc = self.encoder._compute_virtual_center(ca, cb, n_)
-        self.assertAlmostEqual(vc[0, 0], 18.5941, places=4)
-        self.assertAlmostEqual(vc[0, 1], 17.8221, places=4)
-        self.assertAlmostEqual(vc[0, 2], 2.01565, places=4)
-
-    def test_encode_1xso_chainA(self):
-        structure = _get_structure("1xso")
-        chain = structure[structure.chain_id == 'A']
-        centers = self.encoder.encode_atoms(chain, disordered_atom="best")
-
-        self.assertAlmostEqual(centers[0, 0], 30.653, places=4)
-        self.assertAlmostEqual(centers[0, 1], 12.7129, places=4)
-        self.assertAlmostEqual(centers[0, 2], -3.9203, places=4)
-
-        # self.assertAlmostEqual(centers[4, 0], 18.5941, places=4)
-        # self.assertAlmostEqual(centers[4, 1], 17.8221, places=4)
-        # self.assertAlmostEqual(centers[4, 2], 2.01565, places=4)
-
-
-class TestPartnerIndexEncoder(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.encoder = PartnerIndexEncoder()
-
-    @classmethod
-    def get_structure(cls, name):
-        path = pathlib.Path(data_dir("structure")).joinpath(f"{name}.pdb")
-        return cls.parser.get_structure(name, path)
-
-    # def test_encode_1xso_chainA(self):
-    #     structure = _get_structure("1xso")
-    #     chain = structure[structure.chain_id == 'A']
-    #     partners = self.encoder.encode_atoms(chain, disordered_atom="last")
-    #     self.assertListEqual(
-    #         partners[1:-1].tolist(),
-    #         # fmt: off
-    #         [
-    #             17, 145, 15, 143, 13, 141, 6, 53, 8, 11, 12, 32, 5, 30, 3, 28,
-    #             1, 26, 20, 19, 20, 21, 98, 98, 96, 18, 94, 16, 92, 14, 90, 12,
-    #             89, 89, 117, 35, 85, 37, 83, 115, 80, 113, 79, 111, 57, 57,
-    #             110, 143, 50, 53, 50, 50, 50, 55, 56, 55, 45, 45, 132, 43, 77,
-    #             77, 62, 62, 66, 75, 131, 76, 122, 80, 97, 73, 74, 73, 66, 68,
-    #             62, 99, 43, 41, 70, 119, 39, 91, 37, 87, 86, 87, 33, 31, 84,
-    #             29, 94, 27, 94, 25, 71, 23, 78, 106, 23, 107, 104, 103, 104,
-    #             100, 102, 144, 144, 47, 44, 142, 42, 139, 40, 136, 35, 136, 82,
-    #             134, 134, 69, 130, 125, 124, 127, 128, 127, 132, 120, 67, 59,
-    #             120, 120, 136, 116, 138, 137, 114, 7, 6, 112, 4, 109, 2, 109,
-    #             148, 147
-    #         ]
-    #         # fmt: on
-    #     )
-
-
 class TestEncoder(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.encoder = Encoder()
 
-    # def test_encode_1xso_disordered_last(self):
-    #     structure = _get_structure("1xso")
-    #     states = self.encoder.encode_chain(structure[0]["A"], disordered_atom="last")
-    #     sequence = self.encoder.build_sequence(states)
-    #     self.assertEqual(
-    #         sequence,
-    #         "DKKKWWKDFPDPKTKIKIWDDDDLFKIKIWMKIFQADFDKKWKWWACAQDCPVHVVVSHFGAAPPD"
-    #         "FWDFAQPDPRHGLTGDFIFGDDPRMTTDMDIHNSAGCDDPNRQQRIKMFIANAGQCGLPPPDPVSR"
-    #         "GTSPRDDTRIMTGMHDDD",
-    #     )
-
     def test_encode_3bww(self):
         structure = _get_structure("3bww")
         chain = structure[structure.chain_id == 'A']
-        states = self.encoder.encode_atoms(chain)
-        sequence = self.encoder.build_sequence(states)
+        sequence = to_3di(chain)
         self.assertEqual(
-            sequence,
+            str(sequence),
             "DKDFFEAAEDDLVCLVVLLPPPACPQRQAYEDALVVQVPDDPVSVVSVVNSLVHHAYAYEYEAQQL"
             "LDDPQGDVVSLVSVLVCCVVSVPQEYEYENDPPDADALDVVSLVSSLVSQLVSCVSSVGAYAYEDA"
             "ADQDHDPRHPDDVLVSRQSNCVSNVHAHAYELVRLVRCCVRPVPDDSLVSLVRHPLQRHQHYEYQV"
             "VSVVSVLVNLVDHQAHHYYYHDYPDDVVVNSVVRVVSRVSNVVSCVVVVHYIDMD",
         )
 
-    # def test_encode_3bww_masked(self):
-    #     structure = self.get_structure("3bww.masked")
-    #     states = self.encoder.encode_chain(structure[0]["A"])
-    #     sequence = self.encoder.build_sequence(states)
-    #     self.assertEqual(
-    #         sequence,
-    #         "DKDFFEAAEDDLVCLVVLLPPPACPQRQAYEDALVVQVPDDPVSVVSVVNSLVHHAYAYEYEAQQL"
-    #         "DDDPQGDVVSLVSVLVCCVVSVPQEYEYENDPPDADALDPVDDDSSLVSQLVSCVSSVGAYAYEDA"
-    #         "ADQDHDPRHPDDVLVSRQVSCVSNVHAHAYELVRLVRCCVRPVPDDSLVSLVRHPLQRHQHYEYQV"
-    #         "VSVVSVLVNLVDHQAHHYYYHDYPDDVVVNSVVRVVSRVSNVVSCVVVVHYIDMD",
-    #     )
+    def test_encode_8crb(self):
+        structure = _get_structure("8crb")
+        sequences = apply_chain_wise(structure, structure, to_3di, axis=None)
+        self.assertEqual(len(sequences), 3)
 
     def test_encode_8crb_A(self):
         structure = _get_structure("8crb")
-        states = self.encoder.encode_atoms(structure[structure.chain_id == 'A'])
-        sequence = self.encoder.build_sequence(states)
+        chain = structure[structure.chain_id == 'A']
+        sequence = to_3di(chain)
         self.assertEqual(
-            sequence,
+            str(sequence),
             "DWAKDKDWADEDAAQAKTKIKMATPPDLLQDFFKFKWFDAPPDDIDGQAPGACPSPPLADDVHHHH"
             "GKGWHDDSVRRMIMIMGGNDDQVVFGKMKMFTADDADPQVVVPDGDDTDDMHDIDTYGHPPDDFFA"
             "WDKDKDQDDPVPCPVQKPKIKMKTDDGDDDDKDKAWLVNPGDPQKDDFDWDADPVRGIIDMIIGMD"
@@ -143,10 +54,10 @@ class TestEncoder(unittest.TestCase):
 
     def test_encode_8crb_B(self):
         structure = _get_structure("8crb")
-        states = self.encoder.encode_atoms(structure[structure.chain_id == 'B'])
-        sequence = self.encoder.build_sequence(states)
+        chain = structure[structure.chain_id == 'B']
+        sequence = to_3di(chain)
         self.assertEqual(
-            sequence,
+            str(sequence),
             "DAAKDFDQQEEEAAQAKDKGWIFAADVPPVPDAFWKWWDAPPDDIDTAADPNQAGDPVDHSQKGWD"
             "ADHGITIIMGGRDDNSRQGFIWRAQPDDPDHNGHTDDTHGYYHCPDDQDDKDKDWDDAAVVVLVVL"
             "FGKTKIKIDDGDDPPKDKFKDLQNHTDDAQWDWDDWDLDPVRTIMTMIIRRDGVVSCVVSQKMKMW"
@@ -155,10 +66,10 @@ class TestEncoder(unittest.TestCase):
 
     def test_encode_8crb_C(self):
         structure = _get_structure("8crb")
-        states = self.encoder.encode_atoms(structure[structure.chain_id == 'C'])
-        sequence = self.encoder.build_sequence(states)
+        chain = structure[structure.chain_id == 'C']
+        sequence = to_3di(chain)
         self.assertEqual(
-            sequence,
+            str(sequence),
             "DPCVLVVLVLQLVLVVLLLVVVVVVLVVCVVVLFKDWQDPVHDWQLACVSPDHDCPDCCSVPGSNN"
             "VQQCPKPLDDVTATNQSVQQIDDGDLDHDDDDDTIQGCPPPVRCSVVVVVVSVVSVVVSVVSCVVS"
             "VVVVVVD",
