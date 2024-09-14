401ba990244848ec0962bcbdb5a132ccbde0016d
Martin Larralde <martin.larralde@embl.de>
2024-09-14 15:57:27 +0200

    Add `to_3di` helper function to obtain a `StructureSequence` from an `AtomArray`

--- src/biotite/structure/alphabet/__init__.py
+++ src/biotite/structure/alphabet/__init__.py
@@ -6,8 +6,34 @@
 NumPy port of the ``foldseek`` code for encoding structures to 3di.
 """
 
-__all__ = ["Encoder", "FeatureEncoder", "PartnerIndexEncoder", "VirtualCenterEncoder"]
-__author__ = "Martin Larralde <martin.larralde@embl.de>"
 __name__ = "biotite.structure.alphabet"
+__author__ = "Martin Larralde"
+__all__ = ["Encoder", "StructureSequence", "to_3di"]
 
-from .encoder import Encoder, FeatureEncoder, PartnerIndexEncoder, VirtualCenterEncoder
+from .encoder import Encoder
+from .sequence import StructureSequence
+
+def to_3di(array):
+    r"""
+    Encode the atoms to the 3di structure alphabet.
+
+    Parameters
+    ----------
+    atoms : AtomArray
+        The atom array to encode to 3di. All atoms must be part of
+        a single chain.
+
+    Returns
+    -------
+    sequence : StructureSequence
+        The encoded structure sequence.
+
+    Note
+    ----
+    To encode atoms in different chains, use :func:`apply_chain_wise` to
+    return a list with one sequence per chain.
+    """
+    encoder = Encoder()
+    sequence = StructureSequence()
+    sequence.code = encoder.encode_atoms(array).filled()
+    return sequence
