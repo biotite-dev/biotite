d89e5abe9699647d4fd1510447febd5b3896e29e
Patrick Kunzmann <padix.key@gmail.com>
2024-10-15 18:33:23 +0200

    Make `dihedral_backbone()` more robust against missing atoms

--- doc/examples/scripts/structure/protein/ramachandran.py
+++ doc/examples/scripts/structure/protein/ramachandran.py
@@ -21,12 +21,14 @@ import biotite.structure.io as strucio
 # Download and parse file
 file = rcsb.fetch("3vkh", "cif", gettempdir())
 atom_array = strucio.load_structure(file)
+# Extract first peptide chain
+peptide = atom_array[struc.filter_amino_acids(atom_array)]
+chain = peptide[peptide.chain_id == "A"]
 # Calculate backbone dihedral angles
 # from one of the two identical chains in the asymmetric unit
-phi, psi, omega = struc.dihedral_backbone(atom_array[atom_array.chain_id == "A"])
-# Conversion from radians into degree
-phi *= 180 / np.pi
-psi *= 180 / np.pi
+phi, psi, omega = struc.dihedral_backbone(chain)
+phi = np.rad2deg(phi)
+psi = np.rad2deg(psi)
 # Remove invalid values (NaN) at first and last position
 phi = phi[1:-1]
 psi = psi[1:-1]
