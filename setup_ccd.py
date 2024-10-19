3a7437de1eddf14d78eb14140b0d519ca191a854
Simon Mathis <simon.mathis@gmail.com>
2024-10-19 09:04:33 +0100

    Micro-PR to add `alt_atom_id` to Biotite's internal CCD (#668)
    * feat: add alt_atom_id to biotite's internal CCD
    
    * fix: remove accidental duplicate block
    
    * chore: ruff format
    
    * fix: move `alt_atom_id` block below `atom_id`

--- setup_ccd.py
+++ setup_ccd.py
@@ -147,6 +147,22 @@ ATOM_COLUMNS = {
             )
         ],
     ),
+    "alt_atom_id": ColumnInfo(
+        "U6",
+        [
+            StringArrayEncoding(
+                # The unique strings in the column are sorted
+                # -> Indices do not follow distinct pattern
+                data_encoding=[ByteArrayEncoding(type=TypeCode.INT16)],
+                offset_encoding=[
+                    DeltaEncoding(src_type=TypeCode.INT32),
+                    RunLengthEncoding(),
+                    IntegerPackingEncoding(byte_count=1, is_unsigned=True),
+                    ByteArrayEncoding(),
+                ],
+            )
+        ],
+    ),
     "type_symbol": ColumnInfo(
         "U2",
         [
