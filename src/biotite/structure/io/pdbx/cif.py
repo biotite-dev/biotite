a1d361eaed9138f25718031bd37932f3062b95d8
xvlaurent <xavier.laurent@discngine.com>
2024-10-25 16:23:04 +0000

    Enhance _split_one_line and remove whitespace parameter

--- src/biotite/structure/io/pdbx/cif.py
+++ src/biotite/structure/io/pdbx/cif.py
@@ -357,7 +357,7 @@ class CIFCategory(_Component, MutableMapping):
         return CIFBlock
 
     @staticmethod
-    def deserialize(text, expect_whitespace=True):
+    def deserialize(text):
         lines = [line.strip() for line in text.splitlines() if not _is_empty(line)]
 
         if _is_loop_start(lines[0]):
@@ -372,7 +372,7 @@ class CIFCategory(_Component, MutableMapping):
 
         lines = _to_single(lines)
         if is_looped:
-            category_dict = CIFCategory._deserialize_looped(lines, expect_whitespace)
+            category_dict = CIFCategory._deserialize_looped(lines)
         else:
             category_dict = CIFCategory._deserialize_single(lines)
         return CIFCategory(category_dict, category_name)
@@ -467,7 +467,7 @@ class CIFCategory(_Component, MutableMapping):
         return category_dict
 
     @staticmethod
-    def _deserialize_looped(lines, expect_whitespace):
+    def _deserialize_looped(lines):
         """
         Process a category where each field has multiple values
         (category is a table).
@@ -490,20 +490,7 @@ class CIFCategory(_Component, MutableMapping):
         # row-line-alignment at all and simply cycle through columns
         column_indices = itertools.cycle(range(len(column_names)))
         for data_line in data_lines:
-            # If whitespace is expected in quote protected values,
-            # use regex-based _split_one_line() to split
-            # Otherwise use much more faster whitespace split
-            # and quote removal if applicable.
-            if expect_whitespace:
-                values = _split_one_line(data_line)
-            else:
-                values = data_line.split()
-                for k in range(len(values)):
-                    # Remove quotes
-                    if (values[k][0] == '"' and values[k][-1] == '"') or (
-                        values[k][0] == "'" and values[k][-1] == "'"
-                    ):
-                        values[k] = values[k][1:-1]
+            values = _split_one_line(data_line)
             for val in values:
                 column_index = next(column_indices)
                 column_name = column_names[column_index]
@@ -685,15 +672,7 @@ class CIFBlock(_Component, MutableMapping):
             # Element is stored in serialized form
             # -> must be deserialized first
             try:
-                # Special optimization for "atom_site":
-                # Even if the values are quote protected,
-                # no whitespace is expected in escaped values
-                # Therefore slow regex-based _split_one_line() call is not necessary
-                if key == "atom_site":
-                    expect_whitespace = False
-                else:
-                    expect_whitespace = True
-                category = CIFCategory.deserialize(category, expect_whitespace)
+                category = CIFCategory.deserialize(category)
             except Exception:
                 raise DeserializationError(f"Failed to deserialize category '{key}'")
             # Update with deserialized object
@@ -1064,28 +1043,34 @@ def _split_one_line(line):
     if line[0] == ";":
         return [line[1:]]
 
-    # Define the patterns for different types of fields
-    single_quote_pattern = r"('(?:'(?! )|[^'])*')(?:\s|$)"
-    double_quote_pattern = r'("(?:"(?! )|[^"])*")(?:\s|$)'
-    unquoted_pattern = r"([^\s]+)"
-
-    # Combine the patterns using alternation
-    combined_pattern = (
-        f"{single_quote_pattern}|{double_quote_pattern}|{unquoted_pattern}"
-    )
-
-    # Find all matches
-    matches = re.findall(combined_pattern, line)
-
-    # Extract non-empty groups from the matches
-    fields = []
-    for match in matches:
-        field = next(group for group in match if group)
-        if field[0] == field[-1] == "'" or field[0] == field[-1] == '"':
-            field = field[1:-1]
-        fields.append(field)
-    return fields
-
+    # Initialize loop variables
+    res = []
+    separator = " "
+    # Loop over the line
+    while line:
+        # Split the line on separator
+        word, _, new_line = line.lstrip().partition(separator)
+        # Handle the case where the word strat with a quote
+        if word.startswith(("'", '"')):
+            # Set the separator to the quote found
+            separator = word[0]
+            # Handle the case of a quoted word without space
+            if word.endswith(separator) and len(word) > 1:
+                res.append(word[1:-1])
+                separator = " "
+                line = new_line
+                continue
+            # Reconstruct line without the starting quote
+            new_line = line.lstrip()[1:]
+            # Capture the word until the closing quote
+            word = "".join(itertools.takewhile(lambda x : x != separator, new_line))
+            # Reconstruct line without the captured word
+            new_line = new_line.removeprefix(word + separator)
+        # Reset separator to whitespace
+        separator = " "
+        res.append(word)
+        line = new_line
+    return res
 
 def _arrayfy(data):
     if not isinstance(data, (Sequence, np.ndarray)) or isinstance(data, str):
