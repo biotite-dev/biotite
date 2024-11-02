67b11f2352e745b39bb3d8081c9146b90e36ddc2
Patrick Kunzmann <padix.key@gmail.com>
2024-11-02 16:01:47 +0100

    Add docstring for `structure.alphabet` subpackage

--- src/biotite/structure/alphabet/__init__.py
+++ src/biotite/structure/alphabet/__init__.py
@@ -4,6 +4,18 @@
 
 """
 A subpackage for converting structures to structural alphabet sequences.
+
+Structural alphabets represent the local geometry of each residue in a structure as
+symbol in a sequence.
+This allows using sequence-based functionality from :mod:`biotite.sequence` on
+structural data.
+
+For each supported structural alphabet, this subpackage provides a conversion function
+that converts each chain of a given structure into a :class:`Sequence` object from the
+respective structural alphabet.
+
+Note that the structural alphabets use lower-case letters as symbols, in order to
+distinguish them better from the nucleotide and amino acid alphabets.
 """
 
 __name__ = "biotite.structure.alphabet"
