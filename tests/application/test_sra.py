fe77dbf9cdbf249b03900a8d84f62d41a9cbd2f2
Patrick Kunzmann <padix.key@gmail.com>
2024-12-15 13:49:36 +0100

    fixup! Skip SRA-tools test, if not installed

--- tests/application/test_sra.py
+++ tests/application/test_sra.py
@@ -12,7 +12,9 @@ from biotite.sequence.io.fastq import FastqFile
 from tests.util import is_not_installed
 
 
-@pytest.mark.skipif(is_not_installed("fasterq-dump"), reason="sra-tools is not installed")
+@pytest.mark.skipif(
+    is_not_installed("fasterq-dump"), reason="sra-tools is not installed"
+)
 @pytest.mark.parametrize(
     "app_class, custom_prefix",
     itertools.product([FastqDumpApp, FastaDumpApp], [False, True]),
@@ -44,7 +46,9 @@ def test_objects(app_class, custom_prefix):
             assert isinstance(fasta_file, FastaFile)
 
 
-@pytest.mark.skipif(is_not_installed("fasterq-dump"), reason="sra-tools is not installed")
+@pytest.mark.skipif(
+    is_not_installed("fasterq-dump"), reason="sra-tools is not installed"
+)
 @pytest.mark.parametrize(
     "app_class, custom_prefix",
     itertools.product([FastqDumpApp, FastaDumpApp], [False, True]),
