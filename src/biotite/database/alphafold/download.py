73bfa992606c679773d5dbc3111eb9682d32f664
Patrick Kunzmann <padix.key@gmail.com>
2025-01-25 15:38:21 +0100

    Finalize AFDB interface

--- src/biotite/database/alphafold/download.py
+++ src/biotite/database/alphafold/download.py
@@ -3,19 +3,25 @@
 # information.
 
 __name__ = "biotite.database.alphafold"
-__author__ = "Alex Carlin"
+__author__ = "Patrick Kunzmann, Alex Carlin"
 __all__ = ["fetch"]
 
 import io
-import os
-from os.path import getsize, isdir, isfile, join
+import re
+from pathlib import Path
+from xml.etree import ElementTree
 import requests
-from biotite.database.alphafold.check import assert_valid_response
+from biotite.database.error import RequestError
 
-_fetch_url = "https://alphafold.com/api/prediction"
+_METADATA_URL = "https://alphafold.com/api/prediction"
+_BINARY_FORMATS = ["bcif"]
+# Adopted from https://www.uniprot.org/help/accession_numbers
+_UNIPROT_PATTERN = (
+    "[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}"
+)
 
 
-def fetch(ids, target_path=None, format="pdb", overwrite=False, verbose=False):
+def fetch(ids, format="pdb", target_path=None, overwrite=False, verbose=False):
     """
     Download predicted protein structures from the AlphaFold DB.
 
@@ -24,22 +30,19 @@ def fetch(ids, target_path=None, format="pdb", overwrite=False, verbose=False):
     Parameters
     ----------
     ids : str or iterable object of str
-        A single ID or a list of IDs of the file(s)
-        to be downloaded.
+        A single ID or a list of IDs of the file(s) to be downloaded.
+    format : {'pdb', 'pdbx', 'cif', 'mmcif', 'bcif', 'fasta'}
+        The format of the files to be downloaded.
     target_path : str, optional
         The target directory of the downloaded files.
         By default, the file content is stored in a file-like object
         (`StringIO` or `BytesIO`, respectively).
-    format : {"pdb", "cif", "bcif"}
-        The format of the files to be downloaded.
     overwrite : bool, optional
-        If true, existing files will be overwritten. Otherwise the
-        respective file will only be downloaded if the file does not
-        exist yet in the specified target directory or if the file is
-        empty. (Default: False)
+        If true, existing files will be overwritten.
+        Otherwise the respective file will only be downloaded if the file does not
+        exist yet in the specified target directory or if the file is empty.
     verbose: bool, optional
         If true, the function will output the download progress.
-        (Default: False)
 
     Returns
     -------
@@ -55,13 +58,17 @@ def fetch(ids, target_path=None, format="pdb", overwrite=False, verbose=False):
     --------
 
     >>> import os.path
-    >>> file = fetch("P12345", path_to_directory)
-    >>> print(os.path.basename(file))
-    P12345.pdb
-    >>> files = fetch(["P12345", "Q8K9I1"], path_to_directory)
-    >>> print([os.path.basename(file) for file in files])
-    ['P12345.pdb', 'Q8K9I1.pdb']
+    >>> file = fetch("P12345", "cif", path_to_directory)
+    >>> print(Path(file).name)
+    P12345.cif
+    >>> files = fetch(["P12345", "Q8K9I1"], "cif", path_to_directory)
+    >>> print([Path(file).name for file in files])
+    ['P12345.cif', 'Q8K9I1.cif']
     """
+    if format not in ["pdb", "pdbx", "cif", "mmcif", "bcif", "fasta"]:
+        raise ValueError(f"Format '{format}' is not supported")
+    if format in ["pdbx", "mmcif"]:
+        format = "cif"
 
     # If only a single ID is present,
     # put it into a single element list
@@ -70,9 +77,10 @@ def fetch(ids, target_path=None, format="pdb", overwrite=False, verbose=False):
         single_element = True
     else:
         single_element = False
-    # Create the target folder, if not existing
-    if target_path is not None and not isdir(target_path):
-        os.makedirs(target_path)
+    if target_path is not None:
+        target_path = Path(target_path)
+        target_path.mkdir(parents=True, exist_ok=True)
+
     files = []
     for i, id in enumerate(ids):
         # Verbose output
@@ -80,32 +88,102 @@ def fetch(ids, target_path=None, format="pdb", overwrite=False, verbose=False):
             print(f"Fetching file {i + 1:d} / {len(ids):d} ({id})...", end="\r")
         # Fetch file from database
         if target_path is not None:
-            file = join(target_path, id + "." + format)
+            file = target_path / f"{id}.{format}"
         else:
             # 'file = None' -> store content in a file-like object
             file = None
-        if file is None or not isfile(file) or getsize(file) == 0 or overwrite:
-            if format in ["pdb", "cif"]:
-                metadata_response = requests.get(f"{_fetch_url}/{id}")
-                assert_valid_response(metadata_response.status_code)
-                metadata_json = metadata_response.json()[0]
-                # a list of length 1 is always returned
-                file_url = metadata_json[f"{format}Url"]
-                file_response = requests.get(file_url)
-                assert_valid_response(file_response.status_code)
+        if file is None or not file.is_file() or file.stat().st_size == 0 or overwrite:
+            file_response = requests.get(_get_file_url(id, format))
+            _assert_valid_file(file_response, id)
+            if format in _BINARY_FORMATS:
+                content = file_response.content
+            else:
                 content = file_response.text
-            else:
-                raise ValueError(f"Format '{format}' is not supported")
+
             if file is None:
-                file = io.StringIO(content)
+                if format in _BINARY_FORMATS:
+                    file = io.BytesIO(content)
+                else:
+                    file = io.StringIO(content)
             else:
-                with open(file, "w+") as f:
+                mode = "wb+" if format in _BINARY_FORMATS else "w+"
+                with open(file, mode) as f:
                     f.write(content)
+
         files.append(file)
     if verbose:
         print("\nDone")
-    # If input was a single ID, return only a single path
+
+    # Return paths as strings
+    files = [file.as_posix() if isinstance(file, Path) else file for file in files]
+    # If input was a single ID, return only a single element
     if single_element:
         return files[0]
     else:
         return files
+
+
+def _get_file_url(id, format):
+    """
+    Get the actual file URL for the given ID from the ``prediction`` API endpoint.
+
+    Parameters
+    ----------
+    id : str
+        The ID of the file to be downloaded.
+    format : str
+        The format of the file to be downloaded.
+
+    Returns
+    -------
+    file_url : str
+        The URL of the file to be downloaded.
+    """
+    uniprot_id = _extract_id(id)
+    metadata = requests.get(f"{_METADATA_URL}/{uniprot_id}").json()
+    if len(metadata) == 0:
+        raise RequestError(f"ID {id} is invalid")
+    # A list of length 1 is always returned, if the response is valid
+    return metadata[0][f"{format}Url"]
+
+
+def _extract_id(id):
+    """
+    Extract a AFDB compatible UniProt ID from the given qualifier.
+    This may comprise
+
+    - Directly the UniProt ID (e.g. ``P12345``) (trivial case)
+    - Entry ID, as also returned by the RCSB search API (e.g. ``AF-P12345-F1``)
+
+    Parameters
+    ----------
+    id : str
+        The qualifier to extract the UniProt ID from.
+
+    Returns
+    -------
+    uniprot_id : str
+        The UniProt ID.
+    """
+    match = re.search(_UNIPROT_PATTERN, id)
+    if match is None:
+        raise ValueError(f"Cannot extract AFDB identifier from '{id}'")
+    return match.group()
+
+
+def _assert_valid_file(response, id):
+    """
+    Checks whether the response is an actual structure file
+    or the response a *404* error due to invalid UniProt ID.
+    """
+    if len(response.text) == 0:
+        raise RequestError(f"Received no repsone for '{id}'")
+    try:
+        root = ElementTree.fromstring(response.text)
+        if root.tag == "Error":
+            raise RequestError(
+                f"Error while fetching '{id}': {root.find('Message').text}"
+            )
+    except ElementTree.ParseError:
+        # This is not XML -> the response is probably a valid file
+        pass
--- tests/database/test_alphafold.py
+++ tests/database/test_alphafold.py
@@ -2,7 +2,6 @@
 # under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
 # information.
 
-import itertools
 import tempfile
 import pytest
 import biotite.database.alphafold as alphafold
@@ -11,47 +10,64 @@ import biotite.structure.io.pdbx as pdbx
 from biotite.database import RequestError
 from tests.util import cannot_connect_to
 
-ALPHAFOLD_URL = "https://alphafold.com/"
+ALPHAFOLD_URL = "https://alphafold.ebi.ac.uk/"
 
 
 @pytest.mark.skipif(
-    cannot_connect_to(ALPHAFOLD_URL), reason="AlphaFold is not available"
+    cannot_connect_to(ALPHAFOLD_URL), reason="AlphaFold DB is not available"
 )
-@pytest.mark.parametrize("as_file_like", itertools.product([False, True]))
-def test_fetch(as_file_like):
+@pytest.mark.parametrize("as_file_like", [False, True])
+@pytest.mark.parametrize("entry_id", ["P12345", "AF-P12345-F1"])
+@pytest.mark.parametrize("format", ["pdb", "cif", "bcif"])
+def test_fetch(as_file_like, entry_id, format):
+    """
+    Check if files in different formats can be downloaded by being able to parse them.
+    """
     path = None if as_file_like else tempfile.gettempdir()
-    file = alphafold.fetch("P12345", path, overwrite=True)
-    pdb_file = pdb.PDBFile.read(file)
-    array_stack = pdb_file.get_structure()
-    assert len(array_stack) > 0
+    file_path_or_obj = alphafold.fetch(entry_id, format, path, overwrite=True)
+    if format == "pdb":
+        file = pdb.PDBFile.read(file_path_or_obj)
+        pdb.get_structure(file)
+    elif format == "cif":
+        file = pdbx.CIFFile.read(file_path_or_obj)
+        pdbx.get_structure(file)
+    elif format == "bcif":
+        file = pdbx.BinaryCIFFile.read(file_path_or_obj)
+        pdbx.get_structure(file)
 
 
 @pytest.mark.skipif(
-    cannot_connect_to(ALPHAFOLD_URL), reason="AlphaFold is not available"
+    cannot_connect_to(ALPHAFOLD_URL), reason="AlphaFold DB is not available"
 )
-@pytest.mark.parametrize("format", ["pdb", "cif"])
-def test_fetch_invalid(format):
-    with pytest.raises(RequestError):
-        alphafold.fetch(
-            "XYZ", target_path=tempfile.gettempdir(), format=format, overwrite=True
-        )
-
-
-@pytest.mark.skipif(
-    cannot_connect_to(ALPHAFOLD_URL), reason="AlphaFold is not available"
-)
-@pytest.mark.parametrize("format", ["pdb", "cif"])
-def test_fetch_multiple(format):
-    acc = ["P12345", "P12345"]
-    files = alphafold.fetch(
-        acc, target_path=tempfile.gettempdir(), format=format, overwrite=True
-    )
-    print(files)
+def test_fetch_multiple():
+    """
+    Check if multiple files can be downloaded by being able to parse them.
+    """
+    ids = ["P12345", "Q8K9I1"]
+    files = alphafold.fetch(ids, "cif", tempfile.gettempdir(), overwrite=True)
     for file in files:
-        if format == "pdb":
-            pdb_file = pdb.PDBFile.read(file)
-            structure = pdb_file.get_structure()
-            assert len(structure) > 0
-        elif format == "cif":
-            cif_file = pdbx.PDBxFile.read(file)
-            assert "citation_author" in cif_file.keys()
+        assert "citation_author" in pdbx.CIFFile.read(file).block
+
+
+@pytest.mark.skipif(
+    cannot_connect_to(ALPHAFOLD_URL), reason="AlphaFold DB is not available"
+)
+@pytest.mark.parametrize("format", ["pdb", "cif", "bcif"])
+@pytest.mark.parametrize("invalid_id", ["", "XYZ", "A0A12345"])
+@pytest.mark.parametrize("bypass_metadata", [False, True])
+def test_fetch_invalid(monkeypatch, format, invalid_id, bypass_metadata):
+    """
+    Check if proper exceptions are raised if a given ID is invalid.
+    Also check whether the check works on the file retrieval level via
+    :func:`_get_file_url()`, by bypassing the metadata check.
+    """
+    import biotite.database.alphafold.download as module
+
+    if bypass_metadata:
+        monkeypatch.setattr(
+            module,
+            "_get_file_url",
+            lambda id, f: f"https://alphafold.ebi.ac.uk/files/AF-{id}-F1-model_v4.{f}",
+        )
+    with pytest.raises((RequestError, ValueError)):
+        alphafold.fetch(invalid_id, format)
