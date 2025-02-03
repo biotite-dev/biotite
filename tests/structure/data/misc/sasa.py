5cf3151b77adba15c60da06ee7f76a239cb66ade
Simon Mathis <simon.mathis@gmail.com>
2025-02-03 09:47:09 +0000

    fix: udpate test reference SASA values

--- tests/structure/data/misc/sasa.py
+++ tests/structure/data/misc/sasa.py
@@ -2,7 +2,7 @@ import json
 import warnings
 from pathlib import Path
 import mdtraj
-from biotite.structure.info.radii import _SINGLE_ELEMENT_VDW_RADII as SINGLE_RADII
+from biotite.structure.info.radii import _SINGLE_ATOM_VDW_RADII as SINGLE_RADII
 
 PDB_IDS = ["1l2y", "1gya"]
 STRUCTURE_DIR = Path(__file__).parents[1]
@@ -12,14 +12,14 @@ OUTPUT_FILE = Path(__file__).parent / "sasa.json"
 def compute_mdtraj_sasa(pdb_path):
     # Use the same atom radii as Biotite
     radii = {
-        element.capitalize(): radius / 10 for element, radius in SINGLE_RADII.items()
+        element.capitalize(): radius / 10.0 for element, radius in SINGLE_RADII.items()
     }
     # Ignore warning about dummy unit cell vector
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         traj = mdtraj.load(pdb_path)
     # Conversion from nm^2 to A^2
-    return mdtraj.shrake_rupley(traj, change_radii=radii, n_sphere_points=5000)[0] * 100
+    return mdtraj.shrake_rupley(traj, change_radii=radii, n_sphere_points=5000)[0] * 100.0
 
 
 if __name__ == "__main__":
