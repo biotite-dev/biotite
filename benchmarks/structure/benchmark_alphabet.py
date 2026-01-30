4ba967fe39660e8c074954c6ab8ecf6b9ac8943e
Patrick Kunzmann <padix.key@gmail.com>
2026-01-30 21:56:25 +0100

    Add benchmarks for modules to be replaced by Rust code

--- benchmarks/structure/benchmark_alphabet.py
+++ benchmarks/structure/benchmark_alphabet.py
@@ -7,7 +7,7 @@ from tests.util import data_dir
 PDB_ID = "1aki"
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def atoms():
     pdbx_file = pdbx.BinaryCIFFile.read(Path(data_dir("structure")) / f"{PDB_ID}.bcif")
     return pdbx.get_structure(pdbx_file, model=1, include_bonds=True)
